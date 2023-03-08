faef8769fab1eae2c0eb38fd3076ef903485f7b8
Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
2023-03-08 10:46:49 +0000

    ♻️ Refactor `add_users_all_org_members_github_team` with smoke test (#2209)

---
+++ scripts/python/test_add_users_everyone_github_team.py
@@ -0,0 +1,26 @@
+import unittest
+from unittest.mock import patch
+
+from github import Github
+from gql import Client
+from gql.transport.aiohttp import AIOHTTPTransport
+
+import add_users_all_org_members_github_team
+
+
+@patch.object(Github, "__new__")
+@patch.object(AIOHTTPTransport, "__new__")
+@patch.object(Client, "__new__")
+class TestAddUsersEveryoneGithubTeam(unittest.TestCase):
+
+    @patch("sys.argv", ["", "test"])
+    def test_main_smoke_test(self, mock1, mock2, mock3):
+        add_users_all_org_members_github_team.main()
+
+    def test_main_returns_error_when_no_token_provided(self, mock1, mock2, mock3):
+        self.assertRaises(
+            ValueError, add_users_all_org_members_github_team.main)
+
+
+if __name__ == "__main__":
+    unittest.main()
