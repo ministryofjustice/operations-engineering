e4516ed81103b05ce7041d43b9aa3b399127df02
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-03-13 21:47:33 +0000

    refactor(assign-support): remove debugging as it triggers security
    CVE example listed here: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2018-0285

--- scripts/python/assign_support_tickets.py
+++ scripts/python/assign_support_tickets.py
@@ -17,11 +17,7 @@ def identify_support_issues(gh: GithubService, args):
 
 
 def assign_issues_to_creator(support_issues):
-    logging.debug(f"Assigning {len(support_issues)} issues to their creator")
-
     for issue in support_issues:
-        logging.debug(
-            f"Issue number {issue.number}, created by {issue.user.login}")
         issue.edit(assignees=[issue.user.login])
         logging.info(f"Assigned issue {issue.number} to {issue.user.login}")
 
@@ -56,32 +52,20 @@ def add_arguments():
         help="The GitHub tag to use",
     )
 
-    parser.add_argument(
-        "--debug",
-        action="store_true",
-        help="Enable debug logging",
-    )
-
     return parser.parse_args()
-
-
-def setup_logging(debug):
-    if debug:
-        logging.basicConfig(level=logging.DEBUG)
-    else:
-        logging.basicConfig(level=logging.INFO)
 
 
 def main():
     args = add_arguments()
-    setup_logging(args.debug)
-
-    logging.debug(f"args passed to the command line: {args}")
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)-8s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
 
     gh = GithubService(args.oauth_token, args.org)
 
     support_issues = identify_support_issues(gh, args)
-    logging.debug(f"support_issues identified: {support_issues}")
     if not support_issues:
         logging.info("No support issues found")
         sys.exit(0)
