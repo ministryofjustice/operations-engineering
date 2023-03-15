768c70fe3187dd426241904479295a03e921c44c
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-03-15 17:50:01 +0000

    feat(zenhub): create a move pipeline method
    the commit also contains some re-jig to get other methods working

--- scripts/python/services/ZenhubService.py
+++ scripts/python/services/ZenhubService.py
@@ -9,13 +9,45 @@ class ZenhubService:
     A service to interact with the Zenhub API.
     """
 
-    def __init__(self, api_token: str) -> None:
+    def __init__(self, api_token: str, repo_id: str) -> None:
         self.zenhub_client_gql_api: Client = Client(transport=AIOHTTPTransport(
             url="https://api.zenhub.com/public/graphql",
             headers={"Authorization": f"Bearer {api_token}"},
         ), execute_timeout=60)
+        # set workspace id from GitHub repo id
+        self.workspace_id: str = self.get_workspace_id(repo_id)
 
-    def get_pipeline_id(self, workspace_id: str, pipeline_name: str) -> str | TypeError:
+    def move_issue_to_pipeline(self, issue_to_move, pipeline_id: str):
+        print(f"Moving issue {issue_to_move} to pipeline {pipeline_id}")
+        move_issue_input = {
+            "issueId": issue_to_move,
+            "pipelineId": pipeline_id,
+        }
+
+        data = self.zenhub_client_gql_api.execute(gql("""
+           mutation moveIssue($moveIssueInput: MoveIssueInput!, $workspaceId: ID!) {
+              moveIssue(input: $moveIssueInput) {
+                issue {
+                  id
+                  pipelineIssue(workspaceId: $workspaceId) {
+                    priority {
+                      id
+                      name
+                      color
+                    }
+                    pipeline {
+                      id
+                    }
+                  }
+                }
+              }
+            } 
+        """), variable_values={"workspaceId": self.workspace_id, "moveIssueInput": move_issue_input})
+
+        if data["moveIssue"]["issue"]["pipelineIssue"]["pipeline"]["id"] != pipeline_id:
+            return TypeError(f"Could not move issue {issue_to_move} to pipeline {pipeline_id}")
+
+    def get_pipeline_id(self, pipeline_name: str) -> str | TypeError:
         data = self.zenhub_client_gql_api.execute(gql("""
             query getPipelinesForWorkspace($workspace_id: ID!) {
               workspace(id: $workspace_id) {
@@ -28,7 +60,7 @@ class ZenhubService:
                 }
               }
             }
-        """), variable_values={"workspace_id": workspace_id})
+        """), variable_values={"workspace_id": self.workspace_id})
 
         logging.info(f"Searching for pipeline with name {pipeline_name}")
         if data["workspace"]["pipelinesConnection"]["nodes"] is not None:
@@ -39,7 +71,7 @@ class ZenhubService:
 
         return TypeError(f'Could not find pipeline with name {pipeline_name}')
 
-    def get_workspace_id(self, repository_id: int) -> str | TypeError:
+    def get_workspace_id(self, repository_id: int):
         data = self.zenhub_client_gql_api.execute(gql("""
         query getWorkspacesByRepo($repositoryGhId: [Int!]!) {
           repositoriesByGhId(ghIds: $repositoryGhId) {
@@ -54,11 +86,10 @@ class ZenhubService:
         """), variable_values={"repositoryGhId": [repository_id]})
 
         if data["repositoriesByGhId"][0]["workspacesConnection"]["nodes"] is not None:
-            return data["repositoriesByGhId"][0]["workspacesConnection"]["nodes"][0]["id"]
-        else:
-            return TypeError(f"Could not find workspace for repository with ID {repository_id}")
+            workspace_id = data["repositoriesByGhId"][0]["workspacesConnection"]["nodes"][0]["id"]
+            return workspace_id
 
-    def search_issue_by_label(self, pipeline_id: str, label: str):
+    def search_issues_by_label(self, pipeline_id: str, label: str):
         """
         Search for issues in a pipeline (a column in Zenhub) with a specific label
         Args:
