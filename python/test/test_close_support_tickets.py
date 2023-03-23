04efe6b8245e2b7526a07bfa3a0e3018241440df
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-03-23 16:57:04 +0000

    move support scripts to new folder (#2359)

--- python/test/test_close_support_tickets.py
+++ python/test/test_close_support_tickets.py
@@ -1,14 +1,21 @@
+import os
 import unittest
 from unittest.mock import MagicMock, patch
 
-import close_support_tickets
+from python.scripts import close_support_tickets
 
 
 @patch("github.Github.__new__", new=MagicMock)
 class TestSentryProjectsRateLimiting(unittest.TestCase):
 
+    @patch.dict(os.environ, {"ADMIN_GITHUB_TOKEN": "token"})
     def test_main_smoke_test(self):
         close_support_tickets.main()
+
+    def test_main_returns_error_when_no_token_provided(self):
+        self.assertRaises(
+            ValueError, close_support_tickets.main)
+
 
 if __name__ == "__main__":
     unittest.main()
