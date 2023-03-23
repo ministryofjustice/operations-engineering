e2437522b390901ea4a96ee9b60062467dfbc0b4
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-03-23 12:32:25 +0000

    New folder structutre and port over archive a repository script

--- python/test/test_archive_repositories.py
+++ python/test/test_archive_repositories.py
@@ -1,17 +1,23 @@
+import os
 import unittest
 from unittest.mock import patch
 
 from github import Github
 
-import archive_repositories
+from python.scripts import archive_repositories
 
 
 @patch.object(Github, "__new__")
 class TestArchiveRepositories(unittest.TestCase):
 
+    @patch.dict(os.environ, {"ADMIN_GITHUB_TOKEN": "token"})
     @patch("sys.argv", ["", "test"])
     def test_main_smoke_test(self, mock_github):
         archive_repositories.main()
+
+    def test_main_returns_error_when_no_token_provided(self, mock_github):
+        self.assertRaises(
+            ValueError, archive_repositories.main)
 
 
 if __name__ == "__main__":
