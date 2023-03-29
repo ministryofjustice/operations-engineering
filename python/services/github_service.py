056b82605341e080474136c26f4b068a337d6beb
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-03-29 16:49:45 +0100

    feat(github): add public and private method to assign support requests
    the following methods will grab all issues with the support tag and assign them to the user who created them. This functionality isn't new, the methods are.

--- python/services/github_service.py
+++ python/services/github_service.py
@@ -84,6 +84,37 @@ class GithubService:
                 and issue.state == "open"
                 and grace_period < datetime.now())
 
+    def assign_support_issues_to_self(self, repository_name, org_name, tag: str) -> list[any]:
+        """
+            Assigns issues with a specific tag to the user who created the issue.
+            This is used to assign support issues to the user who created the issue.
+
+            :param repository_name: The name of the repository to assign issues in.
+            :param org_name: The name of the organisation to assign issues in.
+            :param tag: The tag to search for.
+        """
+        support_issues = [
+            issue
+            for issue in self.__get_open_issues_from_repo(f"{org_name}/{repository_name}")
+            for label in issue.labels
+            if label.name == tag and len(issue.assignees) == 0
+        ]
+
+        for issue in support_issues:
+            issue.edit(assignees=[issue.user.login])
+            if issue.user.login != issue.assignees[0].login:
+                raise ValueError(
+                    f"Failed to assign issue {issue.number} to {issue.user.login} in {repository_name}")
+
+        return support_issues
+
+    @retries_github_rate_limit_exception_at_next_reset_once
+    def __get_open_issues_from_repo(self, repository_name: str) -> list[Issue]:
+        required_state = "open"
+        repo = self.github_client_core_api.get_repo(repository_name)
+
+        return repo.get_issues(state=required_state) or []
+
     @retries_github_rate_limit_exception_at_next_reset_once
     def create_an_access_removed_issue_for_user_in_repository(self, user_name: str, repository_name: str) -> None:
         logging.info(
