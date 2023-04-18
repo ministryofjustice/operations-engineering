fa394528af6740952554e98d53d30db6ad7071d2
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-04-18 19:22:45 +0100

    Add the move users to team GH action and script (#2524)

---
+++ python/scripts/move_users_to_teams.py
@@ -0,0 +1,27 @@
+import os
+
+from python.lib.organisation import Organisation
+from python.services.github_service import GithubService
+
+
+def main():
+    print("Start")
+
+    org_name = os.getenv("ORG_NAME")
+    if not org_name:
+        raise ValueError("The env variable ORG_NAME is empty or missing")
+
+    oauth_token = os.getenv("ADMIN_GITHUB_TOKEN")
+    if not oauth_token:
+        raise ValueError(
+            "The env variable ADMIN_GITHUB_TOKEN is empty or missing")
+
+    github_service = GithubService(oauth_token, org_name)
+    org = Organisation(github_service, org_name)
+    org.move_users_to_teams()
+    org.close_expired_issues()
+    print("Finished")
+
+
+if __name__ == "__main__":
+    main()
