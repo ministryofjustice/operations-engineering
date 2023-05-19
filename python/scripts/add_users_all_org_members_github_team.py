f79415281cf1133041a541cf6b692f1e1552795a
Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
2023-05-19 12:55:19 +0100

    ♻️ Refactor `add_users_all_org_members_github_team` script to handle multiple organizations (#2760)

--- python/scripts/add_users_all_org_members_github_team.py
+++ python/scripts/add_users_all_org_members_github_team.py
@@ -1,55 +1,47 @@
 import os
-import sys
-import time
-import traceback
 
 from python.services.github_service import GithubService
 
+MINISTRYOFJUSTICE_GITHUB_ORGANIZATION_NAME = "ministryofjustice"
+# Contains a base set of permissions for all users in MoJ
+MINISTRYOFJUSTICE_GITHUB_ORGANIZATION_BASE_TEAM_NAME = "all-org-members"
 
-def print_stack_trace(message):
-    """Print a stack trace when an exception occurs
-
-    Args:
-        message (string): A message to print when exception occurs
-    """
-    print(message)
-    try:
-        exc_info = sys.exc_info()
-    finally:
-        traceback.print_exception(*exc_info)
-        del exc_info
+MOJ_ANALYTICAL_SERVICES_GITHUB_ORGANIZATION_NAME = "moj-analytical-services"
+# Contains a base set of permissions for all users in MoJAS
+MOJ_ANALYTICAL_SERVICES_GITHUB_ORGANIZATION_BASE_TEAM_NAME = "everyone"
 
 
-def run(github_service: GithubService):
-    """A function for the main functionality of the script"""
+def get_environment_variables() -> tuple[str, str]:
+    github_token = os.getenv("ADMIN_GITHUB_TOKEN")
+    if not github_token:
+        raise ValueError(
+            "The env variable ADMIN_GITHUB_TOKEN is empty or missing")
 
-    try:
-        gh = github_service.github_client_core_api
-        org = gh.get_organization("ministryofjustice")
-        team_id = github_service.get_team_id_from_team_name("all-org-members")
-        gh_team = org.get_team(team_id)
-        all_org_members = gh_team.get_members()
-        org_members = org.get_members()
-        for member in org_members:
-            if member not in all_org_members:
-                print(member)
-                gh_team.add_membership(member)
-                # Delay for GH API
-                time.sleep(3)
-    except Exception:
-        message = "Warning: Exception in Run()"
-        print_stack_trace(message)
+    github_organization_name = os.getenv("GITHUB_ORGANIZATION_NAME")
+    if not github_organization_name:
+        raise ValueError(
+            "The env variable GITHUB_ORGANIZATION is empty or missing")
+
+    return github_token, github_organization_name
+
+
+def get_config_for_organization(github_organization_name: str) -> tuple[str, str] | ValueError:
+    if github_organization_name == MINISTRYOFJUSTICE_GITHUB_ORGANIZATION_NAME:
+        return MINISTRYOFJUSTICE_GITHUB_ORGANIZATION_NAME, MINISTRYOFJUSTICE_GITHUB_ORGANIZATION_BASE_TEAM_NAME
+
+    if github_organization_name == MOJ_ANALYTICAL_SERVICES_GITHUB_ORGANIZATION_NAME:
+        return MOJ_ANALYTICAL_SERVICES_GITHUB_ORGANIZATION_NAME, MOJ_ANALYTICAL_SERVICES_GITHUB_ORGANIZATION_BASE_TEAM_NAME
+
+    raise ValueError(
+        f"Unsupported Github Organization Name [{github_organization_name}]")
 
 
 def main():
-    org_token = os.getenv("ADMIN_GITHUB_TOKEN")
-    if not org_token:
-        raise ValueError(
-            "The env variable ADMIN_GITHUB_TOKEN is empty or missing")
-    github_service = GithubService(org_token, "ministryofjustice")
-    print("Start")
-    run(github_service)
-    print("Finished")
+    github_token, github_organization_name = get_environment_variables()
+    organization_name, organization_team_name = get_config_for_organization(
+        github_organization_name)
+    GithubService(github_token, organization_name).add_all_users_to_team(
+        organization_team_name)
 
 
 if __name__ == "__main__":
--- python/services/github_service.py
+++ python/services/github_service.py
@@ -199,6 +199,34 @@ class GithubService:
         self.github_client_core_api.get_organization(
             self.organisation_name).get_team(team_id).add_membership(user)
 
+    def add_all_users_to_team(self, team_name: str) -> None:
+        logging.info(f"Adding all users to {team_name}")
+        team_id = self.get_team_id_from_team_name(team_name)
+        all_users = self.__get_all_users()
+        existing_users_in_team = self.__get_users_from_team(team_id)
+
+        for user in all_users:
+            if user not in existing_users_in_team:
+                self.__add_user_to_team(user["name"], team_id)
+
+    @retries_github_rate_limit_exception_at_next_reset_once
+    def __get_all_users(self) -> list:
+        logging.info("Getting all organization members")
+        return self.github_client_core_api.get_organization(self.organisation_name).get_members() or []
+
+    @retries_github_rate_limit_exception_at_next_reset_once
+    def __add_user_to_team(self, user_name: str, team_id: int) -> None:
+        logging.info(f"Adding user {user_name} to team {team_id}")
+        user = self.github_client_core_api.get_user(user_name)
+        self.github_client_core_api.get_organization(
+            self.organisation_name).get_team(team_id).add_membership(user)
+
+    @retries_github_rate_limit_exception_at_next_reset_once
+    def __get_users_from_team(self, team_id: int) -> list:
+        logging.info(f"Getting all named users for team {team_id}")
+        return self.github_client_core_api.get_organization(self.organisation_name).get_team(
+            team_id).get_members() or []
+
     @retries_github_rate_limit_exception_at_next_reset_once
     def create_new_team_with_repository(self, team_name: str, repository_name: str) -> None:
         logging.info(
