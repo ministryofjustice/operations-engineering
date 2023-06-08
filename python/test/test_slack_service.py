d45cb9bf3e6f5524f3ab169524e7c594dd12300d
connormaglynn <connor.glynn@digital.justice.gov.uk>
2023-06-08 11:02:40 +0100

    ♻️Refactor Slack action into button links

--- python/test/test_slack_service.py
+++ python/test/test_slack_service.py
@@ -24,10 +24,11 @@ class TestSlackServiceSendErrorUsageAlertToOperationsEngineering(unittest.TestCa
         mock_slack_client.return_value.chat_postMessage.assert_called_with(channel='C033QBE511V', mrkdown=True, blocks=[
             {'type': 'section', 'text': {'type': 'mrkdwn',
                                          'text': ':sentry: *Sentry Errors Usage Alert :warning:*\n- Usage threshold: 400%\n- Period: 0 day\n- Max usage for period: 2 Errors\n- Errors consumed over period: 1\n- Percentage consumed: 300%'}},
-            {'type': 'divider'}, {'type': 'actions', 'elements': [{'type': 'button', 'text': {'type': 'plain_text',
-                                                                                              'text': ':sentry: Error Usage For Period',
-                                                                                              'emoji': True},
-                                                                   'value': 'https://ministryofjustice.sentry.io/stats/?dataCategory=errors&statsPeriod=0d'}]}])
+            {'type': 'divider'},
+            {'type': 'section', 'text': {'type': 'mrkdwn', 'text': ' Check Sentry for Spikes :eyes:'},
+             'accessory': {'type': 'button',
+                           'text': {'type': 'plain_text', 'text': ':sentry: Error Usage For Period', 'emoji': True},
+                           'url': 'https://ministryofjustice.sentry.io/stats/?dataCategory=errors&statsPeriod=0d'}}])
 
 
 @patch("slack_sdk.WebClient.__new__")
@@ -39,10 +40,12 @@ class TestSlackServiceSendTransactionUsageAlertToOperationsEngineering(unittest.
         mock_slack_client.return_value.chat_postMessage.assert_called_with(channel='C033QBE511V', mrkdown=True, blocks=[
             {'type': 'section', 'text': {'type': 'mrkdwn',
                                          'text': ':sentry: *Sentry Transactions Usage Alert :warning:*\n- Usage threshold: 400%\n- Period: 0 day\n- Max usage for period: 2 Transactions\n- Transactions consumed over period: 1\n- Percentage consumed: 300%'}},
-            {'type': 'divider'}, {'type': 'actions', 'elements': [{'type': 'button', 'text': {'type': 'plain_text',
-                                                                                              'text': ':sentry: Transaction Usage For Period',
-                                                                                              'emoji': True},
-                                                                   'value': 'https://ministryofjustice.sentry.io/stats/?dataCategory=transactions&statsPeriod=0d'}]}])
+            {'type': 'divider'},
+            {'type': 'section', 'text': {'type': 'mrkdwn', 'text': ' Check Sentry for Spikes :eyes:'},
+             'accessory': {'type': 'button',
+                           'text': {'type': 'plain_text', 'text': ':sentry: Transaction Usage For Period',
+                                    'emoji': True},
+                           'url': 'https://ministryofjustice.sentry.io/stats/?dataCategory=transactions&statsPeriod=0d'}}])
 
 
 if __name__ == "__main__":
