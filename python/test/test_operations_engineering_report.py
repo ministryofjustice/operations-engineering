526e795f7e7db7852cabd33e16bb32f280a064f0
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-06-15 15:18:16 +0100

    Remove encryption tests
    The method to encrypt no longer exists, so the test is pointless

--- python/test/test_operations_engineering_report.py
+++ python/test/test_operations_engineering_report.py
@@ -34,11 +34,9 @@ class TestOrganisationStandardsReport(unittest.TestCase):
         self.url = 'https://test.com'
         self.endpoint = 'test'
         self.api_key = 'secret'
-        # Fake test hex key
-        self.encryption_key = "5a5263317379674b64564a62516856656a504a6e4b386a4959544236626f44365631516531534a4c3165773d"
 
         self.report_service = OperationsEngineeringReportsService(
-            self.url, self.endpoint, self.api_key, self.encryption_key)
+            self.url, self.endpoint, self.api_key)
 
     def test_success_init(self):
         self.assertIsNotNone(self.report_service)
@@ -50,9 +48,9 @@ class TestOrganisationStandardsReport(unittest.TestCase):
             OperationsEngineeringReportsService(endpoint, api_key)
 
     @requests_mock.Mocker()
-    def test_success_override_repository_standards(self, m):
+    def test_success_override_repository_standards(self, mock):
         url = f"{self.url}/{self.endpoint}"
-        m.post(url, status_code=http.HTTPStatus.OK)
+        mock.post(url, status_code=http.HTTPStatus.OK)
         self.report_service.override_repository_standards_reports(
             self.mock_report)
 
@@ -76,15 +74,6 @@ class TestOrganisationStandardsReport(unittest.TestCase):
         m.post(url, status_code=http.HTTPStatus.OK)
         self.assertRaises(
             TypeError, self.report_service.override_repository_standards_reports('bad_payload'))
-
-    @requests_mock.Mocker()
-    def test_bad_encrypt_override_repository_standards(self, m):
-        url = f"{self.url}/{self.endpoint}"
-        m.post(url, status_code=http.HTTPStatus.OK)
-        bad_key = OperationsEngineeringReportsService(
-            self.url, self.endpoint, self.api_key, 1)
-        self.assertRaises(
-            TypeError, bad_key.override_repository_standards_reports, self.mock_report)
 
     @requests_mock.Mocker()
     def test_bad_report_override_repository_standards(self, m):
