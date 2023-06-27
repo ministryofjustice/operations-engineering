e5aa2fc76781a19c6dc936e49bdc65b7502d1bcd
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-06-27 12:53:04 +0100

    Update test_auth0_service.py

--- python/test/test_auth0_service.py
+++ python/test/test_auth0_service.py
@@ -66,14 +66,14 @@ class TestAuth0Service(unittest.TestCase):
 
     def test_delete_user(self):
         self.response.status_code = RESPONSE_NO_CONTENT
-        self.auth0.delete = MagicMock(return_value=self.response)
+        self.auth0._delete = MagicMock(return_value=self.response)
 
         # Call the delete_user method with the given user ID
         user_id = "user123"
         response = self.auth0.delete_user(user_id)
 
         # Check that the delete method was called with the correct URL
-        self.auth0.delete.assert_called_once_with("api/v2/users/user123")
+        self.auth0._delete.assert_called_once_with("api/v2/users/user123")
 
         # Check that the delete_user method returns the mock response object
         self.assertEqual(response, self.response.status_code)
@@ -83,13 +83,13 @@ class TestAuth0Service(unittest.TestCase):
         err_response.status_code = 500
 
         # Set up the mock Auth0 client object
-        self.auth0.delete = MagicMock(return_value=err_response)
+        self.auth0._delete = MagicMock(return_value=err_response)
 
         # Call the delete_user method with the same user ID again
         response = self.auth0.delete_user(user_id)
 
         # Check that the delete method was called with the correct URL
-        self.auth0.delete.assert_called_with("api/v2/users/user123")
+        self.auth0._delete.assert_called_with("api/v2/users/user123")
 
         # Check that the delete_user method returns the mock error response
         self.assertEqual(response, err_response.status_code)
@@ -106,8 +106,8 @@ class TestAuth0Service(unittest.TestCase):
 
     def test_get_users(self):
         self.response.json.return_value = self.users
-        self.auth0.get = MagicMock(return_value=self.response)
-        users = self.auth0.get_users()
+        self.auth0._get = MagicMock(return_value=self.response)
+        users = self.auth0._get_users()
         self.assertEqual(users, self.users)
 
     def test_get_users_with_pages(self):
@@ -115,14 +115,14 @@ class TestAuth0Service(unittest.TestCase):
         small_list = self.users
         joint_list = small_list + large_list
         self.response.json.side_effect = [large_list, small_list]
-        self.auth0.get = MagicMock(return_value=self.response)
-        users = self.auth0.get_users(page=3)
+        self.auth0._get = MagicMock(return_value=self.response)
+        users = self.auth0._get_users(page=3)
         self.assertEqual(users, joint_list)
 
     def test_get_users_when_error(self):
         self.response.status_code = RESPONSE_NO_CONTENT
-        self.auth0.get = MagicMock(return_value=self.response)
-        users = self.auth0.get_users()
+        self.auth0._get = MagicMock(return_value=self.response)
+        users = self.auth0._get_users()
         self.assertEqual(users, [])
 
     def test_get_inactive_users_when_user_is_active(self):
@@ -133,34 +133,34 @@ class TestAuth0Service(unittest.TestCase):
         the_users[0].update(
             {"last_login": f"{fake_date:%Y-%m-%dT%H:%M:%S.%fZ}"})
         self.response.json.return_value = the_users
-        self.auth0.get = MagicMock(return_value=self.response)
+        self.auth0._get = MagicMock(return_value=self.response)
         users = self.auth0.get_inactive_users()
         self.assertEqual(users, [])
 
     def test_get_inactive_users(self):
         self.datetime.utcnow.return_value = self.fake_date
         self.response.json.return_value = self.expired_users
-        self.auth0.get = MagicMock(return_value=self.response)
+        self.auth0._get = MagicMock(return_value=self.response)
         users = self.auth0.get_inactive_users()
         self.assertEqual(users, self.expired_users)
 
     def test_get_inactive_users_when_user_never_logged_in(self):
         self.response.json.return_value = self.users_with_id
-        self.auth0.get = MagicMock(return_value=self.response)
+        self.auth0._get = MagicMock(return_value=self.response)
         users = self.auth0.get_inactive_users()
         self.assertEqual(users, self.users_with_id)
 
     def test_get_active_users_when_user_has_expired(self):
         self.datetime.utcnow.return_value = self.fake_date
         self.response.json.return_value = self.expired_users
-        self.auth0.get = MagicMock(return_value=self.response)
+        self.auth0._get = MagicMock(return_value=self.response)
         users = self.auth0.get_active_users()
         self.assertEqual(users, [])
 
     def test_get_active_users_when_user_never_logged_in(self):
         self.datetime.utcnow.return_value = self.fake_date
         self.response.json.return_value = self.users_with_id
-        self.auth0.get = MagicMock(return_value=self.response)
+        self.auth0._get = MagicMock(return_value=self.response)
         users = self.auth0.get_active_users()
         self.assertEqual(users, [])
 
@@ -169,7 +169,7 @@ class TestAuth0Service(unittest.TestCase):
         fake_date = datetime(2050, 1, 1)
         datetime.utcnow.return_value = fake_date
         self.response.json.return_value = self.active_users
-        self.auth0.get = MagicMock(return_value=self.response)
+        self.auth0._get = MagicMock(return_value=self.response)
         users = self.auth0.get_active_users()
         self.assertEqual(users, self.active_users)
 
@@ -191,13 +191,13 @@ class TestAuth0Service(unittest.TestCase):
     # pylint: disable=W0212
     def test_get(self):
         self.auth0._make_request = MagicMock(return_value=self.response)
-        res = self.auth0.get(self.endpoint)
+        res = self.auth0._get(self.endpoint)
         self.assertEqual(res.status_code, self.response.status_code)
 
     # pylint: disable=W0212
     def test_delete(self):
         self.auth0._make_request = MagicMock(return_value=self.response)
-        res = self.auth0.delete(self.endpoint)
+        res = self.auth0._delete(self.endpoint)
         self.assertEqual(res.status_code, self.response.status_code)
 
 
