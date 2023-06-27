b4f3ba4ded09cd863b9f12587088506354247c70
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-06-27 12:31:20 +0100

    Create constants.py

---
+++ python/config/constants.py
@@ -0,0 +1,7 @@
+# class Constants:
+MINISTRY_OF_JUSTICE = "ministryofjustice"
+MOJ_ANALYTICAL_SERVICES = "moj-analytical-services"
+MISSING_EMAIL_ADDRESS = "-"
+NO_ACTIVITY = "no activity"
+RESPONSE_OKAY = 200
+RESPONSE_NO_CONTENT = 204
