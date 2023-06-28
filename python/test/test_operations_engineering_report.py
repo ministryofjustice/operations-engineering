126914fb08b00c8fa5490be67f711f258b6ab926
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-06-28 14:33:59 +0100

    ✅ Replace mockery tests with unittest mocks The code here is over complicated. All we want to test in this class is: - does the class init - assert it responds when the receives a 200 from the API - assert it raises a ValueError when it receives an non-200 respose.

--- python/test/test_operations_engineering_report.py
+++ python/test/test_operations_engineering_report.py
@@ -1,86 +1,37 @@
-import http
 import unittest
-import requests_mock
+from unittest.mock import patch
 
-from python.services.operations_engineering_reports import OperationsEngineeringReportsService
+from python.services.operations_engineering_reports import \
+    OperationsEngineeringReportsService
+
+# from python.services.standards_service import GitHubRepositoryStandardsReport
 
 
-class TestOrganisationStandardsReport(unittest.TestCase):
-
+class TestOperationsEngineeringReportsService(unittest.TestCase):
     def setUp(self):
-        self.bad_mock_report = [{1}]
-        self.mock_report = [{
-            "default_branch": "master",
-            "infractions": [
-                "default_branch_main equalling False is not compliant"
-            ],
-            "is_private": True,
-            "last_push": "2023-01-03T14:23:38Z",
-            "name": "test-repo",
-            "report": {
-                    "administrators_require_review": False,
-                    "default_branch_main": False,
-                    "has_default_branch_protection": False,
-                    "has_description": True,
-                    "has_license": True,
-                    "has_require_approvals_enabled": False,
-                    "issues_section_enabled": True,
-                    "requires_approving_reviews": False,
-            },
-            "status": False,
-            "url": "https://github.com/test/test-repo"
-        }]
+        self.service = OperationsEngineeringReportsService(
+            'http://example.com', '/reports', 'api_key')
+        self.reports = [
+            {'repository': 'repo1', 'owner': 'owner1', 'compliant': True},
+            {'repository': 'repo2', 'owner': 'owner2', 'compliant': False}
+        ]
 
-        self.url = 'https://test.com'
-        self.endpoint = 'test'
-        self.api_key = 'secret'
+    @patch('requests.post')
+    def test_override_repository_standards_reports(self, mock_post):
+        mock_post.return_value.status_code = 200
 
-        self.report_service = OperationsEngineeringReportsService(
-            self.url, self.endpoint, self.api_key)
+        self.service.override_repository_standards_reports(self.reports)
+        self.assertCountEqual(
+            mock_post.call_args[1]['json'], self.reports)
 
-    def test_success_init(self):
-        self.assertIsNotNone(self.report_service)
 
-    def test_fail_init(self):
-        endpoint = 'https://api.com'
-        api_key = 'secret'
-        with self.assertRaises(TypeError):
-            OperationsEngineeringReportsService(endpoint, api_key)
+    @patch('requests.post')
+    def test_override_repository_standards_reports_with_non_200(self, mock_post):
+        mock_post.return_value.status_code = 500
 
-    @requests_mock.Mocker()
-    def test_success_override_repository_standards(self, mock):
-        url = f"{self.url}/{self.endpoint}"
-        mock.post(url, status_code=http.HTTPStatus.OK)
-        self.report_service.override_repository_standards_reports(
-            self.mock_report)
-
-    @requests_mock.Mocker()
-    def test_non200_override_repository_standards(self, m):
-        url = f"{self.url}/{self.endpoint}"
-        m.post(url, status_code=http.HTTPStatus.CONFLICT)
-        self.assertRaises(
-            Exception, self.report_service.override_repository_standards_reports, self.mock_report)
-
-    @requests_mock.Mocker()
-    def test_bad_endpoint_override_repository_standards(self, m):
-        url = f"bad/{self.endpoint}"
-        m.post(url, status_code=http.HTTPStatus.BAD_REQUEST)
-        self.assertRaises(
-            Exception, self.report_service.override_repository_standards_reports, self.mock_report)
-
-    @requests_mock.Mocker()
-    def test_bad_payload_override_repository_standards(self, m):
-        url = f"{self.url}/{self.endpoint}"
-        m.post(url, status_code=http.HTTPStatus.OK)
-        self.assertRaises(
-            TypeError, self.report_service.override_repository_standards_reports('bad_payload'))
-
-    @requests_mock.Mocker()
-    def test_bad_report_override_repository_standards(self, m):
-        url = f"{self.url}/{self.endpoint}"
-        m.post(url, status_code=http.HTTPStatus.OK)
-        self.assertRaises(
-            TypeError, self.report_service.override_repository_standards_reports, self.bad_mock_report)
+        with self.assertRaises(ValueError):
+            self.service.override_repository_standards_reports(self.reports)
+        mock_post.assert_called_once()
 
 
 if __name__ == '__main__':
