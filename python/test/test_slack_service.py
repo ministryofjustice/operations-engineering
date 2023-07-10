b04fa666e07c8c7a5ee056fda1e8d461ea930504
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-07-10 13:08:34 +0100

    Add inactive user unit tests to test_slack_service

--- python/test/test_slack_service.py
+++ python/test/test_slack_service.py
@@ -9,6 +9,8 @@ START_TIME = "2023-06-08T00:00:00Z"
 END_TIME = "2023-06-09T00:00:00Z"
 DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
+# pylint: disable=W0212, W0221
+
 
 @patch("slack_sdk.WebClient.__new__")
 class TestSlackServiceInit(unittest.TestCase):
@@ -122,6 +124,90 @@ class SendUndeliveredEmailAlertToOperationsEngineering(unittest.TestCase):
         SlackService("").send_undelivered_email_alert_to_operations_engineering(
             email_address, "some-org")
         mock_slack_client.return_value.chat_postMessage.assert_called_with(
+            channel="C033QBE511V",
+            mrkdown=True,
+            blocks=[
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": '*Dormants Users Automation*\nUndelivered emails for some-org GitHub Organisation:\n[\'some-user1@domain.com\', \'some-user2@domain.com\', \'some-user3@domain.com\']\nRemove these users manually'
+                    }
+                }
+            ]
+        )
+
+class TestSlackService(unittest.TestCase):
+
+    @patch("slack_sdk.WebClient.__new__")
+    def setUp(self, mock_slack_client):
+        self.message = "some-message"
+        self.blocks = [
+            {
+                "type": "section",
+                "text": {
+                        "type": "mrkdwn",
+                        "text": self.message
+                }
+            }
+        ]
+        self.mock_slack_client = mock_slack_client
+        self.slack_service = SlackService("")
+
+    def test_create_block_with_message(self):
+        self.assertEqual(
+            self.blocks, self.slack_service._create_block_with_message(self.message))
+
+    def test_send_alert_to_operations_engineering(self):
+        self.slack_service._send_alert_to_operations_engineering(self.blocks)
+        self.mock_slack_client.return_value.chat_postMessage.assert_called_with(
+            channel="C033QBE511V",
+            mrkdown=True,
+            blocks=self.blocks
+        )
+
+    def test_send_unknown_users_slack_message(self):
+        self.slack_service.send_unknown_users_slack_message(
+            ["some-user1", "some-user2", "some-user3"])
+        self.mock_slack_client.return_value.chat_postMessage.assert_called_with(
+            channel="C033QBE511V",
+            mrkdown=True,
+            blocks=[
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": '*Dormants Users Automation*\nRemove these users from the Dormants Users allow list:\n[\'some-user1\', \'some-user2\', \'some-user3\']'
+                    }
+                }
+            ]
+        )
+
+    def test_send_remove_users_slack_message(self):
+        self.slack_service.send_remove_users_slack_message(
+            3, "some-org")
+        self.mock_slack_client.return_value.chat_postMessage.assert_called_with(
+            channel="C033QBE511V",
+            mrkdown=True,
+            blocks=[
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": '*Dormants Users Automation*\nRemoved 3 users from the some-org GitHub Organisation.\nSee the GH Action for more info: https://github.com/ministryofjustice/operations-engineering'
+                    }
+                }
+            ]
+        )
+
+    def test_send_undelivered_emails_slack_message(self):
+        email_address = ["some-user1@domain.com",
+                         "some-user2@domain.com",
+                         "some-user3@domain.com"
+                         ]
+        self.slack_service.send_undelivered_emails_slack_message(
+            email_address, "some-org")
+        self.mock_slack_client.return_value.chat_postMessage.assert_called_with(
             channel="C033QBE511V",
             mrkdown=True,
             blocks=[
