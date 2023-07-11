d165394a391d324bec6416a661dcc896ac41c31e
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-07-11 14:50:28 +0100

    Add the auth0_delete_inactive_users script

---
+++ python/scripts/auth0_delete_inactive_users.py
@@ -0,0 +1,17 @@
+import os
+from python.services.auth0_service import Auth0Service
+
+def delete_inactive_users(auth0_client: Auth0Service):
+    # Delete all users over 180 days inactive
+    auth0_list = auth0_client.get_inactive_users(days_inactive=180)
+
+    for user in auth0_list:
+        auth0_client.delete_user(user["user_id"])
+
+def main():
+    auth0_client = Auth0Service(os.getenv("AUTH0_CLIENT_SECRET"), os.getenv(
+        "AUTH0_CLIENT_ID"), os.getenv("AUTH0_DOMAIN"), "client_credentials")
+    delete_inactive_users(auth0_client)
+
+if __name__ == "__main__":
+    main()
