98b772636cc2cbcdcb08b370b403ff377fb33564
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-09-18 16:58:03 +0100

    Add the extra checks

--- python/scripts/unowned_repositories.py
+++ python/scripts/unowned_repositories.py
@@ -14,33 +14,91 @@ def get_cli_arguments() -> tuple[str, str, str] | ValueError:
     slack_token = sys.argv[3]
     return organisation_name, admin_github_token, slack_token
 
-
-def get_repos_without_maintainers(github_service: GithubService):
-    repos_without_maintainers = []
+def check_the_repositories(github_service: GithubService):
+    repositories_without_maintainers = []
+    repositories_with_no_associations = []
+    repositories_without_admin_team = []
+    repositories_without_any_admin = []
+    repositories_without_any_teams = []
+    allowed_teams = ["organisation-security-auditor", "all-org-members"]
 
     for repository in github_service.fetch_all_repositories_in_org():
         repository_name = repository['node']['name']
-        has_maintainer_collaborators = False
-        has_maintainer_teams = False
+        has_maintainer_collaborator = False
+        has_admin_collaborator = False
+        has_maintainer_team = False
+        has_admin_team = False
+        repository_team_names = []
 
         # Check for teams with maintain or admin permissions
-        for team in github_service.get_repository_teams(repository_name):
-            if team.permission in ['admin', 'maintain']:
-                has_maintainer_teams = True
+        repository_teams = github_service.get_repository_teams(repository_name)
+        for team in repository_teams:
+            repository_team_names.append(team.name)
+
+            if team.permission == 'admin':
+                has_maintainer_team = True
+                has_admin_team = True
                 break
+
+            if team.permission == 'maintain':
+                has_maintainer_team = True
 
         # Check for outside collaborators with maintain or admin permissions
-        for collaborator in github_service.get_repository_collaborators(repository_name):
-            if collaborator.permissions.admin == True or collaborator.permissions.maintain == True:
-                has_maintainer_collaborators = True
+        repository_collaborators = github_service.get_repository_collaborators(repository_name)
+        for collaborator in repository_collaborators:
+            if collaborator.permissions.admin == True:
+                has_maintainer_collaborator = True
+                has_admin_collaborator = True
                 break
 
-        if has_maintainer_teams == False and has_maintainer_collaborators == False:
-            repos_without_maintainers.append(repository_name)
+            if collaborator.permissions.maintain == True:
+                has_maintainer_collaborator = True
 
-    if len(repos_without_maintainers) > 0:
+        for allowed_team in allowed_teams:
+            repository_team_names.remove(allowed_team)
+
+        # Determine if repository has any permission problems via team and collaborators
+
+        if has_maintainer_team == False and has_maintainer_collaborator == False:
+            repositories_without_maintainers.append(repository_name)
+
+        if len(repository_collaborators) == 0 and len(repository_teams) == 0:
+            repositories_with_no_associations.append(repository_name)
+
+        if has_admin_team == False:
+            repositories_without_admin_team.append(repository_name)
+
+        if has_admin_team == False and has_admin_collaborator == False:
+            repositories_without_any_admin.append(repository_name)
+
+        if len(repository_team_names) == 0:
+            repositories_without_any_teams.append(repository_name)
+
+    # Print the repositories that have permission problems
+
+    if len(repositories_without_maintainers) > 0:
         logger.warning("Repositories without any maintainers (team or individual):")
-        for repository_name in repos_without_maintainers:
+        for repository_name in repositories_without_maintainers:
+            logger.warning(repository_name)
+
+    if len(repositories_with_no_associations) > 0:
+        logger.warning("Repositories with no teams or users attached:")
+        for repository_name in repositories_with_no_associations:
+            logger.warning(repository_name)
+
+    if len(repositories_without_admin_team) > 0:
+        logger.warning("Repositories without an admin team:")
+        for repository_name in repositories_without_admin_team:
+            logger.warning(repository_name)
+
+    if len(repositories_without_any_admin) > 0:
+        logger.warning("Repositories without any admin team or admin collaborator:")
+        for repository_name in repositories_without_any_admin:
+            logger.warning(repository_name)
+
+    if len(repositories_without_any_teams) > 0:
+        logger.warning("Repositories without any teams:")
+        for repository_name in repositories_without_any_teams:
             logger.warning(repository_name)
 
 
@@ -50,7 +108,7 @@ def main():
     organisation_name, admin_github_token, slack_token = get_cli_arguments()
     slack_service = SlackService(slack_token)
     github_service = GithubService(admin_github_token, organisation_name)
-    get_repos_without_maintainers(github_service)
+    check_the_repositories(github_service)
 
     logging.info("Finished")
 
