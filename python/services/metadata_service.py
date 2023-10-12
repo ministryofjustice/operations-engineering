23147f86e66c870ea4ea31b8cae85d174b073380
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2023-10-12 11:21:21 +0100

    Reorganised functions for better clarity

--- python/services/metadata_service.py
+++ python/services/metadata_service.py
@@ -6,22 +6,23 @@ class MetadataService:
     def __init__(self, api_url: str, api_token: str):
         self.api_url = api_url
         self.api_token = api_token
-
-    def get_existing_slack_users(self):
+        
+    def get_acceptable_slack_users(self):
         return [
             {"username": "sam.pepper"}
         ]
 
-        # try:
-        #     response = requests.get(f"{self.api_url}/slack_users", headers={"Authorization": f"Bearer {self.api_token}"})
-        #     if response.status_code == 200:
-        #         return response.json()
-        #     else:
-        #         logging.error("Error fetching existing Slack usernames: %s", response.content)
-        #         return []
-        # except Exception as e:
-        #     logging.error("An error occurred while fetching existing Slack usernames: %s", str(e))
-        #     return []
+    def get_existing_slack_users(self):
+        try:
+            response = requests.get(f"{self.api_url}/slack_users", headers={"Authorization": f"Bearer {self.api_token}"})
+            if response.status_code == 200:
+                return response.json()
+            else:
+                logging.error("Error fetching existing Slack usernames: %s", response.content)
+                return []
+        except Exception as e:
+            logging.error("An error occurred while fetching existing Slack usernames: %s", str(e))
+            return []
 
     def add_new_slack_usernames(self, usernames_to_add: list[dict]):
         payload = {"usernames_to_add": usernames_to_add}
