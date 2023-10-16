54680a74b767e4ec43fbdf2701446d1a76de8760
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2023-10-16 13:09:48 +0100

    Added stubbed methods and funcitonality for combining data

--- python/services/metadata_service.py
+++ python/services/metadata_service.py
@@ -11,6 +11,59 @@ class MetadataService:
         return [
             {"username": "sam.pepper"}
         ]
+        
+    def get_acceptable_github_usernames(self):
+        return [
+            {"username": "PepperMoJ"}
+        ]
+        
+    def get_all_slack_usernames_stub(self):
+        """Stubbed method to simulate fetching usernames and user email addresses from the Slack API (for testing purposes)
+
+        Returns:
+            user_data: A list of JSON objects with example data.
+        """
+
+        user_data = [
+            {
+                "username": "user1",
+                "email": "user1@example.com"
+            },
+            {
+                "username": "user2",
+                "email": "user2@example.com"
+            },
+            {
+                "username": "sam.pepper",
+                "email": "sam.pepper@example.com"
+            }
+        ]
+
+        return user_data
+    
+    def get_all_github_usernames_stub(self):
+        """Stubbed method to simulate fetching usernames and user email addresses from the Slack API (for testing purposes)
+
+        Returns:
+            user_data: A list of JSON objects with example data.
+        """
+
+        user_data = [
+            {
+                "username": "user1",
+                "email": "user1@example.com"
+            },
+            {
+                "username": "user2",
+                "email": "user2@example.com"
+            },
+            {
+                "username": "PepperMoJ",
+                "email": "PepperMoJ@example.com"
+            }
+        ]
+
+        return user_data
 
     def get_existing_slack_users(self):
         try:
@@ -27,12 +80,67 @@ class MetadataService:
                 "An error occurred while fetching existing Slack usernames: %s", str(e))
             return []
 
-    def add_new_slack_usernames(self, usernames_to_add: list[dict]):
+    def filter_usernames(self, username_list: list[dict], accepted_username_list: list[dict]):
+        """Filter out all usernames deemed not acceptable.
+
+        Parameters:
+            username_list: Initial list containing all usernames
+            accepted_username_list: A list of acceptable usernames to include in the final list
+
+        Returns:
+            filtered_usernames: A list of filtered usernames
+        """
+
+        accepted_usernames_set = {user["username"]
+                                  for user in accepted_username_list}
+
+        filtered_usernames = [
+            user for user in username_list if user["username"] in accepted_usernames_set
+        ]
+
+        return filtered_usernames
+    
+    def combine_user_data(slack_user_data, github_user_data):
+        """Combine user data from Slack and GitHub based on the email address
+
+        Parameters:
+            slack_user_data: A list of dictionaries containing Slack usernames and emails
+            github_user_data: A list of dictionaries containing GitHub usernames and emails
+
+        Returns:
+            list: A list of dictionaries containing combined user data.
+        """
+    
+        combined_user_data = []
+
+        github_email_to_username = {user['email']: user['username'] for user in github_user_data}
+
+        for slack_user in slack_user_data:
+            email = slack_user['email']
+            if email in github_email_to_username:
+                combined_user = {
+                    "slack_username": slack_user['username'],
+                    "github_username": github_email_to_username[email],
+                    "email": email,
+                }
+                combined_user_data.append(combined_user)
+
+        return combined_user_data
+    
+    def add_new_usernames(self, usernames_to_add: list[dict]):
+        """Send list of new usernames to be added to the Metadata API
+
+        Parameters:
+            usernames_to_add: A list of all new usernames to add
+
+        Returns:
+            list: None
+        """
         payload = {"usernames_to_add": usernames_to_add}
 
         try:
             response = requests.post(
-                f"{self.api_url}/add_slack_users",
+                f"{self.api_url}/add_users",
                 json=payload,
                 headers={"Authorization": f"Bearer {self.api_token}"}
             )
