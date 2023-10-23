10131a2e1c084400f0665afc8bed14e9f577bbb7
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2023-10-23 11:11:10 +0100

    Removed print lines in main script

--- python/scripts/get_github_and_slack_usernames.py
+++ python/scripts/get_github_and_slack_usernames.py
@@ -35,11 +35,9 @@ def main():
     github_service = GithubService(github_token, "ministryofjustice")
 
     # Grabs a list of all slack usernames from the Slack API (using stub for testing purposes)
-    # slack_usernames = metadata_service.get_all_slack_usernames_stub()
     slack_usernames = slack_service.get_all_slack_usernames()
     
     # Grabs a list of all GitHub usernames from the GitHub API (using stub for testing purposes)
-    # github_usernames = metadata_service.get_all_github_usernames_stub()
     github_usernames = github_service.get_github_member_list()
     
     # Grabs a list of acceptable slack usernames (this is for testing purposes)
@@ -47,9 +45,6 @@ def main():
     
     # Grabs a list of acceptable github usernames (this is for testing purposes)
     acceptable_github_users = metadata_service.get_acceptable_github_usernames()
-    
-    print(f"<PEPPER> - Acceptable slack usernames: {acceptable_slack_users}")
-    print(f"<PEPPER> - Acceptable gitHub usernames: {acceptable_github_users}")
 
     # Filter the full list of Slack users to only include some defaults
     filtered_slack_usernames = metadata_service.filter_usernames(
@@ -59,14 +54,9 @@ def main():
     filtered_github_usernames = metadata_service.filter_usernames(
         github_usernames, acceptable_github_users)
     
-    print(f"<PEPPER> - Filtered slack usernames: {filtered_slack_usernames}")
-    print(f"<PEPPER> - Filtered gitHub usernames: {filtered_github_usernames}")
-    
     # Combine both slack and GitHub user lists
     combined_user_information = metadata_service.combine_user_data(filtered_slack_usernames, filtered_github_usernames)
     
-    print(f"<PEPPER> - Combined user information: {combined_user_information}")
-
     # Send the new Slack usernames to the metadata API to be added into the database
     metadata_service.add_new_usernames(combined_user_information)
 
