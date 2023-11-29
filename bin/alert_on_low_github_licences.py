51e9d698563eed377c21b5b7fdb736faf00ced71
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-11-29 14:50:41 +0000

    :sparkles: Add low threshold alarm for GitHub licences

---
+++ bin/alert_on_low_github_licences.py
@@ -0,0 +1,55 @@
+"""
+GitHub License Monitoring Script
+
+Purpose:
+--------
+This script monitors the number of available licenses in a GitHub Enterprise account. 
+It is designed to provide alerts when the license count falls below a 
+specified threshold. 
+"""
+
+import os
+from services.github_service import GithubService
+from services.slack_service import SlackService
+
+def low_theshold_triggered_message(remaining_licences):
+    msg = (
+    f"Hi team 👋, \n\n"
+    f"There are only {remaining_licences} \
+    GitHub licences remaining in the enterprise account. \n\n"
+    f"Please add more licences to the enterprise account. \n\n"
+    f"Thanks, \n\n"
+
+    "The GitHub Licence Alerting Bot"
+    )
+
+    return msg
+
+def alert_on_low_github_licences(threshold=10):
+    github_token = os.environ.get("GITHUB_TOKEN")
+    if github_token is None:
+        print("No GITHUB_TOKEN environment variable set")
+        exit(1)
+    slack_token = os.environ.get("SLACK_TOKEN")
+    if slack_token is None:
+        print("No SLACK_TOKEN environment variable set")
+        exit(1)
+
+    ORGANISATION = "ministryofjustice"
+    ENTERPRISE = "ministry-of-justice-uk"
+    SLACK_CHANNEL = "operations-engineering-alerts"
+
+    remaining_licences = GithubService(github_token, ORGANISATION, ENTERPRISE). \
+    get_remaining_licences()
+    trigger_alert = remaining_licences < threshold
+
+    if trigger_alert:
+        print("Low number of GitHub licences remaining, only %d remaining" \
+            % remaining_licences)
+
+        SlackService(slack_token). \
+        send_message_to_plaintext_channel_name\
+        (low_theshold_triggered_message(remaining_licences), SLACK_CHANNEL)
+        
+if __name__ == "__main__":
+    alert_on_low_github_licences(10)
