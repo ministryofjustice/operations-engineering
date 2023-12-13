91722bdd1123606602136ace5dc75c163070c20c
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-12-13 16:52:55 +0000

    :recycle: Add audit log url to the slack message

--- bin/check_for_github_admins.py
+++ bin/check_for_github_admins.py
@@ -12,13 +12,16 @@ def _calculate_date(in_last_days: int) -> str:
     return date.strftime(timestamp_format)
 
 
-def new_owner_detected_message(new_owner, date_added, added_by, org):
+def new_owner_detected_message(new_owner, date_added, added_by, org, audit_log_url):
     msg = (
         f"Hi all, \n\n"
         f"A new owner has been detected in the `{org}` GitHub org. \n\n"
         f"New owner: {new_owner}\n"
         f"Date added: {date_added}\n"
         f"By who: {added_by}\n\n"
+
+        f"Please review the audit log for more details: {audit_log_url}\n\n"
+
 
         f"Thanks, \n\n"
 
@@ -32,6 +35,8 @@ def check_for_new_organisation_owners(in_last_days: int):
     ORGINISATION = "ministryofjustice"
     SLACK_CHANNEL = "operations-engineering-alerts"
 
+    audit_log_url = f"https://github.com/organizations/{ORGINISATION}/settings/audit-log?q=action%3Aorg.add_member++action%3Aorg.update_member"
+
     admin_token = os.getenv("ADMIN_GITHUB_TOKEN")
     slack_token = os.getenv("ADMIN_SLACK_TOKEN")
 
@@ -44,7 +49,7 @@ def check_for_new_organisation_owners(in_last_days: int):
 
     for change in changes:
         message = new_owner_detected_message(
-            change["userLogin"], change["createdAt"], change["actorLogin"], ORGINISATION)
+            change["userLogin"], change["createdAt"], change["actorLogin"], ORGINISATION, audit_log_url)
         slack.send_message_to_plaintext_channel_name(
             message, SLACK_CHANNEL)
 
