51729f5d33a7b6ed972d0804c9324e0cda79bd3d
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-01-03 17:18:28 +0000

    :wrench: Add required terraform provider with s3 backend

--- terraform/auth0/operations-engineering-test/main.tf
+++ terraform/auth0/operations-engineering-test/main.tf
@@ -1,6 +1,17 @@
-provider "auth0" {
-  domain        = "<domain>"
-  client_id     = "<client-id>"
-  client_secret = "<client-secret>"
-  debug         = "<debug>"
+terraform {
+  backend "s3" {
+    bucket = "cloud-platform-7a8908cb6aea78a104f6ff554c4db90b"
+    region = "eu-west-2"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.31.0"
+    }
+    auth0 = {
+      source  = "auth0/auth0"
+      version = "1.1.1"
+    }
+  }
+  required_version = "~> 1.6"
 }
