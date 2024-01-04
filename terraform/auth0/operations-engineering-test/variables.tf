360384afaa1e2ca7b243901468361f69010b559b
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-01-04 16:49:40 +0000

    :construction: Add terraform plan and auth0 data for interpolation

---
+++ terraform/auth0/operations-engineering-test/variables.tf
@@ -0,0 +1,14 @@
+variable "auth0_domain" {
+  description = "value of the Auth0 domain"
+  default     = ""
+}
+
+variable "auth0_client_id" {
+  description = "value of the Auth0 client id"
+  default     = ""
+}
+
+variable "auth0_client_secret" {
+  description = "value of the Auth0 client secret"
+  default     = ""
+}
