6af60d7c927021eb1879bfcec20af57b8e230f9e
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-01-04 16:51:32 +0000

    :sparkles: Add auth0 provider with details

--- terraform/auth0/operations-engineering-test/main.tf
+++ terraform/auth0/operations-engineering-test/main.tf
@@ -18,3 +18,9 @@ terraform {
   }
   required_version = "~> 1.6"
 }
+
+provider "auth0" {
+  domain        = var.auth0_domain.secret_string
+  client_id     = var.auth0_client_id.secret_string
+  client_secret = var.auth0_client_secret.secret_string
+}
