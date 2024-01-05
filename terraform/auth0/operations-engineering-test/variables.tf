c9ed37dce35dc77fef63ed18eeb6c8bdc0c75705
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-01-05 16:20:05 +0000

    :label: Declare data type

--- terraform/auth0/operations-engineering-test/variables.tf
+++ terraform/auth0/operations-engineering-test/variables.tf
@@ -1,14 +1,17 @@
 variable "auth0_domain" {
   description = "value of the Auth0 domain"
   default     = ""
+  type        = string
 }
 
 variable "auth0_client_id" {
   description = "value of the Auth0 client id"
   default     = ""
+  type        = string
 }
 
 variable "auth0_client_secret" {
   description = "value of the Auth0 client secret"
   default     = ""
+  type        = string
 }
