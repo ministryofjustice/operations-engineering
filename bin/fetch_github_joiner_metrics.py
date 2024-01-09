2f7e136d1c67ee74bd0789bef0db8bf2d1cec2f8
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-01-09 15:54:09 +0000

    New script to fetch new GitHub joiners

---
+++ bin/fetch_github_joiner_metrics.py
@@ -0,0 +1,49 @@
+import os
+from datetime import datetime, timedelta
+from config.constants import MINISTRY_OF_JUSTICE, SLACK_CHANNEL
+
+from services.github_service import GithubService
+from services.slack_service import SlackService
+
+def get_environment_variables() -> tuple:
+    slack_token = os.getenv("ADMIN_SLACK_TOKEN")
+    if not slack_token:
+        raise ValueError(
+            "The env variable ADMIN_SLACK_TOKEN is empty or missing")
+    github_token = os.getenv("ADMIN_GITHUB_TOKEN")
+    if not github_token:
+        raise ValueError(
+            "The env variable ADMIN_GITHUB_TOKEN is empty or missing")
+
+    return slack_token, github_token
+
+
+def _calculate_date(in_last_days: int) -> str:
+    current_date = datetime.now()
+    date = current_date - timedelta(days=in_last_days)
+    timestamp_format = "%Y-%m-%d"
+    return date.strftime(timestamp_format)
+
+
+def main():
+    # audit_log_url = f"https://github.com/organizations/{MINISTRY_OF_JUSTICE}/settings/audit-log?q=action%3Aorg.add_member"
+
+    time_delta_in_days = 7
+    slack_token, github_token = get_environment_variables()
+
+    github_service = GithubService(str(github_token), MINISTRY_OF_JUSTICE)
+    slack = SlackService(str(slack_token))
+    new_members = github_service.check_for_audit_log_new_members(_calculate_date(time_delta_in_days))
+
+    if new_members:
+        for member in new_members:
+            print(f"New member detected: {member['userLogin']}")
+            # message = new_member_detected_message(
+            #     member["userLogin"], member["createdAt"], member["actorLogin"], MINISTRY_OF_JUSTICE, audit_log_url)
+            # slack.send_message_to_plaintext_channel_name(
+            #     message, SLACK_CHANNEL)
+
+
+if __name__ == "__main__":
+    main()
+
