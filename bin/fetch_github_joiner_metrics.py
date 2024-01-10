e4aee68c0532e5100a27143c419e970945d61217
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-01-10 13:36:34 +0000

    Added new message to separate users added by OE and external sources

--- bin/fetch_github_joiner_metrics.py
+++ bin/fetch_github_joiner_metrics.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime, timedelta
-from config.constants import MINISTRY_OF_JUSTICE, SLACK_CHANNEL
+from config.constants import MINISTRY_OF_JUSTICE, SLACK_CHANNEL, OPERATIONS_ENGINEERING_GITHUB_USERNAMES
 
 from services.github_service import GithubService
 from services.slack_service import SlackService
@@ -24,11 +24,14 @@ def _calculate_date(in_last_days: int) -> str:
     timestamp_format = "%Y-%m-%d"
     return date.strftime(timestamp_format)
 
-def new_members_detected_message(new_members, org, audit_log_url, time_delta_in_days):
+def new_members_detected_message(new_members_added_by_oe, new_members_added_externally, org, audit_log_url, time_delta_in_days):
     msg = (
         f"Hi all, \n\n"
-        f"Here are the members added in the last {time_delta_in_days} days within the '{org}' GitHub org. \n\n"
-        f"{new_members}\n\n"
+        f"Here are the new members added in the last {time_delta_in_days} days within the '{org}' GitHub org. \n\n"
+        f"Added by us:\n"
+        f"{new_members_added_by_oe}\n\n"
+        f"Added externally:\n"
+        f"{new_members_added_externally}\n\n"
         f"Please review the audit log for more details: {audit_log_url}\n\n"
         f"Have a swell day, \n\n"
         "The GitHub Organisation Monitoring Bot"
@@ -45,16 +48,19 @@ def main():
     github_service = GithubService(str(github_token), MINISTRY_OF_JUSTICE)
     slack_service = SlackService(str(slack_token))
     new_members = github_service.check_for_audit_log_new_members(_calculate_date(time_delta_in_days))
-    full_member_list = ""
+    new_members_added_by_oe = ""
+    new_members_added_externally = ""
 
     if new_members:
         for member in new_members:
-            print(f"New member detected: {member['userLogin']}")
             individual_message = f"*New member:* {member['userLogin']} added by {member['actorLogin']}.\n"
-            full_member_list += individual_message
+            if member['actorLogin'] in OPERATIONS_ENGINEERING_GITHUB_USERNAMES:
+                new_members_added_by_oe += individual_message
+            else:
+                new_members_added_externally += individual_message
 
         slack_service.send_message_to_plaintext_channel_name(
-            new_members_detected_message(full_member_list, MINISTRY_OF_JUSTICE, audit_log_url, time_delta_in_days), SLACK_CHANNEL)
+            new_members_detected_message(new_members_added_by_oe, new_members_added_externally, MINISTRY_OF_JUSTICE, audit_log_url, time_delta_in_days), SLACK_CHANNEL)
 
 
 if __name__ == "__main__":
