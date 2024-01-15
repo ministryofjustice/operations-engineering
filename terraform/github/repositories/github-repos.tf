e47b702fad1a22eff641f9ad97ad19bac09303ab
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-01-15 09:32:09 +0000

    use ops-eng module

--- terraform/github/repositories/github-repos.tf
+++ terraform/github/repositories/github-repos.tf
@@ -1,5 +1,5 @@
 module "repository" {
-  source  = "github.com/ministryofjustice/data-platform/tree/main/terraform/github/modules/repository"
+  source  = "github.com/ministryofjustice/operations-engineering-terraform-github-repositories"
 
   name = "test-repo-levg"
   description = "This repo was created using terraform by the operations engineering team"
