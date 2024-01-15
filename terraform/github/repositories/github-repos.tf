984f6e4b25eaee75c070d1612da889bb64bf2ade
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-01-15 09:50:22 +0000

    required module parameters

--- terraform/github/repositories/github-repos.tf
+++ terraform/github/repositories/github-repos.tf
@@ -1,6 +1,13 @@
 module "repository" {
-  source  = "github.com/ministryofjustice/operations-engineering-terraform-github-repositories"
 
-  name = "test-repo-levg"
-  description = "This repo was created using terraform by the operations engineering team"
+  source = "github.com/ministryofjustice/operations-engineering-terraform-github-repositories"
+
+  application_name = "test-app-levg"
+  description = "this repository was create by terraform managed by operations-engineering team"
+  name = "test-repository-levg"
+  tags = {
+    Team = "operations-engineering"
+    Phase = "POC"
+  }
+  topics = ["github", "terraform", "operations-engineering"]
 }
