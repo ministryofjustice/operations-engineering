dc22630f3f67c85863823f6740d48fc2a210d7f2
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-01-17 15:18:50 +0000

    remove all repositories and terraform plan for testing

--- terraform/github/repositories/test-repository-levg.tf
+++ terraform/github/repositories/test-repository-levg.tf
@@ -1,16 +1,12 @@
-module "test-repository-levg" {
-  source = "github.com/ministryofjustice/operations-engineering-terraform-github-repositories"
+# module "test-repository-levg" {
+#   source = "github.com/ministryofjustice/operations-engineering-terraform-github-repositories"
 
-  name = "test-repository-levg"
-  application_name = "test-repository-levg"
-  description = "this repository was create by terraform managed by operations-engineering team"
-  topics = ["github", "terraform", "operations-engineering"]
-  tags = {
-    Team = "operations-engineering"
-    Phase = "POC"
-  }
-  secrets = {
-    TEST_SECRET = ""
-    UPDATED_TEST_SECRET = ""
-  }
-}
+#   name = "test-repository-levg"
+#   application_name = "test-repository-levg"
+#   description = "this repository was create by terraform managed by operations-engineering team"
+#   topics = ["github", "terraform", "operations-engineering"]
+#   tags = {
+#     Team = "operations-engineering"
+#     Phase = "POC"
+#   }
+# }
