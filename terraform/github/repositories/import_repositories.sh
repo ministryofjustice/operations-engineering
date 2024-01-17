51bf755a4fd71839466b50b9896e7b4fc04fbf1f
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-01-17 16:04:22 +0000

    template bash terraform batch import script & cleanup

--- terraform/github/repositories/import_repositories.sh
+++ terraform/github/repositories/import_repositories.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-terraform import -input=false -no-color module.test-repository-levg.github_repository.default test-repository-levg
-terraform import -input=false -no-color module.test-repository-levg2.github_repository.default test-repository-levg2
+# For Repo
+terraform import -input=false -no-color module.REPO_NAME.github_repository.default REPO_NAME
+
+# For secret
+terraform import -input=false -no-color 'module.REPO_NAME.github_actions_secret.default["SECRET_NAME"]' REPO_NAME/SECRET_NAME
