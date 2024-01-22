d7c274283ba3c5c2cef69a991c08fe2082488f58
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-01-22 13:37:45 +0000

    import devcontainer repo

--- terraform/github/repositories/import_repositories.sh
+++ terraform/github/repositories/import_repositories.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # For Repo
-terraform import -input=false -no-color module.REPO_NAME.github_repository.default REPO_NAME
+terraform import -input=false -no-color module.operations-engineering-devcontainer.github_repository.default operations-engineering-devcontainer
 
 # For secret
-terraform import -input=false -no-color 'module.REPO_NAME.github_actions_secret.default["SECRET_NAME"]' REPO_NAME/SECRET_NAME
+# terraform import -input=false -no-color 'module.REPO_NAME.github_actions_secret.default["SECRET_NAME"]' REPO_NAME/SECRET_NAME
---
+++ terraform/github/repositories/operations-engineering-devcontainer.tf
@@ -0,0 +1,12 @@
+module "operations-engineering-devcontainer" {
+  source = "github.com/ministryofjustice/operations-engineering-terraform-github-repositories"
+
+  name             = "operations-engineering-devcontainer"
+  application_name = "operations-engineering-devcontainer"
+  description      = ""
+  topics           = ["operations-engineering"]
+  tags = {
+    Team  = "operations-engineering"
+    Phase = "POC"
+  }
+}
