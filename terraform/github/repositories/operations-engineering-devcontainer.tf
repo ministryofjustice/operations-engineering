191f2d0126603709c571476020ecab5ef383fe54
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-01-22 14:45:53 +0000

    try different method of referencing tag

--- terraform/github/repositories/operations-engineering-devcontainer.tf
+++ terraform/github/repositories/operations-engineering-devcontainer.tf
@@ -1,5 +1,5 @@
 module "operations-engineering-devcontainer" {
-  source = "git::https://github.com/ministryofjustice/operations-engineering-terraform-github-repositories?ref=v0.0.1"
+  source = "github.com/ministryofjustice/operations-engineering-terraform-github-repositories?ref=0.0.1"
 
   name             = "operations-engineering-devcontainer"
   application_name = "operations-engineering-devcontainer"
