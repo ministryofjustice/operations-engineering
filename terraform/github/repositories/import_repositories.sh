c3e3bd84d2849678974e9bef910b46b5557d272a
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-01-22 13:42:06 +0000

    remove import commands

--- terraform/github/repositories/import_repositories.sh
+++ terraform/github/repositories/import_repositories.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # For Repo
-terraform import -input=false -no-color module.operations-engineering-devcontainer.github_repository.default operations-engineering-devcontainer
+# terraform import -input=false -no-color module.REPO_NAME.github_repository.default REPO_NAME
 
 # For secret
 # terraform import -input=false -no-color 'module.REPO_NAME.github_actions_secret.default["SECRET_NAME"]' REPO_NAME/SECRET_NAME
