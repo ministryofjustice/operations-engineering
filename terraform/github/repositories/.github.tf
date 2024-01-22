33281f62ef41818611ec1bcacac3607bc527821e
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-01-22 15:11:49 +0000

    import .github repo

---
+++ terraform/github/repositories/.github.tf
@@ -0,0 +1,12 @@
+module ".github" {
+  source = "github.com/ministryofjustice/operations-engineering-terraform-github-repositories?ref=0.0.1"
+
+  name             = ".github"
+  application_name = ".github"
+  description      = "Default organisational policies for the Ministry of Justice"
+  topics           = []
+  tags = {
+    Team  = "operations-engineering"
+    Phase = "production"
+  }
+}
