dd0e360b10fe704815c48c55cee0593d5a4424ec
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-01-22 14:54:30 +0000

    remove module version terraform variable

--- terraform/github/repositories/operations-engineering-devcontainer.tf
+++ terraform/github/repositories/operations-engineering-devcontainer.tf
@@ -1,7 +1,5 @@
 module "operations-engineering-devcontainer" {
-  source = "github.com/ministryofjustice/operations-engineering-terraform-github-repositories"
-
-  version = var.module_version
+  source = "github.com/ministryofjustice/operations-engineering-terraform-github-repositories?ref=0.0.1"
 
   name             = "operations-engineering-devcontainer"
   application_name = "operations-engineering-devcontainer"
--- terraform/github/repositories/variables.tf
+++ terraform/github/repositories/variables.tf
@@ -3,9 +3,3 @@ variable "github_token" {
   description = "Required by the GitHub Terraform provider"
   default     = ""
 }
-
-variable "module_version" {
-  type        = string
-  description = "version of github repository terraform module to use"
-  default     = "0.0.1"
-}
