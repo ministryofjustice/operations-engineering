8835bd4b7d49b393815df455ffdf52e5e3adf92e
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-01-23 09:48:48 +0000

    add has_discussions parameter for github_collabortors repo

--- terraform/github/repositories/github-collaborators.tf
+++ terraform/github/repositories/github-collaborators.tf
@@ -4,6 +4,7 @@ module "github-collaborators" {
   name             = "github-collaborators"
   application_name = "github-collaborators"
   description      = "Manage outside collaborators on our Github repositories"
+  has_discussions  = true
   tags = {
     Team  = "operations-engineering"
     Phase = "production"
