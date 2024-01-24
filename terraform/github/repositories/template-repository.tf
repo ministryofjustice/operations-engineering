2f1941996955dbd78cd7e48968f20c13afbceed5
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-01-24 11:21:50 +0000

    remove terraform import commands and adjust terraform for congruence

--- terraform/github/repositories/template-repository.tf
+++ terraform/github/repositories/template-repository.tf
@@ -5,7 +5,6 @@ module "template-repository" {
   application_name = "template-repository"
   type             = "template"
   description      = "Github \"template\" repository, from which to create new MoJ Repositories with organisation defaults"
-  homepage_url     = "https://hub.docker.com/r/ministryofjustice/tech-docs-github-pages-publisher"
   tags = {
     Team  = "operations-engineering"
     Phase = "production"
