fb66b2a656caea8e20cc2ea07065169e5685a45a
Jason Birchall <31217584+jasonBirchall@users.noreply.github.com>
2024-01-24 16:03:14 +0000

    Update operations-engineering-join-github.tf

--- terraform/github/repositories/operations-engineering-join-github.tf
+++ terraform/github/repositories/operations-engineering-join-github.tf
@@ -9,25 +9,4 @@ module "operations-engineering-join-github" {
     Team  = "operations-engineering"
     Phase = "production"
   }
-  secrets = {
-    DEV_KUBE_CERT           = ""
-    DEV_KUBE_NAMESPACE      = ""
-    DEV_KUBE_TOKEN          = ""
-    PROD_KUBE_CERT          = ""
-    PROD_KUBE_NAMESPACE     = ""
-    PROD_KUBE_TOKEN         = ""
-    AUTH0_CLIENT_ID         = ""
-    AUTH0_CLIENT_SECRET     = ""
-    DEV_ADMIN_GITHUB_TOKEN  = ""
-    DEV_ECR_ROLE_TO_ASSUME  = ""
-    DEV_FLASK_APP_SECRET    = ""
-    FLASK_APP_SECRET        = ""
-    KUBE_CLUSTER            = ""
-    PROD_KUBE_CERT          = ""
-    PROD_KUBE_NAMESPACE     = ""
-    PROD_KUBE_TOKEN         = ""
-    PROD_ADMIN_GITHUB_TOKEN = ""
-    PROD_ECR_ROLE_TO_ASSUME = ""
-    PROD_FLASK_APP_SECRET   = ""
-  }
 }
