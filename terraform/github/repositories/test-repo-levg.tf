626360938805a09308e4c4af3692212662abf951
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-02-07 15:32:15 +0000

    allow apply from branch and revert alias config

--- terraform/github/repositories/test-repo-levg.tf
+++ terraform/github/repositories/test-repo-levg.tf
@@ -1,6 +1,8 @@
 module "test-repo-levg" {
-  source   = "github.com/ministryofjustice/terraform-github-repository?ref=provider-config"
-  provider = github.ministryofjustice-test
+  source = "github.com/ministryofjustice/terraform-github-repository?ref=provider-config"
+  providers = {
+    github.organisation = github.ministryofjustice-test
+  }
 
   name             = "test-repo-levg"
   application_name = "test-repo-levg"
