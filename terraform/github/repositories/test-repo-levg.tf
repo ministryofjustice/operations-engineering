bbe5ac59419f8f07273a8ab4cd9b90a40b1b9903
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-02-07 16:19:57 +0000

    revert method of referencing provider

--- terraform/github/repositories/test-repo-levg.tf
+++ terraform/github/repositories/test-repo-levg.tf
@@ -1,6 +1,8 @@
 module "test-repo-levg" {
   source    = "github.com/ministryofjustice/terraform-github-repository?ref=provider-config"
-  providers = var.ministryofjustice-test_provider_mapping
+  providers = {
+    github = github.ministryofjustice-test
+  }
 
   name             = "test-repo-levg"
   application_name = "test-repo-levg"
