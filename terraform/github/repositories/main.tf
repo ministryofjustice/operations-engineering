38e30995fb200ab7f89edb2ebffd81f71c89a392
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-02-07 10:49:50 +0000

    Added DynamoDB table name to main.tf

--- terraform/github/repositories/main.tf
+++ terraform/github/repositories/main.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     acl     = "private"
     bucket  = "cloud-platform-fad89ef06d68fdbc1928cb37acd8fc9f"
+    dynamodb_table = "github-repos-prod-state-lock-table"
     encrypt = true
     key     = "terraform/github-repos/operations-engineering-prod/terraform.tfstate"
     region  = "eu-west-2"
