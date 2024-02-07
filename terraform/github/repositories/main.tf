975640f7ec1175ee336cdc134cf7a1729d3e90e6
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-02-07 13:36:59 +0000

    refactor terraform into organisation based directories and add provider alias for moj-test org

--- terraform/github/repositories/main.tf
+++ terraform/github/repositories/main.tf
@@ -23,3 +23,9 @@ provider "github" {
   token = var.github_token
   owner = "ministryofjustice"
 }
+
+provider "github" {
+  alias = "ministryofjustice-test"
+  token = var.github_token
+  owner = "ministryofjustice-test"
+}
---
+++ terraform/github/repositories/ministryofjustice-test/test-repo-levg.tf
@@ -0,0 +1,9 @@
+module "test-repo-levg" {
+  source  = "ministryofjustice/repository/github"
+  version = "0.0.4"
+  provider = github.ministryofjustice-test
+
+  name             = "test-repo-levg"
+  application_name = "test-repo-levg"
+  description      = "test repo"
+}
