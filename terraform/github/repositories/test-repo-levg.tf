19b01c4a9ea69b29498471c92391d1552d111da5
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-02-07 15:44:57 +0000

    remove test repo

--- terraform/github/repositories/test-repo-levg.tf
+++ terraform/github/repositories/test-repo-levg.tf
@@ -1,10 +1,10 @@
-module "test-repo-levg" {
-  source = "github.com/ministryofjustice/terraform-github-repository?ref=provider-config"
-  providers = {
-    github = github.ministryofjustice-test
-  }
+# module "test-repo-levg" {
+#   source = "github.com/ministryofjustice/terraform-github-repository?ref=provider-config"
+#   providers = {
+#     github = github.ministryofjustice-test
+#   }
 
-  name             = "test-repo-levg"
-  application_name = "test-repo-levg"
-  description      = "test repo"
-}
+#   name             = "test-repo-levg"
+#   application_name = "test-repo-levg"
+#   description      = "test repo"
+# }
