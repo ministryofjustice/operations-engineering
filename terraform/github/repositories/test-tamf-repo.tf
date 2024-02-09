327fe3816e9d89076f80984d2aebb07b9d7b6873
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-02-09 14:59:30 +0000

    test in main org

--- terraform/github/repositories/test-tamf-repo.tf
+++ terraform/github/repositories/test-tamf-repo.tf
@@ -1,17 +1,13 @@
 module "test_tamf_repo" {
   source = "github.com/ministryofjustice/terraform-github-repository.git?ref=output-repo-name"
 
-  providers = {
-    github = github.ministryofjustice-test
-  }
-
   name        = "test-tamf-repo"
   description = "Test repo to test team association"
   topics      = ["operations-engineering"]
 }
 
 resource "github_team_repository" "test_tamf_repo" {
-  team_id    = data.github_team.operations_engineering_test.id
+  team_id    = data.github_team.operations_engineering.id
   repository = module.test_tamf_repo.repository_name
   permission = "admin"
 }
