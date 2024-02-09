050bb47901b1dec52dec5efa7832fd014b0aae59
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-02-09 12:59:35 +0000

    try that

--- terraform/github/repositories/acronyms.tf
+++ terraform/github/repositories/acronyms.tf
@@ -10,6 +10,6 @@ module "acronyms" {
 
 resource "github_team_repository" "acronyms_ops_eng_admin" {
   team_id    = data.github_team.operations_engineering
-  repository = github_repository.this.name
+  repository = acronyms.name
   permission = "admin"
 }
