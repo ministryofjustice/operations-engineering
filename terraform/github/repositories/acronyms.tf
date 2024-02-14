abe19ad90c2b16c35481424e84bff6c9efab78b6
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-02-14 15:21:54 +0000

    import acronyms team-repo asscoiation

--- terraform/github/repositories/acronyms.tf
+++ terraform/github/repositories/acronyms.tf
@@ -6,4 +6,8 @@ module "acronyms" {
   description  = "List of abbreviations used within the MoJ, and their definitions"
   homepage_url = "https://ministry-of-justice-acronyms.service.justice.gov.uk/"
   topics       = ["operations-engineering"]
+
+  team_access = {
+    admin = [data.github_team.operations_engineering.id]
+  }
 }
--- terraform/github/repositories/imports.tf
+++ terraform/github/repositories/imports.tf
@@ -1,4 +1,11 @@
+# Imports to ministryofjustice-test org team_id 9472191
 import {
   to = module.test_tamf_repo_test_org.github_team_repository.push["9472191"]
   id = "9472191:test-tamf-repo-test-org"
+}
+
+# Imports to ministryofjustice operations-engineering team
+import {
+  to = module.test_tamf_repo_test_org.github_team_repository.admin["operations-engineering"]
+  id = "operations-engineering:acronyms"
 }
