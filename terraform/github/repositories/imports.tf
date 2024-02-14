6ef823beb7ef20dbb73ba5b9d9fee847e37ca752
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-02-14 15:06:29 +0000

    try ID 9472191:test-tamf-repo-test-org

--- terraform/github/repositories/imports.tf
+++ terraform/github/repositories/imports.tf
@@ -1,4 +1,4 @@
 import {
-  to = module.test_tamf_repo_test_org.github_team_repository.push
-  id = "9472191"
+  to = module.test_tamf_repo_test_org.github_team_repository.push["9472191"]
+  id = "9472191:test-tamf-repo-test-org"
 }
