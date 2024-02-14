e1cd27e2f3cf3bfddd90ec0a8af8f0c88378213d
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-02-14 13:49:16 +0000

    remove provider from import block

--- terraform/github/repositories/imports.tf
+++ terraform/github/repositories/imports.tf
@@ -1,5 +1,4 @@
 import {
-  provider = github.ministryofjustice-test
-  to       = module.test_tamf_repo_test_org.team_access.push
-  id       = data.github_team.test_team_access.id
+  to = module.test_tamf_repo_test_org.team_access.push
+  id = data.github_team.test_team_access.id
 }
