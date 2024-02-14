f9667df901dfebe0276b00efb6260ad9dc536797
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-02-14 14:11:54 +0000

    remove import block

--- terraform/github/repositories/imports.tf
+++ terraform/github/repositories/imports.tf
@@ -1,4 +1,4 @@
-import {
-  to = module.test_tamf_repo_test_org.team_access.push
-  id = data.github_team.test_team_access.id
-}
+# import {
+#   to = module.test_tamf_repo_test_org.team_access.push
+#   id = data.github_team.test_team_access.id
+# }
