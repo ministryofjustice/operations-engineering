f05a93b400f97b5e72391f020d2b03a3dcf5a421
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-02-14 15:46:05 +0000

    try locals and for each block

--- terraform/github/repositories/imports.tf
+++ terraform/github/repositories/imports.tf
@@ -5,12 +5,25 @@ import {
 }
 
 # Imports to ministryofjustice operations-engineering team_id 4192115 with admin access
-import {
-  to = module.acronyms.github_team_repository.admin["4192115"]
-  id = "4192115:acronyms"
+# import {
+#   to = module.acronyms.github_team_repository.admin["4192115"]
+#   id = "4192115:acronyms"
+# }
+
+# import {
+#   to = module.cloud-platform-maintenance-pages.github_team_repository.admin["4192115"]
+#   id = "4192115:cloud-platform-maintenance-pages"
+# }
+
+locals {
+  repositories = {
+    "acronyms"                         = "admin"
+    "cloud-platform-maintenance-pages" = "admin"
+  }
 }
 
 import {
-  to = module.cloud-platform-maintenance-pages.github_team_repository.admin["4192115"]
-  id = "4192115:cloud-platform-maintenance-pages"
+  for_each = local.repositories
+  to       = module.each.key.github_team_repository.each.value["4192115"]
+  id       = "4192115:${each.key}"
 }
