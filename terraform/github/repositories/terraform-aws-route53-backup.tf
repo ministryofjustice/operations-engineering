9fd03b5194b23a1b34c98bf3af731a481cec3450
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-02-20 16:11:09 +0000

    two rs in terraform

--- terraform/github/repositories/terraform-aws-route53-backup.tf
+++ terraform/github/repositories/terraform-aws-route53-backup.tf
@@ -1,8 +1,8 @@
-module "teraform-aws-route53-backup" {
+module "terraform-aws-route53-backup" {
   source  = "ministryofjustice/repository/github"
   version = "0.0.8"
 
-  name            = "teraform-aws-route53-backup"
+  name            = "terraform-aws-route53-backup"
   description     = "A Terraform module for backup and restoration of Amazon Route 53 records."
   has_discussions = true
   topics          = ["operations-engineering", "terraform", "terraform-module"]
