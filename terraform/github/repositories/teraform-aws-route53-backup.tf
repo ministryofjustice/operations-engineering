8b057b9846d46ece1ed8d82117baaf41e2efc673
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-02-20 15:51:10 +0000

    terraform fmt

--- terraform/github/repositories/teraform-aws-route53-backup.tf
+++ terraform/github/repositories/teraform-aws-route53-backup.tf
@@ -2,12 +2,12 @@ module "teraform-aws-route53-backup" {
   source  = "ministryofjustice/repository/github"
   version = "0.0.8"
 
-  name              = "teraform-aws-route53-backup"
-  description       = "A Terraform module for baclkup and restore of Amazon Route 53 records."
-  has_discussions   = true
-  topics            = ["operations-engineering", "terraform", "terraform-module"]
-  type              = "module"
-  visibilvisibility = "internal"
+  name            = "teraform-aws-route53-backup"
+  description     = "A Terraform module for baclkup and restore of Amazon Route 53 records."
+  has_discussions = true
+  topics          = ["operations-engineering", "terraform", "terraform-module"]
+  type            = "module"
+  visibility      = "internal"
 
   team_access = {
     admin = [data.github_team.operations_engineering.id]
