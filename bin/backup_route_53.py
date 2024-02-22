c0e49e80be187a2d974031cd406c0208b9aec9d6
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-02-22 14:54:33 +0000

    output json file

--- bin/backup_route_53.py
+++ bin/backup_route_53.py
@@ -1,13 +1,15 @@
 from services.route_53_service import Route53Service
 import json
 
-def test(route_53_service: Route53Service):
+def export_hosted_zones_to_json(route_53_service: Route53Service):
     data_to_backup = route_53_service.bulk_export_route53_records()
-    print(data_to_backup)
+
+    with open('hosted_zones.json', "w") as json_file:
+        json_file.write(data_to_backup)
 
 def main():
     route_53_service = Route53Service()
-    test(route_53_service)
+    export_hosted_zones_to_json(route_53_service)
 
 if __name__ == "__main__":
     main()
