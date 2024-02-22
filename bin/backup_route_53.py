6cb7786530207016803a79c198056e909b91969f
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-02-22 12:44:26 +0000

    route53 service, route53-backup base and test

---
+++ bin/backup_route_53.py
@@ -0,0 +1,12 @@
+from services.route_53_service import Route53Service
+
+def test(route_53_service: Route53Service):
+    data_to_backup = route_53_service.bulk_export_route53_records
+    print(data_to_backup)
+
+def main():
+    route_53_service = Route53Service
+    test(route_53_service)
+
+if __name__ == "__main__":
+    main()
---
+++ services/route_53_service.py
@@ -0,0 +1,45 @@
+import boto3
+import json
+from datetime import datetime
+
+
+class Route53Service:
+    def __init__(self) -> None:
+        self.client = boto3.client("route53")
+
+    def get_route53_hosted_zones(self):
+
+        response = self.client.list_hosted_zones()
+
+        hosted_zone_ids = [zone['Id'] for zone in response['HostedZones']]
+
+        return hosted_zone_ids
+    
+    def export_route53_records(self, zone_id: str):
+
+        response = self.client.list_resource_record_sets(HostedZoneId=zone_id)
+
+        records = []
+
+        for record_set in response['ResourceRecordSets']:
+            record = {
+                "Name": record_set['Name'],
+                "Type": record_set['Type'],
+                "TTL": record_set['TTL'],
+                "ResourceRecords": [record['Value'] for record in record_set.get('ResourceRecords', [])]
+            }
+            records.append(record)
+
+        return json.dumps(records, indent=4)
+    
+    def bulk_export_route53_records(self):
+
+        hosted_zone_ids = self.get_route53_hosted_zones()
+
+        exported_records = {}
+
+        for zone_id in hosted_zone_ids:
+            records = self.export_route53_records(zone_id)
+            exported_records[zone_id] = records
+
+        return json.dumps(exported_records, indent=4)
