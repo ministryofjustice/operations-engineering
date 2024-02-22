593e5b532d42243117109a2ac93270873eccaaa9
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-02-22 14:28:53 +0000

    account for alias records

--- services/route_53_service.py
+++ services/route_53_service.py
@@ -24,12 +24,20 @@ class Route53Service:
         for record_set in response['ResourceRecordSets']:
             print(zone_id)
             print(record_set)
-            record = {
-                "Name": record_set['Name'],
-                "Type": record_set['Type'],
-                "TTL": record_set['TTL'],
-                "ResourceRecords": [record['Value'] for record in record_set.get('ResourceRecords', [])]
-            }
+            record = None
+            if record_set["ResourceRecords"]:
+                record = {
+                    "Name": record_set['Name'],
+                    "Type": record_set['Type'],
+                    "TTL": record_set['TTL'],
+                    "ResourceRecords": [record['Value'] for record in record_set.get('ResourceRecords', [])],
+                }
+            else:
+                record = {
+                    "Name": record_set['Name'],
+                    "Type": record_set['Type'],
+                    'AliasTarget': record_set['AliasTarget']
+                }
             records.append(record)
 
         return json.dumps(records, indent=4)
