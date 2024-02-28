d2d60f8254b1274632c844e5dee8f23bbf3a0032
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-02-28 18:13:14 +0000

    service layer code and unnit tests for bulk restore of hosted zones

--- services/route_53_service.py
+++ services/route_53_service.py
@@ -1,6 +1,5 @@
 import boto3
 import json
-from datetime import datetime
 
 
 class Route53Service:
@@ -11,9 +10,11 @@ class Route53Service:
 
         response = self.client.list_hosted_zones()
 
-        hosted_zone_ids = [zone['Id'] for zone in response['HostedZones']]
+        hosted_zone_data = []
+        for zone in response['HostedZones']:
+            hosted_zone_data.append({'id': zone['Id'], 'name': zone['Name']})
 
-        return hosted_zone_ids
+        return hosted_zone_data 
     
     def export_route53_records(self, zone_id: str):
 
@@ -42,12 +43,98 @@ class Route53Service:
     
     def bulk_export_route53_records(self):
 
-        hosted_zone_ids = self.get_route53_hosted_zones()
+        hosted_zone_data = self.get_route53_hosted_zones()
 
         exported_records = {}
 
-        for zone_id in hosted_zone_ids:
-            records = self.export_route53_records(zone_id)
-            exported_records[zone_id] = records
+        for zone in hosted_zone_data:
+            records = self.export_route53_records(zone['id'])
+            exported_records[zone['id']] = {'name': zone['name'], 'records': records}
 
         return json.dumps(exported_records, indent=4)
+    
+    def check_if_hosted_zone_exists(self, hosted_zone_id):
+        try:
+            self.client.get_hosted_zone(Id=hosted_zone_id)
+            print(f"Hosted zone '{hosted_zone_id}' already exists.")
+            return True
+        except self.client.exceptions.NoSuchHostedZone:
+            print(f"Hosted zone '{hosted_zone_id}' doesn't exist.")
+            return False
+    
+    def restore_hosted_zone(self, hosted_zone_id, hosted_zone_name):
+        if not self.check_if_hosted_zone_exists(hosted_zone_id):
+            caller_reference = str(hash(hosted_zone_id))
+            zone = self.client.create_hosted_zone(Name=hosted_zone_name, CallerReference=caller_reference)
+            print(f"Created hosted zone '{hosted_zone_name}'.")
+            return zone['HostedZone']['Id']
+        else:
+            print(f"A hosted zone with id '{hosted_zone_id}' already exists")
+            return False
+        
+    def check_if_record_exists(self, hosted_zone_id, record_name, record_type):
+        records = self.client.list_resource_record_sets(HostedZoneId=hosted_zone_id)['ResourceRecordSets']
+        for record in records:
+            if record["Name"] == record_name and record["Type"] == record_type:
+                print(f"Resource record '{record_name}' of type '{record_type}' already exists.")
+                return True
+        return False
+    
+    def create_change(self, record):
+        change = {
+            'Action': 'CREATE',
+            'ResourceRecordSet': {
+                'Name': record['Name'],
+                'Type': record['Type'],
+                'TTL': record['TTL'],
+            }
+        }
+
+        if 'ResourceRecords' in record:
+            change['ResourceRecordSet']['ResourceRecords'] = [{'Value': value} for value in record['ResourceRecords']]
+        elif 'AliasTarget' in record:
+            change['ResourceRecordSet']['AliasTarget'] = record['AliasTarget']
+
+        return change
+
+    def create_change_batch(self, hosted_zone_id, records):
+        changes = []
+        for record in records:
+            if not self.check_if_record_exists(hosted_zone_id, record['Name'], record['Type']):
+                change = self.create_change(record)
+                changes.append(change)
+        return changes
+    
+    def restore_hosted_zone_records(self, hosted_zone_id, records):
+        if not self.check_if_hosted_zone_exists(hosted_zone_id):
+            return False
+        
+        changes = self.create_change_batch(hosted_zone_id, records)
+        response = self.client.change_resource_record_sets(
+            HostedZoneId=hosted_zone_id,
+            ChangeBatch={
+                'Changes': changes
+            }
+        )
+        print("Hosted zone records restored successfully.")
+        print("Change info:", response)
+
+    def bulk_restore_hosted_zones(self, json_backup, hosted_zones_to_restore):
+
+        backup_data = json.loads(json_backup)
+
+        new_zone_ids = []
+
+        if hosted_zones_to_restore == 'all':
+            for hz_id in backup_data.keys():
+                new_zone_id = self.restore_hosted_zone(hz_id, backup_data[hz_id]['name'])
+                self.restore_hosted_zone_records(new_zone_id, backup_data[hz_id]['records'])
+                new_zone_ids.append(new_zone_id)
+        else:
+            for hz_id in hosted_zones_to_restore:
+                if hz_id in backup_data.keys():
+                    new_zone_id = self.restore_hosted_zone(hz_id, backup_data[hz_id]['name'])
+                    self.restore_hosted_zone_records(new_zone_id, backup_data[hz_id]['records'])
+                    new_zone_ids.append(new_zone_id)
+        
+        return new_zone_ids
--- test/test_services/test_route_53_service.py
+++ test/test_services/test_route_53_service.py
@@ -18,7 +18,8 @@ class TestRoute53Service(unittest.TestCase):
 
         res = self.route_53_service.get_route53_hosted_zones()
 
-        assert res[0] == zone['HostedZone']['Id']
+        assert res[0]['id'] == zone['HostedZone']['Id']
+        assert res[0]['name'] == zone['HostedZone']['Name']
 
     @mock_aws
     def test_export_route53_records(self):
@@ -73,7 +74,7 @@ class TestRoute53Service(unittest.TestCase):
     @patch.object(Route53Service, "export_route53_records")
     def test_bulk_export_route53_records(self, mock_records, mock_zone_id):
 
-        mock_zone_id.return_value = ["/hostedzone/Z31RX3GZS94JZS"]
+        mock_zone_id.return_value = [{"id": "/hostedzone/Z31RX3GZS94JZS", "name": "testdns.aws.com."}]
         mock_records.return_value = [
             {'Name': 'testdns.aws.com.', 'Type': 'NS', 'TTL': 172800, 'ResourceRecords': ['ns-2048.awsdns-64.com', 'ns-2049.awsdns-65.net', 'ns-2050.awsdns-66.org', 'ns-2051.awsdns-67.co.uk']}, 
             {'Name': 'testdns.aws.com.', 'Type': 'SOA', 'TTL': 900, 'ResourceRecords': ["{'Value': 'ns-2048.awsdns-64.com. hostmaster.example.com. 1 7200 900 1209600 86400'}"]}
@@ -81,7 +82,121 @@ class TestRoute53Service(unittest.TestCase):
 
         res = self.route_53_service.bulk_export_route53_records()
 
-        assert json.loads(res) == {'/hostedzone/Z31RX3GZS94JZS': [{'Name': 'testdns.aws.com.', 'Type': 'NS', 'TTL': 172800, 'ResourceRecords': ['ns-2048.awsdns-64.com', 'ns-2049.awsdns-65.net', 'ns-2050.awsdns-66.org', 'ns-2051.awsdns-67.co.uk']}, {'Name': 'testdns.aws.com.', 'Type': 'SOA', 'TTL': 900, 'ResourceRecords': ["{'Value': 'ns-2048.awsdns-64.com. hostmaster.example.com. 1 7200 900 1209600 86400'}"]}]}
+        assert json.loads(res) == {'/hostedzone/Z31RX3GZS94JZS': {'name': 'testdns.aws.com.', 'records': [{'Name': 'testdns.aws.com.', 'Type': 'NS', 'TTL': 172800, 'ResourceRecords': ['ns-2048.awsdns-64.com', 'ns-2049.awsdns-65.net', 'ns-2050.awsdns-66.org', 'ns-2051.awsdns-67.co.uk']}, {'Name': 'testdns.aws.com.', 'Type': 'SOA', 'TTL': 900, 'ResourceRecords': ["{'Value': 'ns-2048.awsdns-64.com. hostmaster.example.com. 1 7200 900 1209600 86400'}"]}]}}
+
+    @mock_aws
+    def test_check_if_hosted_zone_exists_if_true(self):
+        zone = self.route_53_service.client.create_hosted_zone(Name="testdns.aws.com.", CallerReference=str(hash("foo")))
+        res = self.route_53_service.check_if_hosted_zone_exists(zone['HostedZone']['Id'])
+        assert res == True
+
+    @mock_aws
+    def test_check_if_hosted_zone_exists_if_false(self):
+        res = self.route_53_service.check_if_hosted_zone_exists('/hostedzone/Z31RX3GZS94JZS')
+        assert res == False
+        
+    @patch.object(Route53Service, "check_if_hosted_zone_exists")
+    @mock_aws
+    def test_restore_hosted_zone_if_doesnt_exist(self, mock_response):
+        mock_response.return_value = False
+        res = self.route_53_service.restore_hosted_zone('/hostedzone/Z31RX3GZS94JZS', 'testdns.aws.com.')
+        hosted_zone_data = self.route_53_service.client.list_hosted_zones()['HostedZones']
+        assert hosted_zone_data[0]['Name'] == 'testdns.aws.com.'
+        assert res == hosted_zone_data[0]['Id']
+
+    @patch.object(Route53Service, "check_if_hosted_zone_exists")
+    def test_restore_hosted_zone_if_exists(self, mock_response):
+        mock_response.return_value = True
+        res = self.route_53_service.restore_hosted_zone('/hostedzone/Z31RX3GZS94JZS', 'testdns.aws.com.')          
+        assert res == False                
+
+    @mock_aws
+    def test_check_if_record_exists_if_false(self):
+        zone = self.route_53_service.client.create_hosted_zone(Name="testdns.aws.com.", CallerReference=str(hash("foo")))
+        res = self.route_53_service.check_if_record_exists(zone['HostedZone']['Id'], "testdns.aws.com.", "MX")
+        assert res == False
+
+    @mock_aws
+    def test_check_if_record_exists_if_true(self):
+        zone = self.route_53_service.client.create_hosted_zone(Name="testdns.aws.com.", CallerReference=str(hash("foo")))
+        self.route_53_service.client.change_resource_record_sets(
+            HostedZoneId=zone['HostedZone']['Id'],
+            ChangeBatch={
+                "Changes": [
+                    {
+                        "Action": "CREATE",
+                        "ResourceRecordSet": {
+                            "Name": "testdns.aws.com.",
+                            "Type": "MX",
+                            "TTL": 1800,
+                            "ResourceRecords": [
+                                {"Value": "10 inbound-smtp.eu-west-1.amazonaws.com"}
+                            ]
+                        },
+                    }
+                ]
+            },
+        )
+
+        res = self.route_53_service.check_if_record_exists(zone['HostedZone']['Id'], "testdns.aws.com.", "MX")
+        assert res == True
+
+    def test_create_change(self):
+        record = {'Name': 'testdns.aws.com.', 'Type': 'MX', 'TTL': 1800, 'ResourceRecords': ['10 inbound-smtp.eu-west-1.amazonaws.com']}
+        res = self.route_53_service.create_change(record)
+        assert res == {'Action': 'CREATE', 'ResourceRecordSet': {'Name': 'testdns.aws.com.', 'Type': 'MX', 'TTL': 1800, 'ResourceRecords': [{'Value': '10 inbound-smtp.eu-west-1.amazonaws.com'}]}}
+
+    @patch.object(Route53Service, "create_change")
+    @patch.object(Route53Service, "check_if_record_exists")
+    def test_create_change_batch_if_new_changes(self, mock_check_if_record_exists, mock_change):
+        mock_check_if_record_exists.return_value = False
+        mock_change.return_value = {'Action': 'CREATE', 'ResourceRecordSet': {'Name': 'testdns.aws.com.', 'Type': 'MX', 'TTL': 1800, 'ResourceRecords': [{'Value': '10 inbound-smtp.eu-west-1.amazonaws.com'}]}}
+        res = self.route_53_service.create_change_batch('/hostedzone/Z31RX3GZS94JZS', [{'Name': 'test', 'Type': 'test'}])
+        assert res == [{'Action': 'CREATE', 'ResourceRecordSet': {'Name': 'testdns.aws.com.', 'Type': 'MX', 'TTL': 1800, 'ResourceRecords': [{'Value': '10 inbound-smtp.eu-west-1.amazonaws.com'}]}}]
+
+    @patch.object(Route53Service, "check_if_record_exists")
+    def test_create_change_batch_if_record_exists(self, mock_check_if_record_exists):
+        mock_check_if_record_exists.return_value = True
+        res = self.route_53_service.create_change_batch(mock_check_if_record_exists, mock_check_if_record_exists)
+        assert res == []
+
+    @mock_aws
+    @patch.object(Route53Service, "create_change_batch")
+    def test_restore_hosted_zone_records_if_zone_exists(self, mock_change_batch):
+        mock_change_batch.return_value = [{'Action': 'CREATE', 'ResourceRecordSet': {'Name': 'testdns.aws.com.', 'Type': 'MX', 'TTL': 1800, 'ResourceRecords': [{'Value': '10 inbound-smtp.eu-west-1.amazonaws.com'}]}}]
+        zone = self.route_53_service.client.create_hosted_zone(Name="testdns.aws.com.", CallerReference=str(hash("foo")))
+        self.route_53_service.restore_hosted_zone_records(zone['HostedZone']['Id'], mock_change_batch)
+        res = self.route_53_service.client.list_resource_record_sets(HostedZoneId=zone['HostedZone']['Id'])['ResourceRecordSets'][0]
+        assert res == {'Name': 'testdns.aws.com.', 'Type': 'MX', 'TTL': 1800, 'ResourceRecords': [{'Value': '10 inbound-smtp.eu-west-1.amazonaws.com'}]}
+
+    @mock_aws
+    def test_restore_hosted_zone_records_if_zone_doesnt_exists(self):
+        res = self.route_53_service.restore_hosted_zone_records('/hostedzone/Z31RX3GZS94JZS', [])
+        assert res == False
+
+    @mock_aws
+    def test_bulk_restore_hosted_zones_all(self):
+        json_backup = json.dumps({'/hostedzone/Z31RX3GZS94JZS': {'name': 'testdns.aws.com.', 'records': [{'Name': 'testdns.aws.com.', 'Type': 'NS', 'TTL': 172800, 'ResourceRecords': ['ns-2048.awsdns-64.com', 'ns-2049.awsdns-65.net', 'ns-2050.awsdns-66.org', 'ns-2051.awsdns-67.co.uk']}, {'Name': 'testdns.aws.com.', 'Type': 'SOA', 'TTL': 900, 'ResourceRecords': ["{'Value': 'ns-2048.awsdns-64.com. hostmaster.example.com. 1 7200 900 1209600 86400'}"]}, {'Name': 'testdns.aws.com.', 'Type': 'MX', 'TTL': 1800, 'ResourceRecords': ['10 inbound-smtp.eu-west-1.amazonaws.com']}]}})
+        hosted_zones_to_restore = "all"
+        new_zone_ids = self.route_53_service.bulk_restore_hosted_zones(json_backup, hosted_zones_to_restore)
+        hz_data = self.route_53_service.client.list_hosted_zones()['HostedZones'][0]
+        hz_records = self.route_53_service.client.list_resource_record_sets(HostedZoneId=new_zone_ids[0])['ResourceRecordSets']
+        assert hz_data['Id'] == new_zone_ids[0]
+        assert hz_data['Name'] == 'testdns.aws.com.'
+        assert hz_records == [{'Name': 'testdns.aws.com.', 'Type': 'MX', 'TTL': 1800, 'ResourceRecords': [{'Value': '10 inbound-smtp.eu-west-1.amazonaws.com'}]}, {'Name': 'testdns.aws.com.', 'Type': 'NS', 'TTL': 172800, 'ResourceRecords': [{'Value': 'ns-2048.awsdns-64.com'}, {'Value': 'ns-2049.awsdns-65.net'}, {'Value': 'ns-2050.awsdns-66.org'}, {'Value': 'ns-2051.awsdns-67.co.uk'}]}, {'Name': 'testdns.aws.com.', 'Type': 'SOA', 'TTL': 900, 'ResourceRecords': [{'Value': "{'Value': 'ns-2048.awsdns-64.com. hostmaster.example.com. 1 7200 900 1209600 86400'}"}]}]
+
+    @mock_aws
+    def test_bulk_restore_hosted_zones_all(self):
+        json_backup = json.dumps({'/hostedzone/Z31RX3GZS94JZS': {'name': 'testdns.aws.com.', 'records': [{'Name': 'testdns.aws.com.', 'Type': 'NS', 'TTL': 172800, 'ResourceRecords': ['ns-2048.awsdns-64.com', 'ns-2049.awsdns-65.net', 'ns-2050.awsdns-66.org', 'ns-2051.awsdns-67.co.uk']}, {'Name': 'testdns.aws.com.', 'Type': 'SOA', 'TTL': 900, 'ResourceRecords': ["{'Value': 'ns-2048.awsdns-64.com. hostmaster.example.com. 1 7200 900 1209600 86400'}"]}, {'Name': 'testdns.aws.com.', 'Type': 'MX', 'TTL': 1800, 'ResourceRecords': ['10 inbound-smtp.eu-west-1.amazonaws.com']}]}})
+        hosted_zones_to_restore = ['/hostedzone/Z31RX3GZS94JZS']
+        new_zone_ids = self.route_53_service.bulk_restore_hosted_zones(json_backup, hosted_zones_to_restore)
+        hz_data = self.route_53_service.client.list_hosted_zones()['HostedZones'][0]
+        hz_records = self.route_53_service.client.list_resource_record_sets(HostedZoneId=new_zone_ids[0])['ResourceRecordSets']
+        assert hz_data['Id'] == new_zone_ids[0]
+        assert hz_data['Name'] == 'testdns.aws.com.'
+        assert hz_records == [{'Name': 'testdns.aws.com.', 'Type': 'MX', 'TTL': 1800, 'ResourceRecords': [{'Value': '10 inbound-smtp.eu-west-1.amazonaws.com'}]}, {'Name': 'testdns.aws.com.', 'Type': 'NS', 'TTL': 172800, 'ResourceRecords': [{'Value': 'ns-2048.awsdns-64.com'}, {'Value': 'ns-2049.awsdns-65.net'}, {'Value': 'ns-2050.awsdns-66.org'}, {'Value': 'ns-2051.awsdns-67.co.uk'}]}, {'Name': 'testdns.aws.com.', 'Type': 'SOA', 'TTL': 900, 'ResourceRecords': [{'Value': "{'Value': 'ns-2048.awsdns-64.com. hostmaster.example.com. 1 7200 900 1209600 86400'}"}]}]
+
+
 
 
 if __name__ == "__main__":
