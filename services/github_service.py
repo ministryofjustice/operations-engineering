c10857fc2409c3f88c89667a0728a3626b14c44d
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-03-08 12:03:32 +0000

    refactor function call to get all orgs for enterprise

--- services/github_service.py
+++ services/github_service.py
@@ -1210,4 +1210,4 @@ class GithubService:
     @retries_github_rate_limit_exception_at_next_reset_once
     def get_all_organisations_in_enterprise(self) -> list[Organization]:
         logging.info(f"Getting all organisations for enterprise {self.ENTERPRISE_NAME}")
-        return self.github_client_core_api.get_organizations() or []
+        return self.github_client_core_api.get_organization(f"enterprises/'{self.ENTERPRISE_NAME}'").get_orgs() or []
