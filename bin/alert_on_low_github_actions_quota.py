6e73a83b38a0ab1b898029cb02916c6064f9150c
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-03-11 15:43:54 +0000

    refactor - create separate function to tally up minutes used

--- bin/alert_on_low_github_actions_quota.py
+++ bin/alert_on_low_github_actions_quota.py
@@ -3,6 +3,18 @@ from config.constants import ENTERPRISE, MINISTRY_OF_JUSTICE, SLACK_CHANNEL
 import os
 import json
 
+def calculate_total_minutes_used(organisations, github_service):
+    total = 0 
+    
+    for org in organisations:
+        billing_data = github_service.get_gha_minutes_used_for_organisation(org.login)
+        processed_data = json.loads(billing_data)
+        minutes_used = processed_data["total_minutes_used"]
+        total += minutes_used
+
+    return total
+
+
 def alert_on_low_github_actions_quota():
     github_token = os.environ.get("GH_TOKEN")
 
@@ -10,13 +22,11 @@ def alert_on_low_github_actions_quota():
     
     organisations = github_service.get_all_organisations_in_enterprise()
 
-    for org in organisations:
-        billing_data = github_service.get_gha_minutes_used_for_organisation(org.login)
-        processed_data = json.loads(billing_data)
-        minutes_used = processed_data["total_minutes_used"]
-        print(minutes_used)
+    total_minutes_used = calculate_total_minutes_used(organisations, github_service)
+
+    return total_minutes_used
         
 if __name__ == "__main__":
-    alert_on_low_github_actions_quota()
+    print(alert_on_low_github_actions_quota())
 
 
