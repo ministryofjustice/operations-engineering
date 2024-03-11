f24d0aa65f72c45f8d58d9c5b831bb56a5e7b6b0
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-03-11 15:11:06 +0000

    only 1 gh token used that provides all necessary perms

--- bin/alert_on_low_github_actions_quota.py
+++ bin/alert_on_low_github_actions_quota.py
@@ -3,7 +3,7 @@ from config.constants import ENTERPRISE, MINISTRY_OF_JUSTICE, SLACK_CHANNEL
 import os
 
 def alert_on_low_github_actions_quota():
-    github_token = os.environ.get("ADMIN_GITHUB_TOKEN")
+    github_token = os.environ.get("GH_TOKEN")
 
     github_service = GithubService(github_token, MINISTRY_OF_JUSTICE, ENTERPRISE)
     
