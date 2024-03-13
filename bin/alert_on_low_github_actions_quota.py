87ea6d71d016b832f3e8409f7659e3acce7b7428
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-03-13 13:23:44 +0000

    alert if percentage of minutes used is greater than or equal to threshold - test threshold

--- bin/alert_on_low_github_actions_quota.py
+++ bin/alert_on_low_github_actions_quota.py
@@ -2,6 +2,15 @@ from services.github_service import GithubService
 from config.constants import ENTERPRISE, MINISTRY_OF_JUSTICE, SLACK_CHANNEL
 import os
 import json
+from services.slack_service import SlackService
+
+def calculate_percentage_used(total_minutes_used):
+    total_available = 50000
+
+    return (total_minutes_used / total_available) * 100
+
+def low_threshold_triggered_message(threshold):
+    return f"{100 - threshold}% of the github actions minutes quota is remaining."
 
 def calculate_total_minutes_used(organisations, github_service):
     total = 0 
@@ -17,16 +26,23 @@ def calculate_total_minutes_used(organisations, github_service):
 
 def alert_on_low_github_actions_quota():
     github_token = os.environ.get("GH_TOKEN")
+    slack_token = os.environ.get("ADMIN_SLACK_TOKEN")
 
     github_service = GithubService(github_token, MINISTRY_OF_JUSTICE, ENTERPRISE)
+    slack_service = SlackService(slack_token)
     
     organisations = github_service.get_all_organisations_in_enterprise()
 
     total_minutes_used = calculate_total_minutes_used(organisations, github_service)
 
-    return total_minutes_used
+    percentage_used = calculate_percentage_used(total_minutes_used)
+
+    threshold = 10
+
+    if percentage_used >= threshold:
+        slack_service.send_message_to_plaintext_channel_name(low_threshold_triggered_message(threshold), SLACK_CHANNEL)
         
 if __name__ == "__main__":
-    print(alert_on_low_github_actions_quota())
+    alert_on_low_github_actions_quota()
 
 
