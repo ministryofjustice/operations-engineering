b8ccbe11c9245c3b27d2546a3ddcfc8d90294753
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-03-15 17:04:05 +0000

    check for empty tokens

--- bin/alert_on_low_github_actions_quota.py
+++ bin/alert_on_low_github_actions_quota.py
@@ -1,6 +1,7 @@
 from services.github_service import GithubService
 from config.constants import ENTERPRISE, MINISTRY_OF_JUSTICE, SLACK_CHANNEL
 import os
+import sys
 from services.slack_service import SlackService
 
 def calculate_percentage_used(total_minutes_used):
@@ -13,7 +14,13 @@ def low_threshold_triggered_message(percentage_used):
 
 def alert_on_low_quota():
     github_token = os.environ.get("GH_TOKEN")
+    if github_token is None:
+        print("No GITHUB_TOKEN environment variable set")
+        sys.exit(1)
     slack_token = os.environ.get("ADMIN_SLACK_TOKEN")
+    if slack_token is None:
+        print("No SLACK_TOKEN environment variable set")
+        sys.exit(1)
 
     github_service = GithubService(github_token, MINISTRY_OF_JUSTICE, ENTERPRISE)
     slack_service = SlackService(slack_token)
