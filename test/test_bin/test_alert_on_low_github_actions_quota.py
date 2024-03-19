c14524e4022d9351ee1a0947b97b89cc44900b36
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-03-19 11:44:07 +0000

    Removed unnecessary f strings and reformatted file

--- test/test_bin/test_alert_on_low_github_actions_quota.py
+++ test/test_bin/test_alert_on_low_github_actions_quota.py
@@ -3,7 +3,7 @@ from unittest.mock import patch, MagicMock
 
 from bin.alert_on_low_github_actions_quota import (
     low_threshold_triggered_message,
-    alert_on_low_quota 
+    alert_on_low_quota
 )
 
 from services.github_service import GithubService
@@ -14,7 +14,7 @@ class TestGithubACtionsQuotaAlerting(unittest.TestCase):
 
     def test_low_threshold_triggered_message(self):
 
-        self.assertEqual(low_threshold_triggered_message(10), f"Warning:\n\n 90% of the Github Actions minutes quota remains.")
+        self.assertEqual(low_threshold_triggered_message(10), "Warning:\n\n 90% of the Github Actions minutes quota remains.")
 
     @patch("gql.transport.aiohttp.AIOHTTPTransport.__new__", new=MagicMock)
     @patch("gql.Client.__new__", new=MagicMock)
@@ -24,23 +24,24 @@ class TestGithubACtionsQuotaAlerting(unittest.TestCase):
     @patch.object(SlackService, "send_message_to_plaintext_channel_name")
     @patch.object(GithubService, "check_if_gha_minutes_quota_is_low")
     @patch('os.environ')
-    def test_alert_on_low_quota_if_low(self, 
+    def test_alert_on_low_quota_if_low(
+        self,
         mock_env,
         mock_check_if_gha_minutes_quota_is_low,
         mock_send_message_to_plaintext_channel_name,
         mock_modify_gha_minutes_quota_threshold,
         mock_low_threshold_triggered_message,
-        mock_github_client_core_api
+        _mock_github_client_core_api
     ):
 
         mock_env.get.side_effect = lambda k: 'mock_token' if k in ['GH_TOKEN', 'ADMIN_SLACK_TOKEN'] else None
-        mock_check_if_gha_minutes_quota_is_low.return_value = { 'threshold': 70, 'percentage_used': 75 }
-        mock_low_threshold_triggered_message.return_value = f"Warning:\n\n 25% of the Github Actions minutes quota remains."
+        mock_check_if_gha_minutes_quota_is_low.return_value = {'threshold': 70, 'percentage_used': 75}
+        mock_low_threshold_triggered_message.return_value = "Warning:\n\n 25% of the Github Actions minutes quota remains."
 
         alert_on_low_quota()
 
         mock_check_if_gha_minutes_quota_is_low.assert_called_once()
-        mock_send_message_to_plaintext_channel_name.assert_called_once_with(f"Warning:\n\n 25% of the Github Actions minutes quota remains.", "operations-engineering-alerts")
+        mock_send_message_to_plaintext_channel_name.assert_called_once_with("Warning:\n\n 25% of the Github Actions minutes quota remains.", "operations-engineering-alerts")
         mock_modify_gha_minutes_quota_threshold.assert_called_once_with(80)
 
     @patch("gql.transport.aiohttp.AIOHTTPTransport.__new__", new=MagicMock)
@@ -50,12 +51,13 @@ class TestGithubACtionsQuotaAlerting(unittest.TestCase):
     @patch.object(SlackService, "send_message_to_plaintext_channel_name")
     @patch.object(GithubService, "check_if_gha_minutes_quota_is_low")
     @patch('os.environ')
-    def test_alert_on_low_quota_if_not_low(self,
-        mock_env, 
+    def test_alert_on_low_quota_if_not_low(
+        self,
+        mock_env,
         mock_check_if_gha_minutes_quota_is_low,
         mock_send_message_to_plaintext_channel_name,
         mock_modify_gha_minutes_quota_threshold,
-        mock_github_client_core_api
+        _mock_github_client_core_api
     ):
 
         mock_env.get.side_effect = lambda k: 'mock_token' if k in ['GH_TOKEN', 'ADMIN_SLACK_TOKEN'] else None
@@ -66,6 +68,7 @@ class TestGithubACtionsQuotaAlerting(unittest.TestCase):
         mock_check_if_gha_minutes_quota_is_low.assert_called_once()
         assert not mock_send_message_to_plaintext_channel_name.called
         assert not mock_modify_gha_minutes_quota_threshold.called
-        
+
+
 if __name__ == '__main__':
     unittest.main()
