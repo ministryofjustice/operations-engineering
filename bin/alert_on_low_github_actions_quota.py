33766d0eb224ab8b60e3777848bdde9efd689d96
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-03-19 11:15:52 +0000

    E712: using 'is not' for singleton comparison, reformatted imports and general spacing

--- bin/alert_on_low_github_actions_quota.py
+++ bin/alert_on_low_github_actions_quota.py
@@ -1,11 +1,13 @@
-from services.github_service import GithubService
-from config.constants import ENTERPRISE, MINISTRY_OF_JUSTICE, SLACK_CHANNEL
 import os
 import sys
+from services.github_service import GithubService
 from services.slack_service import SlackService
+from config.constants import ENTERPRISE, MINISTRY_OF_JUSTICE, SLACK_CHANNEL
+
 
 def low_threshold_triggered_message(percentage_used):
     return f"Warning:\n\n {round(100 - percentage_used, 1)}% of the Github Actions minutes quota remains."
+
 
 def alert_on_low_quota():
     github_token = os.environ.get("GH_TOKEN")
@@ -22,9 +24,10 @@ def alert_on_low_quota():
 
     check_result = github_service.check_if_gha_minutes_quota_is_low()
 
-    if check_result != False:
+    if check_result is not False:
         slack_service.send_message_to_plaintext_channel_name(low_threshold_triggered_message(check_result['percentage_used']), SLACK_CHANNEL)
         github_service.modify_gha_minutes_quota_threshold(check_result['threshold'] + 10)
+
 
 if __name__ == "__main__":
     alert_on_low_quota()
