66ad168d8ee0c11321e6021bca76ee3f789c83c8
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-03-19 16:56:37 +0000

    unit tests

--- test/test_bin/test_alert_on_low_github_actions_quota.py
+++ test/test_bin/test_alert_on_low_github_actions_quota.py
@@ -14,7 +14,7 @@ class TestGithubACtionsQuotaAlerting(unittest.TestCase):
 
     def test_low_threshold_triggered_message(self):
 
-        self.assertEqual(low_threshold_triggered_message(10), "Warning:\n\n 90% of the Github Actions minutes quota remains.")
+        self.assertEqual(low_threshold_triggered_message(10), "Warning:\n\n 90% of the Github Actions minutes quota remains.\n\n What to do next: https://runbooks.operations-engineering.service.justice.gov.uk/documentation/internal/low-github-actions-minutes-procedure.html#low-github-actions-minutes-procedure")
 
     @patch("gql.transport.aiohttp.AIOHTTPTransport.__new__", new=MagicMock)
     @patch("gql.Client.__new__", new=MagicMock)
@@ -36,12 +36,12 @@ class TestGithubACtionsQuotaAlerting(unittest.TestCase):
 
         mock_env.get.side_effect = lambda k: 'mock_token' if k in ['GH_TOKEN', 'ADMIN_SLACK_TOKEN'] else None
         mock_check_if_gha_minutes_quota_is_low.return_value = {'threshold': 70, 'percentage_used': 75}
-        mock_low_threshold_triggered_message.return_value = "Warning:\n\n 25% of the Github Actions minutes quota remains."
+        mock_low_threshold_triggered_message.return_value = "Warning:\n\n 25% of the Github Actions minutes quota remains.\n\n What to do next: https://runbooks.operations-engineering.service.justice.gov.uk/documentation/internal/low-github-actions-minutes-procedure.html#low-github-actions-minutes-procedure"
 
         alert_on_low_quota()
 
         mock_check_if_gha_minutes_quota_is_low.assert_called_once()
-        mock_send_message_to_plaintext_channel_name.assert_called_once_with("Warning:\n\n 25% of the Github Actions minutes quota remains.", "operations-engineering-alerts")
+        mock_send_message_to_plaintext_channel_name.assert_called_once_with("Warning:\n\n 25% of the Github Actions minutes quota remains.\n\n What to do next: https://runbooks.operations-engineering.service.justice.gov.uk/documentation/internal/low-github-actions-minutes-procedure.html#low-github-actions-minutes-procedure", "operations-engineering-alerts")
         mock_modify_gha_minutes_quota_threshold.assert_called_once_with(80)
 
     @patch("gql.transport.aiohttp.AIOHTTPTransport.__new__", new=MagicMock)
