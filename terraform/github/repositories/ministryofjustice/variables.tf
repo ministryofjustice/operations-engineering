0dc9eaf93570328e3b3ca87200013ab115df119c
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-04-02 10:20:58 +0100

    terraform fmt

--- terraform/github/repositories/ministryofjustice/variables.tf
+++ terraform/github/repositories/ministryofjustice/variables.tf
@@ -14,13 +14,3 @@ variable "operations_engineering_team_id" {
   description = "operations-engineering team id"
   type        = string
 }
-
-variable "operations_engineering_test_team_id" {
-  description = "operations-engineering-test team id"
-  type        = string
-}
-
-variable "test_team_access_team_id" {
-  description = "test-team-access team id"
-  type        = string
-}
