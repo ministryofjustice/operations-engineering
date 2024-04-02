18aaa338c7ad7554a0cdcfef6d91dfed2c672d0f
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-04-02 13:44:04 +0100

    try moving test repo resources into new parent module

---
+++ terraform/github/repositories/moved.tf
@@ -0,0 +1,14 @@
+moved {
+  from = module.test-repo-levg.github_branch_protection.default
+  to   = module.ministryofjustice-test.module.test-repo-levg.github_branch_protection.default
+}
+
+moved {
+  from = module.test-repo-levg.github_repository.default
+  to   = module.ministryofjustice-test.module.test-repo-levg.github_repository.default
+}
+
+moved {
+  from = module.test-repo-levg.github_repository_tag_protection.default
+  to   = module.ministryofjustice-test.module.test-repo-levg.github_repository_tag_protection.default
+}
