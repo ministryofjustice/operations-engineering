f9c72f0e43c2e1d24b137107af5c2f60f5480cae
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-04-04 15:06:03 +0100

    :sparkles: Created script to handle threshold calculation and alerts

---
+++ bin/alert_on_low_gandi_funds.py
@@ -0,0 +1,23 @@
+import os
+from services.gandi_service import GandiService
+
+GANDI_FUND_THRESHOLD = 500
+
+
+def send_gandi_alert_to_slack(remaining_gandi_funds):
+    print(f'Sending alert to slack as funds are low: {remaining_gandi_funds}')
+
+
+def alert_on_low_gandi_funds():
+    gandi_token = os.environ.get("GANDI_FUNDS_TOKEN")
+    gandi_service = GandiService(gandi_token, "v5/billing/info/")
+    organisation_id = "ministryofjustice"
+
+    remaining_gandi_funds = gandi_service.get_current_account_balance_from_org(organisation_id)
+
+    if remaining_gandi_funds < GANDI_FUND_THRESHOLD:
+        send_gandi_alert_to_slack(remaining_gandi_funds)
+
+
+if __name__ == "__main__":
+    alert_on_low_gandi_funds()
