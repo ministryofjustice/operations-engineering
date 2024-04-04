42ffe3ec295a92df98ef86269c7a544ba3edae74
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-04-04 14:04:11 +0100

    refactor

--- services/github_service.py
+++ services/github_service.py
@@ -270,10 +270,11 @@ class GithubService:
 
     @retries_github_rate_limit_exception_at_next_reset_once
     def get_users_of_multiple_organisations(self, organisations: list) -> list:
-        users = []
+        all_users = []
         for org in organisations:
-            [users.append(user['login']) for user in self.github_client_core_api.get_organization(org).get_members() if user['login'] not in users]
-        return users
+            org_users = [user["login"] for user in self.github_client_core_api.get_organization(org).get_members() if user['login'] not in all_users]
+            all_users = all_users + org_users
+        return all_users
 
     @retries_github_rate_limit_exception_at_next_reset_once
     def __add_user_to_team(self, user: NamedUser, team_id: int) -> None:
