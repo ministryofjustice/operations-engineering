5cf08bb3305427f1e3ee06d8c5d282b0367db4a4
Sam Pepper <124164618+PepperMoJ@users.noreply.github.com>
2024-04-05 13:47:14 +0100

    Gandi Low Funding Alert (#4365)
    * :sparkles: New blank script file added to ensure action will run
    
    * :sparkles: New GitHub Action created to run the low gandi funds script each day.
    
    * :hammer: Fixing Glynnter issues
    
    * :negative_squared_cross_mark: Removed schedule
    
    * :sparkles: Created Gandi service to handle interactions with Gandi
    
    * :sparkles: Created script to handle threshold calculation and alerts
    
    * :recycle: Changed org id to use environment variable
    
    * :recycle: Updated token reference
    
    * :recycle: Changed threshold for testing purposes
    
    * :recycle: Included posting of slack message to default operations engineering channel
    
    * :recycle: Schdule re-added to main action
    
    * :recycle: Threshold reset to default value and message altered for better spacing
    
    * 🧪 Test coverage added for all new code
    
    * :negative_squared_cross_mark: Removed print leftover from testing
    
    * :recycle: Added check for GANDI_ORG_ID

---
+++ services/gandi_service.py
@@ -0,0 +1,20 @@
+import requests
+
+
+class GandiService:
+    def __init__(self, token, url_extension) -> None:
+        self.headers = {'Authorization': f'Bearer {token}'}
+        self.url = "https://api.gandi.net/" + url_extension
+
+    def get_current_account_balance_from_org(self, org_id):
+        try:
+            response = requests.get(
+                url=self.url + org_id, headers=self.headers, timeout=60)
+            response.raise_for_status()
+            return float(response.json()['prepaid']['amount'])
+        except requests.exceptions.HTTPError as authentication_error:
+            raise requests.exceptions.HTTPError(
+                f"You may need to export your Gandi API key:\n {authentication_error}") from authentication_error
+        except TypeError as api_key_error:
+            raise TypeError(
+                f"Gandi API key does not exist or is in the wrong format:\n {api_key_error}") from api_key_error
