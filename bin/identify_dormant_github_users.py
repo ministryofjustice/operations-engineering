1b9db65a31a1319dfd9890555a75f23a83e607b4
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-04-09 16:18:50 +0100

    :recycle: Update Dormancy Threshold and Extraction Mechanism
    This commit changes the process by which dormant GitHub users are identified. The dormancy threshold was updated from 90 to 30 days, meaning inactivity is now detected over a shorter period.
    
    The token retrieval method has been altered to use environment variables for increased security, while the retrieval of users was refactored to directly identify inactive users from the enterprise audit log.
    
    A date calculation function was added to facilitate the process. The changes should result in more reliable identification of genuinely dormant GitHub users.

--- bin/identify_dormant_github_users.py
+++ bin/identify_dormant_github_users.py
@@ -1,6 +1,16 @@
+import os
+from datetime import datetime, timedelta
+
 from services.github_service import GithubService
 
-DORMANT_USER_THRESHOLD = 90
+DORMANT_USER_THRESHOLD = 30
+
+
+def _calculate_date(in_last_days: int) -> str:
+    current_date = datetime.now()
+    date = current_date - timedelta(days=in_last_days)
+    timestamp_format = "%Y-%m-%d"
+    return date.strftime(timestamp_format)
 
 
 def setup_environment() -> tuple[str, str]:
@@ -19,12 +29,12 @@ def identify_dormant_users(github_users: list[str], audit_logs: list[str]) -> li
 
 
 def identify_dormant_github_users():
-    github_token, auth0_token = setup_environment()
+    github_token = os.getenv("GH_ADMIN_TOKEN")
 
     github = GithubService(github_token, "ministryofjustice")
-    all_users = github.get_users_of_multiple_organisations(["ministryofjustice", "moj-analytical-services"])
+    active_github_users = github.get_all_inactive_users_in_enterprise_audit_log_since_date(
+        _calculate_date(DORMANT_USER_THRESHOLD))
 
-    active_github_users = github.get_audit_log_active_users()
     # get list of all users from both organisations
     # get github and auth0 audit logs
     # identify users that don't appear in the logs
