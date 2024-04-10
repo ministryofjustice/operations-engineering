378840d95e141fec4f99927bc7e909831324ca8e
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-04-10 13:40:02 +0100

    tf fmt

--- terraform/dsd/cloudtrail_data_lake/providers.tf
+++ terraform/dsd/cloudtrail_data_lake/providers.tf
@@ -1,5 +1,12 @@
 terraform {
   required_version = ">= 1.3.0"
+  backend "s3" {
+    acl     = "private"
+    bucket  = "operations-engineering-test-terraform-state-bucket"
+    encrypt = true
+    key     = "terraform/dsd/cloudtrail_data_lake/terraform.tfstate"
+    region  = "eu-west-2"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
