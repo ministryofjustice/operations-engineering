7a0af0cc5cbe4c9beba92306fd126db62cc40b04
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-04-10 17:03:35 +0100

    use module from branch where lambdas are in module

--- terraform/dsd/cloudtrail_data_lake/main.tf
+++ terraform/dsd/cloudtrail_data_lake/main.tf
@@ -1,5 +1,5 @@
 module "github-cloudtrail-auditlog" {
-  source                          = "github.com/ministryofjustice/operations-engineering-cloudtrail-lake-github-audit-log-terraform-module/terraform/terraform-aws-cloudtrail-lake-github-audit-log"
+  source                          = "github.com/ministryofjustice/operations-engineering-cloudtrail-lake-github-audit-log-terraform-module/terraform/terraform-aws-cloudtrail-lake-github-audit-log?ref=move-lambdas-back"
   create_github_auditlog_s3bucket = var.create_github_auditlog_s3bucket
   github_auditlog_s3bucket        = var.github_auditlog_s3bucket
   cloudtrail_lake_channel_arn     = var.cloudtrail_lake_channel_arn
