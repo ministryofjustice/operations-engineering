7d5368614b82ad350f9a3ac843addee6384a6565
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-04-10 13:49:15 +0100

    amend module source

--- terraform/dsd/cloudtrail_data_lake/main.tf
+++ terraform/dsd/cloudtrail_data_lake/main.tf
@@ -1,5 +1,5 @@
 module "github-cloudtrail-auditlog" {
-  source                          = "github.com/aws-samples/aws-cloudtrail-lake-github-audit-log/tree/main/terraform/terraform-aws-cloudtrail-lake-github-audit-log"
+  source                          = "github.com/aws-samples/aws-cloudtrail-lake-github-audit-log/terraform/terraform-aws-cloudtrail-lake-github-audit-log"
   create_github_auditlog_s3bucket = var.create_github_auditlog_s3bucket
   github_auditlog_s3bucket        = var.github_auditlog_s3bucket
   cloudtrail_lake_channel_arn     = var.cloudtrail_lake_channel_arn
