d0f418e2b37c5b022b5b3c6085b6c172a5c4a9e5
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-04-12 14:52:21 +0100

    remove mypy

--- bin/alert_on_low_github_actions_quota.py
+++ bin/alert_on_low_github_actions_quota.py
@@ -19,14 +19,21 @@ def alert_on_low_quota():
         print("No SLACK_TOKEN environment variable set")
         sys.exit(1)
 
-    github_service = GithubService(github_token, MINISTRY_OF_JUSTICE, ENTERPRISE)
+    github_service = GithubService(
+        github_token, MINISTRY_OF_JUSTICE, ENTERPRISE
+    )
     slack_service = SlackService(slack_token)
 
     check_result = github_service.check_if_gha_minutes_quota_is_low()
 
     if check_result is not False:
-        slack_service.send_message_to_plaintext_channel_name(low_threshold_triggered_message(check_result['percentage_used']), SLACK_CHANNEL)
-        github_service.modify_gha_minutes_quota_threshold(check_result[ 'threshold' ] + 10)
+        slack_service.send_message_to_plaintext_channel_name(
+            low_threshold_triggered_message(check_result["percentage_used"]),
+            SLACK_CHANNEL,
+        )
+        github_service.modify_gha_minutes_quota_threshold(
+            check_result["threshold"] + 10
+        )
 
 
 if __name__ == "__main__":
