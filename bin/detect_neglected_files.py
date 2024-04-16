d125696012609ecb6787f28ff04d80074470f7f0
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-04-16 13:16:55 +0100

    trigger Slack alert

--- bin/detect_neglected_files.py
+++ bin/detect_neglected_files.py
@@ -5,8 +5,8 @@ from services.slack_service import SlackService
 from config.constants import ENTERPRISE, MINISTRY_OF_JUSTICE, SLACK_CHANNEL
 
 
-# def low_threshold_triggered_message(percentage_used):
-#     return f"Warning:\n\n {round(100 - percentage_used, 1)}% of the Github Actions minutes quota remains.\n\n What to do next: https://runbooks.operations-engineering.service.justice.gov.uk/documentation/internal/low-github-actions-minutes-procedure.html#low-github-actions-minutes-procedure"
+def construct_neglected_files_slack_message(paths_to_review: str, repo: str):
+    return f"The following paths in the {repo} repository are due for review:\n\n{"\n".join(paths_to_review)}"
 
 
 def detect_neglected_files():
@@ -14,17 +14,20 @@ def detect_neglected_files():
     if github_token is None:
         print("No GITHUB_TOKEN environment variable set")
         sys.exit(1)
-    # slack_token = os.environ.get("ADMIN_SLACK_TOKEN")
-    # if slack_token is None:
-    #     print("No SLACK_TOKEN environment variable set")
-    #     sys.exit(1)
+    slack_token = os.environ.get("ADMIN_SLACK_TOKEN")
+    if slack_token is None:
+        print("No SLACK_TOKEN environment variable set")
+        sys.exit(1)
 
     github_service = GithubService(github_token, MINISTRY_OF_JUSTICE, ENTERPRISE)
-    # slack_service = SlackService(slack_token)
+    slack_service = SlackService(slack_token)
 
-    paths_to_review = github_service.detect_neglected_files_in_multiple_directories("operations-engineering", ["bin"])
+    repo = "operations-engineering"
 
-    print(paths_to_review)
+    paths_to_review = github_service.detect_neglected_files_in_multiple_directories(repo, ["bin"])
+
+    if len(paths_to_review) > 1:
+        slack_service.send_message_to_plaintext_channel_name(construct_neglected_files_slack_message(paths_to_review, repo), SLACK_CHANNEL)
 
 
 if __name__ == "__main__":
