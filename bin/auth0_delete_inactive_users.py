040e307126fd6160de4cad85c63ea3ce958a7f15
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-04-18 15:19:53 +0100

    **:recycle: Extract Auth0 client initialisation into separate function**
    This commit introduces a new `get_auth0_client_details()` function to handle the extraction and validation of necessary environment variables for initializing the Auth0 client. It checks the presence of `AUTH0_CLIENT_SECRET`, `AUTH0_CLIENT_ID`, and `AUTH0_DOMAIN` environment variables, raising an exception if they are not set.
    
    Also, this commit updates the main function to use this new method for improving readability and encapsulating the logic for getting client details.
    
    In addition, corresponding unit tests have been added to verify the behavior of the new function when all required environment variables are present and when they are missing.

--- bin/auth0_delete_inactive_users.py
+++ bin/auth0_delete_inactive_users.py
@@ -1,4 +1,5 @@
 import os
+
 from services.auth0_service import Auth0Service
 
 
@@ -10,9 +11,27 @@ def delete_inactive_users(auth0_client: Auth0Service):
         auth0_client.delete_user(user["user_id"])
 
 
+def get_auth0_client_details() -> tuple[str, str, str]:
+    client_secret = os.getenv('AUTH0_CLIENT_SECRET')
+    client_id = os.getenv('AUTH0_CLIENT_ID')
+    domain = os.getenv('AUTH0_DOMAIN')
+
+    if not all([client_secret, client_id, domain]):
+        raise ValueError(
+            'Required environment variables AUTH0_CLIENT_SECRET, AUTH0_CLIENT_ID, or AUTH0_DOMAIN are not set')
+
+    return client_secret, client_id, domain
+
+
 def main():
-    auth0_client = Auth0Service(os.getenv("AUTH0_CLIENT_SECRET"), os.getenv(
-        "AUTH0_CLIENT_ID"), os.getenv("AUTH0_DOMAIN"), "client_credentials")
+    auth0_client_secret, auth0_client_id, auth0_domain = get_auth0_client_details()
+
+    auth0_client = Auth0Service(
+        client_secret=auth0_client_secret,
+        client_id=auth0_client_id,
+        domain=auth0_domain,
+        grant_type="client_credentials"
+    )
     delete_inactive_users(auth0_client)
 
 
--- test/test_bin/test_auth0_delete_inactive_users.py
+++ test/test_bin/test_auth0_delete_inactive_users.py
@@ -1,8 +1,12 @@
+import os
 import unittest
-from unittest.mock import patch, MagicMock
-from bin.auth0_delete_inactive_users import delete_inactive_users, main
+from unittest.mock import MagicMock, patch
+
+from bin.auth0_delete_inactive_users import (delete_inactive_users,
+                                             get_auth0_client_details, main)
 
 
+@patch.dict(os.environ, {"AUTH0_CLIENT_SECRET": "secret", "AUTH0_CLIENT_ID": "id", "AUTH0_DOMAIN": "domain"})
 class TestAuth0DeleteInactiveUsers(unittest.TestCase):
 
     @patch("services.auth0_service.Auth0Service")
@@ -23,6 +27,20 @@ class TestAuth0DeleteInactiveUsers(unittest.TestCase):
     def test_main(self, mock_delete_inactive_users):
         main()
         mock_delete_inactive_users.assert_called_once()
+
+
+class TestGetAuth0ClientDetails(unittest.TestCase):
+    @patch.dict(os.environ, {"AUTH0_CLIENT_SECRET": "secret", "AUTH0_CLIENT_ID": "id", "AUTH0_DOMAIN": "domain"})
+    def test_get_auth0_client_details_success(self):
+        # Test that the function returns the correct values when the environment variables are set
+        self.assertEqual(get_auth0_client_details(),
+                         ("secret", "id", "domain"))
+
+    @patch.dict(os.environ, {"AUTH0_CLIENT_SECRET": "", "AUTH0_CLIENT_ID": "", "AUTH0_DOMAIN": ""}, clear=True)
+    def test_get_auth0_client_details_failure(self):
+        # Test that the function raises a ValueError when the environment variables are not set
+        with self.assertRaises(ValueError):
+            get_auth0_client_details()
 
 
 if __name__ == "__main__":
