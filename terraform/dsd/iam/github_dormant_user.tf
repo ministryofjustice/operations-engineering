c74ac74e68a1941c7d527ef9f37c144b5760bcde
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-04-22 15:36:37 +0100

    :recycle: Refactor IAM Policy and Role for GitHub Dormant User
    This commit updates the AWS IAM policy description to more accurately reflect that it is associated with the role for GitHub Dormant Users, rather than a user.
    
    It also refactors the `github_dormant_user` terraform module by removing the `aws_iam_user` resource and its policy attachment. All of this functionality is now handled by the `github_dormant_user_role` and attached `github_dormant_user_policy`, making the application of policies to users less complex and easier to manage.
    
    Moreover, the assume_role_policy for `github_dormant_user_role` has been updated to refer to a pre-existing IAM policy document, reducing redundancy in the codebase.

--- terraform/dsd/iam/github_dormant_user.tf
+++ terraform/dsd/iam/github_dormant_user.tf
@@ -1,10 +1,6 @@
-resource "aws_iam_user" "github_dormant_user" {
-  name = "operations_engineering_github_dormant_user"
-}
-
 resource "aws_iam_policy" "github_dormant_user_policy" {
   name        = "DormantUserS3Access"
-  description = "Policy to grant S3 access to github_dormant_user"
+  description = "Policy to grant S3 access to the GitHub Dormant User role"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -18,25 +14,10 @@ resource "aws_iam_policy" "github_dormant_user_policy" {
   })
 }
 
-# This block creates an IAM role named `github_dormant_user_role`.
-# The `assume_role_policy` is set to allow EC2 instances to assume this role.
-# This is specified by the `Service = "ec2.amazonaws.com"` line in the `Principal` block.
-# If a different AWS service or entity needs to assume the role, this value should be replaced with the appropriate service principal.
 resource "aws_iam_role" "github_dormant_user_role" {
   name = "github_dormant_user_role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        },
-        Effect = "Allow",
-      },
-    ]
-  })
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy_document.json
 }
 
 resource "aws_iam_role_policy_attachment" "role_policy_attachment" {
@@ -48,8 +29,4 @@ resource "github_actions_secret" "aws_role_arn" {
   repository      = "operations-engineering"
   secret_name     = "GH_DORMANT_USER_AWS_ROLE_ARN"
   plaintext_value = aws_iam_role.github_dormant_user_role.arn
-}
-resource "aws_iam_user_policy_attachment" "user_policy_attachment" {
-  user       = aws_iam_user.github_dormant_user.name
-  policy_arn = aws_iam_policy.github_dormant_user_policy.arn
 }
