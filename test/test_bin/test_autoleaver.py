3bfbbfbf9b0827fd7cf638cf7d125d9a5d1cec1b
Vijay <158477216+vijaykannan21@users.noreply.github.com>
2024-05-07 13:09:12 +0100

    modified the code

--- test/test_bin/test_autoleaver.py
+++ test/test_bin/test_autoleaver.py
@@ -1,48 +1,115 @@
 import os
 import requests
 
-# Get the credentials from the environment variables
-leaver_username = os.gettenv("LEAVER_USERNAME")
-github_token = os.getenv("GITHUB_TOKEN")
-onepassword_token = os.getenv("ONEPASSWORD_TOKEN")
-gandi_api_key = os.getenv("GANDI_API_KEY")
-os_data_hub_api_key = os.getenv("OS_DATA_HUB_API_KEY")
-google_group_client_id = os.getenv("GOOGLE_GROUPS_CLIENT_ID")
-auth0_api_key = os.getenv("AUTH0_API_KEY")
+# Get  credentials from the environment variables
+leaver_username = os.getenv("INPUT_LEAVER_USERNAME")
+github_token = os.getenv("INPUT_GITHUB_TOKEN")
+onepassword_token = os.getenv("INPUT_ONEPASSWORD_TOKEN")
+gandi_api_key = os.getenv("INPUT_GANDI_API_KEY")
+os_data_hub_api_key = os.getenv("INPUT_OS_DATA_HUB_API_KEY")
+google_group_client_id = os.getenv("INPUT_GOOGLE_GROUPS_CLIENT_ID")
+auth0_api_key = os.getenv("INPUT_AUTH0_API_KEY")
 
 def remove_from_github():
     headers ={"Authorization": f"Bearer {github_token}"}
-    response =requests.delete(f"https://api.github.com/org/ministryofjustice/members/{leavers_username}", headers=headers)
-    assert response.status_code ==204
-    response =requests.delete(f"https://api.github.com/orgs/moj-analytical-services/members/{leavers_username}", headers=headers)
-    assert response.status_code ==204
-    response =requests.delete(f"https://api.github.com/orgs/ministryofjustice-test/members/{leavers_username}", headers=headers)
-    assert response.status_code ==204
+    urls = [
+         f"https://api.github.com/org/ministryofjustice/members/{leaver_username}",
+         f"https://api.github.com/orgs/moj-analytical-services/members/{leaver_username}",
+         f"https://api.github.com/orgs/ministryofjustice-test/members/{leaver_username}",
+    ]
+    for url in urls:
+        try:
+            response = requests.delete(url, headers=headers)
+            response.raise_for_status()
+        except requests.exceptions.RequestsException as err:
+            print(f"Error: {err}")
+        except requests.exceptions.HTTPError as errh:
+            print(f"Http Error: {errh}")
+        except requests.exceptions.ConnectionError as errc:
+            print(f"Error Connecting {errc}")
+        except requests.exceptions.Timeout as errt:
+            print(f"Timeout Error: {errt}")   
+            
+def remove_from_google_groups():
+    headers = {"Authorization": f"Bearer {google_group_client_id}"}
+    urls = [
+          f"https://group.google.com/a/digital.justice.gov.uk/g/opertatioans-engineering/members/{leaver_username}",
+          f"https://group.google.com/a/digital.justice.gov.uk/g/domain/members{leaver_username}",
+    ]    
+    for url in urls:
+        try:
+            response = requests.delete(url, headers=headers)
+            response.raise_for_status()
+        except requests.exceptions.RequestsException as err:
+            print(f"Error: {err}")
+        except requests.exceptions.RequestsException as errh:
+            print(f"Http Error: {errh}")
+        except requests.exceptions.ConnectionError as errc:
+            print(f"Error Connectiong {errc}")
+        except requests.exceptions.Timeout as errt:
+            print(f"Timeout Error: {errt}")   
+                                
     
 def remove_from_1password():
     headers = {"Authorization": f"Bearer {onepassword_token}"}
-    response = requests.delete(f"https://ministryofjustice.1password.eu/v1/users/{leavers_username}",headers=headers)
-    assert response.status_code == 204
-    
+    try:
+        response = requests.delete(f"https://ministryofjustice.1password.eu/v1/users/{leavers_username}",headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.RequestsException as err:
+        print(f"Error: {err}")
+    except requests.exceptions.HTTPError as errh:
+        print(f"Http Error: {errh}")
+    except requests.exceptions.ConnectionError as errc:
+        print(f"Error Connectiong {errc}")
+    except requests.exceptions.Timeout as errt:
+        print(f"Timeout Error: {errt}")  
+                  
 def remove_from_gandi():
     headers = {"Authorization": f"Bearer {gandi_api_key}"}
-    response = requests.delete(f"https://api.gandi.net/v5/domai/users/{leaver_username}", headers=headers)
-    assert response.status_code == 204
-    
+    try:
+        response = requests.delete(f"https://api.gandi.net/v5/domai/users/{leaver_username}", headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        print(f"Error: {err}")
+    except requests.exceptions.HTTPError as errh:
+        print(f"Http Error: {errh}")
+    except requests.exceptions.ConnectionError as errc:
+        print(f"Error Connecting: {errc}")
+    except requests.exceptions.Timeout as errt:
+        print(f"Timeout Error: {errt}")
+                                        
 def remove_from_os_data_hub():
     headers = {"Authorization": f"Bearer {os_data_hub_api_key}"}
-    response = requests.delete(f"https://osdatahub.os.uk/api/v1/users/{leaver_username}",headers=headers)
-    assert response.status_code ==204
+    try:
+        response = requests.delete(f"https://osdatahub.os.uk/api/v1/users/{leaver_username}",headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        print(f"Error: {err}")
+    except requests.exceptions.HTTPError as errh:
+        print(f"Http Error: {errh}")
+    except requests.exceptions.ConnectionError as errc:
+        print(f"Error Connecting: {errc}")
+    except requests.exceptions.Timeout as errt:
+        print(f"Timeout Error: {errt}")    
     
-def remove_from_google_groups():
-    headers = {"Authorization": f"Bearer {google_group_client_id}"}
-    response = requests.delete(f"https://group.google.com/a/digital.justice.gov.uk/g/opertatioans-engineering/members/{leaver_username}", headers=headers)
-    assert response.status_code ==204
-    response =requests.delete(f"https://group.google.com/a/digital.justice.gov.uk/g/domain/members{leaver_username}", headers=headers)
-    assert response.status_code ==204
-
+    
 def remove_from_auth0():
     headers = {"Authorization": f"Bearer {auth0_api_key}"}
-    response = requests.delete(f"https://api_auth0_domain.auth0.com/api/v2/users/{leavers_username}",headers=headers)
-    assert response.status_code ==204                   
-
+    try:
+        response = requests.delete(f"https://api_auth0_domain.auth0.com/api/v2/users/{leavers_username}",headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        print(f"Error: {err}")
+    except requests.exceptions.HTTPError as errh:
+        print(f"Http Error: {errh}")
+    except requests.exceptions.ConnectionError as errc:
+        print(f"Error Connecting: {errc}")
+    except requests.exceptions.Timeout as errt:
+        print(f"Timeout Error: {errt}")                         
+if __name__ == "__main__":
+    remove_from_github()
+    remove_from_google_groups()
+    remove_from_gandi()
+    remove_from_os_data_hub()
+    remove_from_1password()
+    remove_from_auth0()
