548ede28acae3360fbf0c488ad60ab5d74786405
Jason Birchall <31217584+jasonBirchall@users.noreply.github.com>
2024-05-08 15:44:58 +0100

    :bug: Change Terraform resource name

--- terraform/dsd/iam/octodns_role.tf
+++ terraform/dsd/iam/octodns_role.tf
@@ -34,7 +34,7 @@ resource "aws_iam_role" "octodns_role" {
   assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role_policy_document.json
 }
 
-resource "aws_iam_role_policy_attachment" "role_policy_attachment" {
+resource "aws_iam_role_policy_attachment" "octo_dns_role_policy_attachment" {
   role       = aws_iam_role.octodns_role.name
   policy_arn = aws_iam_policy.dns_change_records_policy.arn
 }
