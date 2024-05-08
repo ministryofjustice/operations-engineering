61e84c31c71c5e1f9ccf4e0c30e81fc675d20733
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-05-08 15:36:39 +0100

    :sparkles: Create IAM policy and role for OctoDNS in Route53
    Added a new Terraform configuration for AWS IAM to support the OctoDNS tool. This change introduces an IAM policy `Route53ChangeRecordsPolicy` to allow modification of Route53 record sets specifically for `dns-test.service.justice.gov.uk`. Additionally, an IAM role `octodns_role` is created to be assumed by GitHub Actions with the necessary permissions attached via `role_policy_attachment`. A GitHub Actions secret `OCTODNS_AWS_ROLE_ARN` is also set up to store the role ARN for use in CI/CD pipelines.

---
+++ terraform/dsd/iam/octodns_role.tf
@@ -0,0 +1,46 @@
+
+resource "aws_iam_policy" "dns_change_records_policy" {
+  name        = "Route53ChangeRecordsPolicy"
+  description = "A policy that allows modification of the Route53 record sets for the dns-test.service.justice.gov.uk hosted zone."
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "route53:ChangeResourceRecordSets",
+        "route53:ListResourceRecordSets"
+      ],
+      "Resource": "arn:aws:route53:::hostedzone/test-dns.service.justice.gov.uk"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "route53:GetHostedZone",
+        "route53:ListHostedZones"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "octodns_role" {
+  name = "octodns_role"
+
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role_policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "role_policy_attachment" {
+  role       = aws_iam_role.octodns_role.name
+  policy_arn = aws_iam_policy.dns_change_records_policy.arn
+}
+
+resource "github_actions_secret" "octodns_role" {
+  repository      = "operations-engineering-dns-spike"
+  secret_name     = "OCTODNS_AWS_ROLE_ARN"
+  plaintext_value = aws_iam_role.octodns_role.arn
+}
