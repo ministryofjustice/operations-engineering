448fa1781f04a8d946ee4f3c8aa4426e910d4d0d
Vijay <158477216+vijaykannan21@users.noreply.github.com>
2024-05-10 16:46:50 +0100

    addedpagerdutyand sentry

--- test/test_bin/test_autoleaver.py
+++ test/test_bin/test_autoleaver.py
@@ -9,6 +9,8 @@ gandi_api_key = os.getenv("INPUT_GANDI_API_KEY")
 os_data_hub_api_key = os.getenv("INPUT_OS_DATA_HUB_API_KEY")
 docker_api_token = os.getenv("INPUT_DOCKER_API_TOKEN")
 sentry_api_token = os.getenv("INPUT_SENTRY_API_TOKEN")
+pagerduty_api_token = os.getenv("INPUT_PAGERDUTY_API_TOKEN")
+Pingdom_api_token = os.getenv("INPUT_PINGDOM_API_TOKEN")
 
 
 
@@ -33,6 +35,7 @@ def remove_from_github():
             print(f"Timeout Error: {errt}")   
                                                 
 def remove_from_1password():
+    
     headers = {"Authorization": f"Bearer {onepassword_token}"}
     try:
         response = requests.delete(f"https://ministryofjustice.1password.eu/v1/users/{leavers_username}",headers=headers)
@@ -47,6 +50,7 @@ def remove_from_1password():
         print(f"Timeout Error: {errt}")  
                   
 def remove_from_gandi():
+    
     headers = {"Authorization": f"Bearer {gandi_api_key}"}
     try:
         response = requests.delete(f"https://api.gandi.net/v5/domai/users/{leaver_username}", headers=headers)
@@ -61,6 +65,7 @@ def remove_from_gandi():
         print(f"Timeout Error: {errt}")
                                         
 def remove_from_os_data_hub():
+    
     headers = {"Authorization": f"Bearer {os_data_hub_api_key}"}
     try:
         response = requests.delete(f"https://osdatahub.os.uk/api/v1/users/{leaver_username}",headers=headers)
@@ -75,6 +80,7 @@ def remove_from_os_data_hub():
         print(f"Timeout Error: {errt}")   
         
 def remove_from_docker():
+    
     headers = {"Authorization": f"Bearer {docker_api_token}"}
     try:
         response = requests.delete(f"https://docker.com/api/v1/user/{leavers_username}",headers=headers)
@@ -89,6 +95,7 @@ def remove_from_docker():
         print(f"Timeout Error: {errt}")
 
 def remove_from_sentry():
+    
     headers = {"Authorization": f"Bearer {sentry_api_token}"}
     try:
         response = requests.delete(f"https://sentry.com/api/0/organizations/myorg/members/{leavers_username}", headers=headers)
@@ -101,14 +108,45 @@ def remove_from_sentry():
         print(f"Error Connecting: {errc}")
     except requests.exceptions.Timeout as errt:
         print(f"Timeout Error: {errt}") 
-                             
-                            
-              
+        
+def remove_from_pagerduty():
+    
+    headers = {"Authorization": f"Bearer {pagerduty_api_token}"}
+    try:
+        response = requests.delete(f"https://api.pagerduty.com/incidents/{incident_id}", headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.RequestsExceptions as err:
+        print(f"Error: {err}")
+    except requests.exceptions.HTTPError as errh:
+        print(f"Http Error: {errh}")
+    except requests.exceptions.ConnectionError as errc:
+        print(f"Error Connecting: {errc}")
+    except requests.exceptions.Timeout as errt:
+        print(f"Timeout Error: {errt}")    
+        
+def remove_pingdom():
+    
+    headers = {"Authorization" : f"Bearer {pingdom_api_token}"}
+    try:
+        response = requests.delete(f"https://api.pingdom.com/api/3.1checks/{check_id}", headers=headers)  
+        response.raise_for_status()
+    except requests.exceptions.RequestsExceptions as err:
+        print(f"Error: {err}")
+    except requests.exceptions.HTTPError as errh:
+        print(f"Http Error: {errh}")
+    except requests.exceptions.ConnectionError as errc:
+        print(f"Error Connecting: {errc}")
+    except requests.exceptions.Timeout as errt:
+        print(f"Timeout Error: {errt}")                
+                                           
 if __name__ == "__main__":
     remove_from_github()
     remove_from_gandi()
     remove_from_os_data_hub()
     remove_from_1password()
     remove_from_docker()
+    remove_from_pagerduty()
+    remove_from_pingdom()
+    
     
     
