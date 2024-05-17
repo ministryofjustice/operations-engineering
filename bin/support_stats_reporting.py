26d9b00249f0b6213cd3388f6273a670e8fe517c
andyrogers1973 <andy.rogers@digital.justice.gov.uk>
2024-05-17 11:03:03 +0100

    Change code format
    As per W293, removed whitespace

--- bin/support_stats_reporting.py
+++ bin/support_stats_reporting.py
@@ -22,18 +22,18 @@ class SupportRequest:
 
     def __hash__(self):
         return hash(self.request_action)
-    
 
-def create_dataframe_from_csv(filename:str):
+
+def create_dataframe_from_csv(filename: str):
 
     dataframe = pd.read_csv(filename)
     pd.options.display.max_rows = 9999
 
-    dataframe.set_index('Type')
+    dataframe.set_index("Type")
 
     return dataframe
 
-    
+
 def yesterdays_date():
     today = date.today()
     yesterday = today - timedelta(days=1)
@@ -45,24 +45,27 @@ def yesterdays_date():
 def get_environment_variables() -> tuple:
     slack_token = os.getenv("ADMIN_SLACK_TOKEN")
     if not slack_token:
-        raise ValueError(
-            "The env variable ADMIN_SLACK_TOKEN is empty or missing")
+        raise ValueError("The env variable ADMIN_SLACK_TOKEN is empty or missing")
 
     return slack_token
 
 
-def get_dict_of_requests_and_volume(requests: list[SupportRequest]) -> dict[SupportRequest, int]:
+def get_dict_of_requests_and_volume(
+    requests: list[SupportRequest],
+) -> dict[SupportRequest, int]:
     dict_of_requests = {}
- 
+
     for request in requests:
         dict_of_requests[request.request_action] += 1
-    
+
     return dict_of_requests
 
 
 def craft_message_to_slack(yesterdays_support_requests: list[SupportRequest]):
-    dict_of_requests_and_volume = get_dict_of_requests_and_volume(yesterdays_support_requests)
-    
+    dict_of_requests_and_volume = get_dict_of_requests_and_volume(
+        yesterdays_support_requests
+    )
+
     msg = (
         f"Good morning, \n\n"
         f"Yesterday we received {len(yesterdays_support_requests)} Support Requests: \n\n"
@@ -71,33 +74,35 @@ def craft_message_to_slack(yesterdays_support_requests: list[SupportRequest]):
     for request in yesterdays_support_requests:
         number_of_requests = dict_of_requests_and_volume[request.request_action]
         msg += f"--\n*Type:* {request.request_type}\n*Action:* {request.request_action}\n*Number of requests:* {number_of_requests}\n"
-    
+
     return msg
 
 
-def get_support_requests_from_csv(filepath:str) ->list[SupportRequest]:
+def get_support_requests_from_csv(filepath: str) -> list[SupportRequest]:
     if exists(filepath):
         return get_list_of_support_requests(create_dataframe_from_csv(filepath))
-    raise FileExistsError(
-        f"File path {filepath} does not exist."
-    )
+    raise FileExistsError(f"File path {filepath} does not exist.")
 
 
 def get_list_of_support_requests(data) -> list[SupportRequest]:
-        list_of_support_requests = []
-        for _, row in data.iterrows():
-            list_of_support_requests.append(SupportRequest(row['Type'], row['Action'], row['Date']))
-        
-        return list_of_support_requests
+    list_of_support_requests = []
+    for _, row in data.iterrows():
+        list_of_support_requests.append(
+            SupportRequest(row["Type"], row["Action"], row["Date"])
+        )
 
-            
-def get_yesterdays_support_requests(all_support_requests: list[SupportRequest]) -> list[SupportRequest]:
+    return list_of_support_requests
+
+
+def get_yesterdays_support_requests(
+    all_support_requests: list[SupportRequest],
+) -> list[SupportRequest]:
     yesterday = yesterdays_date()
     list_of_yesterdays_support_requests = []
     for request in all_support_requests:
         if request.request_date == yesterday:
             list_of_yesterdays_support_requests.append(request)
-        
+
     return list_of_yesterdays_support_requests
 
 
@@ -105,7 +110,7 @@ def main():
 
     slack_token = get_environment_variables()
     slack_service = SlackService(str(slack_token))
-    FILE_PATH = 'data/support_stats/support_stats.csv'
+    FILE_PATH = "data/support_stats/support_stats.csv"
     all_support_requests = get_support_requests_from_csv(FILE_PATH)
     yesterdays_requests = get_yesterdays_support_requests(all_support_requests)
     slack_message = craft_message_to_slack(yesterdays_requests)
