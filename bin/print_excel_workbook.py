173fcf48c647798327fdd22d44b63ceb3ff9a6e8
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-05-30 12:44:58 +0100

    trailing whitespace

---
+++ bin/print_excel_workbook.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def print_excel_contents(excel_file):
+    try:
+        excel_data = pd.read_excel(excel_file)
+    except Exception as e:
+        print(f"Error reading the Excel file: {e}")
+        return
+
+    print(excel_data)
+
+if __name__ == "__main__":
+    print_excel_contents('DNS_Request_Form.xlsx')
