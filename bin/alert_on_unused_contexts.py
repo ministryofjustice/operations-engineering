f988727063a860b4c4d5b279650615669cfea7a0
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-06-04 08:51:02 +0100

    :recycle: Restructured main script and added in some error capturing.

--- bin/alert_on_unused_contexts.py
+++ bin/alert_on_unused_contexts.py
@@ -32,6 +32,13 @@ def get_environment_variables() -> tuple:
     return slack_token, github_token, circleci_token
 
 
+def get_all_pipelines_for_all_repositories(repo_list, circle_ci_service):
+    for repo in repo_list:
+        print("PEPPER - Fetching pipelines for repo...")
+        full_pipeline_list = circle_ci_service.get_circleci_pipelines_for_repository(repo)
+    return full_pipeline_list
+
+
 def main():
     slack_token, github_token, circleci_token = get_environment_variables()
     github_service = GithubService(github_token, GITHUB_ORG)
@@ -41,31 +48,34 @@ def main():
 
     full_repository_list = github_service.get_org_repo_names()
 
-    for repo in full_repository_list:
-        print("PEPPER - Fetching pipelines for repo...")
-        pipelines = circle_ci_service.get_circleci_pipelines_for_repository(repo)
+    print("All repositories have been gathered.")
 
-        for pipeline in pipelines:
+    full_pipeline_list = get_all_pipelines_for_all_repositories(full_repository_list, circle_ci_service)
+
+    print("All pipelines have been gathered.")
+
+    try:
+        for pipeline in full_pipeline_list:
             pipeline_id = pipeline["id"]
-            configurations = circle_ci_service.get_pipeline_configurations_from_pipeline_id(pipeline_id)
-            if configurations:
-                compiled_config = configurations.get("compiled", "")
-                compiled_setup_config = configurations.get("compiled-setup-config", "")
-                all_configs = [compiled_config, compiled_setup_config]
-                for cfg in all_configs:
-                    if cfg:
-                        print("\n\n PEPPER - Configuration had been found!")
-                        configuration_data = yaml.safe_load(cfg)
-                        contexts = circle_ci_service.find_all_contexts_from_configuration(configuration_data)
-                        all_used_contexts.update(contexts)
-            time.sleep(1)
+            full_configuration_list = circle_ci_service.get_pipeline_configurations_from_pipeline_id(pipeline_id)
+            if full_configuration_list:
+                compiled_config = full_configuration_list.get("compiled", "")
+                compiled_setup_config = full_configuration_list.get("compiled-setup-config", "")
+                all_configurations_for_pipeline = [compiled_config, compiled_setup_config]
+                for configuration in all_configurations_for_pipeline:
+                    print("\n\n PEPPER - Configuration had been found!")
+                    configuration_data = yaml.safe_load(configuration)
+                    contexts_in_configuration = circle_ci_service.find_all_contexts_from_configuration(configuration_data)
+                    all_used_contexts.update(contexts_in_configuration)
 
-    all_contexts = circle_ci_service.list_all_contexts()
-    unused_contexts = set(all_contexts) - all_used_contexts
+        all_contexts = circle_ci_service.list_all_contexts()
+        unused_contexts = set(all_contexts) - all_used_contexts
 
-    print("\n\n PEPPER - Unused context names:")
-    for context in unused_contexts:
-        print(context)
+        print("\n\n PEPPER - Unused context names:")
+        for context in unused_contexts:
+            print(f"Context name: {context}")
+    except Exception as e:
+        print(f"Error gathering contexts: {e}")
 
 
 if __name__ == "__main__":
