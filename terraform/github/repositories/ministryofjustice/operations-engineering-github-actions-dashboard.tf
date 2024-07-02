198886a9845874451f2e08856c9b2f2682a2eef1
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-07-02 15:14:36 +0100

    :sparkles: Add operations-engineering-github-actions-dashboard repo
    This repository will host a Flask application displaying a simple graph
    of GitHub Actions usage for private repositories.

---
+++ terraform/github/repositories/ministryofjustice/operations-engineering-github-actions-dashboard.tf
@@ -0,0 +1,12 @@
+module "operations-engineering-github-actions-dashboard" {
+  source  = "ministryofjustice/repository/github"
+  version = "1.0.1"
+
+  name        = "operations-engineering-github-actions-dashboard"
+  description = "A GitHub repository for the Operations Engineering GitHub Actions dashboard"
+  topics      = ["operations-engineering"]
+
+  team_access = {
+    admin = [var.operations_engineering_team_id]
+  }
+}
