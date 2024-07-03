2ce94617828f9f80e726546ff3907b1450331c6f
andyrogers1973 <andy.rogers@digital.justice.gov.uk>
2024-07-03 13:50:48 +0100

    Fix more megalinter issues
    Megalinter is giving warnings about the support_stats.py script that did not occur before.  This should resolve them.

--- bin/support_stats_reporting.py
+++ bin/support_stats_reporting.py
@@ -25,17 +25,6 @@ class SupportRequest:
         return hash(self.request_action)
 
 
-def check_file_path():
-
-    filepath = "data/support_stats/support_stats.csv"
-
-    try:
-        with open(filepath, "r"):
-            pass
-    except FileNotFoundError:
-        print("The file was not found")
-
-
 def create_dataframe_from_csv(filename: str):
 
     dataframe = pd.read_csv(filename)
@@ -127,7 +116,7 @@ def get_yesterdays_support_requests(
 def main():
     slack_token = get_environment_variables()
     slack_service = SlackService(str(slack_token))
-    file_path = check_file_path("data/support_stats/support_stats.csv")
+    file_path = "data/support_stats/support_stats.csv"
     all_support_requests = get_support_requests_from_csv(file_path)
     yesterdays_requests = get_yesterdays_support_requests(all_support_requests)
     slack_message = craft_message_to_slack(yesterdays_requests)
