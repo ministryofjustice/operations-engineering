22f52bac3362ffb4e788fd2c58005a99f0ba0702
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-07-11 13:45:42 +0100

    :recycle: Configured AWS provider to use new dsd_secret_manager_access_profile

--- terraform/github/repositories/main.tf
+++ terraform/github/repositories/main.tf
@@ -25,8 +25,14 @@ provider "github" {
   owner = "ministryofjustice"
 }
 
+
 provider "github" {
   alias = "ministryofjustice-test"
   token = var.github_token
   owner = "ministryofjustice-test"
+}
+
+provider "aws" {
+  profile = "dsd_secret_manager_access_profile"
+  region  = "eu-west-2"
 }
