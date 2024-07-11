8f5b6488ffeb7f7aebb5d64879ab97e2febfdb6e
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-07-11 15:20:12 +0100

    :sparkles: Autogenerate operations-engineering-test terraform configuration
    This commit creates infrastructure as code for all items we want to manage in Terraform. I used the auto-generation technique outlined here and hope that after importing, I can remove the auth0_import.tf file.

---
+++ terraform/auth0/operations-engineering-test/auth0_import.tf
@@ -0,0 +1,30 @@
+import {
+  id = "38c317cd-592f-4110-81b1-8c10ccdabbce"
+  to = auth0_attack_protection.attack_protection
+}
+
+import {
+  id = "qkvVnu0b4pXd6FdEbjm073w4MchhpOoB"
+  to = auth0_client.default_app
+}
+
+import {
+  id = "xI46sB1af7TKeycQ3xfTA2qIxeLiX2R8"
+  to = auth0_client.terraform_provider_auth0
+}
+
+import {
+  id = "con_yyrB18AQbSUcMLTm"
+  to = auth0_connection.google_oauth2
+}
+
+import {
+  id = "65942c8d2208302c27c666f4"
+  to = auth0_resource_server.auth0_management_api
+}
+
+import {
+  id = "356f7392-d013-479e-a184-4dd1237568c8"
+  to = auth0_tenant.tenant
+}
+
