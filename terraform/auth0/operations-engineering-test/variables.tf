21ec2ec834e14ca3ff0e80e615018776e7f60ef8
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-07-11 16:39:28 +0100

    :recycle: Rename auth0 test tenant variables to avoid conflict
    We'll be managing and maintaining the production tenant in terraform and dual env vars will have to be used.

--- terraform/auth0/operations-engineering-test/variables.tf
+++ terraform/auth0/operations-engineering-test/variables.tf
@@ -1,18 +1,18 @@
-variable "auth0_domain" {
+variable "auth0_domain_test" {
   description = "value of the Auth0 domain"
   default     = ""
   type        = string
   sensitive   = true
 }
 
-variable "auth0_client_id" {
+variable "auth0_client_id_test" {
   description = "value of the Auth0 client id"
   default     = ""
   type        = string
   sensitive   = true
 }
 
-variable "auth0_client_secret" {
+variable "auth0_client_secret_test" {
   description = "value of the Auth0 client secret"
   default     = ""
   type        = string
