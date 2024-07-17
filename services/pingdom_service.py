b2de06eaad3fce1692b34228f23a98011ecac883
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-07-17 12:13:55 +0100

    :sparkles: New Pingdom service created to utilise secret retrieved from AWS

---
+++ services/pingdom_service.py
@@ -0,0 +1,20 @@
+import requests
+
+
+class PingdomService:
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+        self.base_url = "https://api.pingdom.com/api/3.1"
+        self.headers = {
+            "Authorization": f"Bearer {self.api_key}",
+            "Content-Type": "application/json"
+        }
+
+    def get_checks(self):
+        url = f"{self.base_url}/checks"
+        response = requests.get(url, headers=self.headers, timeout=60)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            response.raise_for_status()
