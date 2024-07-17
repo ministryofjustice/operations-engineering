8f1764d77c88bf1e21685e872d2b4ba032f65a6f
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-07-17 12:13:24 +0100

    :sparkles: New script to grab secrets from AWS and use an API key stored there to interact with Pingdom

---
+++ bin/fetch_secrets_from_aws.py
@@ -0,0 +1,27 @@
+import os
+from services.secret_manager_service import SecretsManagerService
+from services.pingdom_service import PingdomService
+
+
+def main():
+    region_name = os.getenv('AWS_REGION')
+    access_key = os.getenv('AWS_ACCESS_KEY_ID')
+    secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+    secrets_service = SecretsManagerService(
+        region_name=region_name,
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key
+    )
+
+    secret_set_name = "operations-engineering-pingdom-api-key"
+
+    secret_set = secrets_service.get_secret_set(secret_set_name)
+
+    pingdom_api_key = secret_set['operations-engineering-pingdom-api-key-read']
+
+    pingdom_service = PingdomService(api_key=pingdom_api_key)
+
+    pingdom_checks = pingdom_service.get_checks()
+
+    print(f"Pingdom checks received: {pingdom_checks}")
