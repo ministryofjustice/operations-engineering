e038385607b453f45188d11bc160032ae4fabea2
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-07-17 15:57:13 +0100

    :scroll: pylint R1710: Add return expression for exception

--- services/secret_manager_service.py
+++ services/secret_manager_service.py
@@ -10,9 +10,10 @@ class SecretsManagerService:
         cache_config = SecretCacheConfig()
         self.cache = SecretCache(config=cache_config, client=client)
 
-    def get_secret_set(self, secret_set_name: str) -> str:
+    def get_secret_set(self, secret_set_name: str):
         try:
             secret_set = json.loads(self.cache.get_secret_string(secret_set_name))
             return secret_set
         except ClientError as error:
             print(f"Error retrieving secret set: {error}")
+            return None
