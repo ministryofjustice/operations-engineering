db2b6d3007489fe05edda2ac1a9706bbd5603f87
connormaglynn <connor.glynn@digital.justice.gov.uk>
2024-07-22 07:24:12 +0100

    ✨ Add All Records to HostedZone

--- bin/dns_delegations_metrics.py
+++ bin/dns_delegations_metrics.py
@@ -1,4 +1,5 @@
-from services.route53_service import Route53Service
+from services.route53_service import Route53Service, HostedZoneModel
+import json
 
 
 def main():
@@ -7,8 +8,17 @@ def main():
         profile="cloud_platform_route53_read"
     )
 
-    print(dsd_route53_service.get_route53_hosted_zones()[0].name)
-    print(cloud_platform_route53_service.get_route53_hosted_zones()[0].name)
+    dsd_hosted_zones: list[HostedZoneModel] = dsd_route53_service.get_hosted_zones()
+    cloud_platform_hosted_zones: list[HostedZoneModel] = (
+        cloud_platform_route53_service.get_hosted_zones()
+    )
+
+    print(json.dumps(dsd_hosted_zones[0].__dict__, default=lambda o: o.__dict__))
+    print(
+        json.dumps(
+            cloud_platform_hosted_zones[0].__dict__, default=lambda o: o.__dict__
+        )
+    )
 
 
 if __name__ == "__main__":
--- services/route53_service.py
+++ services/route53_service.py
@@ -3,8 +3,21 @@ import boto3
 
 
 @dataclass
+class RecordModel:
+    value: str
+
+
+@dataclass
+class RecordSetModel:
+    name: str
+    type: str
+    records: list[RecordModel]
+
+
+@dataclass
 class HostedZoneModel:
     name: str
+    record_sets: list[RecordSetModel]
 
 
 class Route53Service:
@@ -12,11 +25,39 @@ class Route53Service:
         session = boto3.Session(profile_name=profile)
         self.client = session.client("route53")
 
-    def get_route53_hosted_zones(self) -> list[HostedZoneModel]:
+    def __get_hosted_zone_record_sets(self, zone_id: str):
+        response = self.client.list_resource_record_sets(HostedZoneId=zone_id)
+
+        record_sets: list[RecordSetModel] = []
+        for record_set in response["ResourceRecordSets"]:
+            record_set_name = record_set["Name"]
+            record_set_type = record_set["Type"]
+
+            records: list[RecordModel] = []
+            for record in record_set.get("ResourceRecords", []):
+                records.append(RecordModel(value=record["Value"]))
+
+            record_sets.append(
+                RecordSetModel(
+                    name=record_set_name,
+                    type=record_set_type,
+                    records=records,
+                )
+            )
+
+        return record_sets
+
+    def get_hosted_zones(self) -> list[HostedZoneModel]:
         response = self.client.list_hosted_zones(MaxItems="1")
 
         hosted_zones: list[HostedZoneModel] = []
         for zone in response["HostedZones"]:
-            hosted_zones.append(HostedZoneModel(name=zone["Name"]))
-
+            zone_name = zone["Name"]
+            zone_id = zone["Id"]
+            hosted_zones.append(
+                HostedZoneModel(
+                    name=zone_name,
+                    record_sets=self.__get_hosted_zone_record_sets(zone_id),
+                )
+            )
         return hosted_zones
