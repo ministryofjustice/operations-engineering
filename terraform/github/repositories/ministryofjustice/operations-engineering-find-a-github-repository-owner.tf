504aeb41e260ebb20aa97b839109157a6985396f
connormaglynn <connor.glynn@digital.justice.gov.uk>
2024-07-24 16:05:05 +0100

    ✨ Add `operations-engineering-find-a-github-repository-owner` Repository

---
+++ terraform/github/repositories/ministryofjustice/operations-engineering-find-a-github-repository-owner.tf
@@ -0,0 +1,12 @@
+module "operations-engineering-find-a-github-repository-owner" {
+  source  = "ministryofjustice/repository/github"
+  version = "1.0.1"
+
+  name        = "operations-engineering-find-a-github-repository-owner"
+  description = "Find Owners of GitHub Reposistories"
+  topics      = ["operations-engineering"]
+
+  team_access = {
+    admin = [var.operations_engineering_team_id]
+  }
+}
