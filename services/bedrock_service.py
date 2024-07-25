98da7716f5a60c79ed65aa64ab7ded65fea218d3
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-07-25 10:49:41 +0100

    :recycle: Refactored Bedrock service to enable multi-model requests

--- services/bedrock_service.py
+++ services/bedrock_service.py
@@ -1,38 +1,71 @@
 import boto3, json
+from config.constants import CLAUDE_MODEL_ID, LLAMA_MODEL_ID
+
 
 class BedrockService:
     def __init__(self) -> None:
         self.client = boto3.client("bedrock", region_name="eu-west-2")
 
-    def make_request(self, prompt):
+    def request_model_response_from_bedrock(self, prompt, model):
 
         bedrock_runtime = boto3.client(
             service_name="bedrock-runtime",
             region_name="eu-west-2",
         )
-        model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
+
+        model_id = ""
+        model_request = {}
+
+        if model == "llama":
+            model_request = self.format_llama_request(prompt)
+            model_id = LLAMA_MODEL_ID
+
+            response = bedrock_runtime.invoke_model(
+                modelId=model_id,
+                body=json.dumps(model_request),
+            )
+
+            response_body = json.loads(response["body"].read())
+            result = response_body["generation"]
+
+        else:
+            model_request = self.format_claude_request(prompt)
+            model_id = CLAUDE_MODEL_ID
+
+            response = bedrock_runtime.invoke_model(
+                modelId=model_id,
+                body=json.dumps(model_request),
+            )
+
+            result = json.loads(response.get("body").read()).get("content", [])[0].get("text", "")
+
+        return result
+
+    def format_claude_request(self, prompt):
         model_kwargs = {
             "max_tokens": 2048,
             "temperature": 0.1,
             "top_k": 250,
-            "top_p": 1,
-            "stop_sequences": ["\n\nHuman"],
+            "top_p": 1
         }
 
-        body = {
+        request = {
             "anthropic_version": "bedrock-2023-05-31",
             "system": "Generate only the tests, with no additional speech or explanation.",
             "messages": [
                 {"role": "user", "content": [{"type": "text", "text": prompt}]},
             ],
         }
-        body.update(model_kwargs)
 
-        response = bedrock_runtime.invoke_model(
-            modelId=model_id,
-            body=json.dumps(body),
-        )
+        request.update(model_kwargs)
 
-        result = json.loads(response.get("body").read()).get("content", [])[0].get("text", "")
+        return request
 
-        return result
+    def format_llama_request(self, prompt):
+        request = {
+                        "prompt": prompt,
+                        "temperature": 0.5,
+                        "top_p": 0.9,
+                        "max_gen_len": 2048,
+                    }
+        return request
