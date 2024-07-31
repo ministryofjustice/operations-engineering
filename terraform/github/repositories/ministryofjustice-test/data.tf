da0f9b8e5c751dd54330c8712af452d4067b6f83
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-07-31 14:05:53 +0100

    remove provider variable from team data blocks

--- terraform/github/repositories/ministryofjustice-test/data.tf
+++ terraform/github/repositories/ministryofjustice-test/data.tf
@@ -1,9 +1,7 @@
 data "github_team" "operations_engineering_test" {
-  provider = github.ministryofjustice-test
-  slug     = "operations-engineering-test"
+  slug = "operations-engineering-test"
 }
 
 data "github_team" "test_team_access" {
-  provider = github.ministryofjustice-test
-  slug     = "test-team-access"
+  slug = "test-team-access"
 }
