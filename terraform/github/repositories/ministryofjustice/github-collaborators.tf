7543524483d309c4303be0fc6b3fbf1c09d4d625
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-08-01 13:47:40 +0100

    test change stack

--- terraform/github/repositories/ministryofjustice/github-collaborators.tf
+++ terraform/github/repositories/ministryofjustice/github-collaborators.tf
@@ -3,7 +3,7 @@ module "github-collaborators" {
   version = "1.0.1"
 
   name            = "github-collaborators"
-  description     = "Manage outside collaborators on our Github repositories lalalal"
+  description     = "Manage outside collaborators on our Github repositories lal"
   has_discussions = true
   topics          = ["operations-engineering"]
 
