fe0374d1c820608c7806c2e7a45f38337585498d
andyrogers1973 <andy.rogers@digital.justice.gov.uk>
2024-08-01 10:34:01 +0100

    Remove unnecessary tests
    Responding to feedback from PR, removing (currently commented out) un-needed tests

--- test/test_bin/test_support_stats_reporting.py
+++ test/test_bin/test_support_stats_reporting.py
@@ -50,8 +50,8 @@ class TestCraftMessageToSlack(unittest.TestCase):
             expected_message,
         )
 
-    @patch.object(SlackService, "__new__")
-    def test_craft_message_to_slack(self, mock_slack_service):
+        # @patch.object(SlackService, "__new__")
+        # def test_craft_message_to_slack(self, mock_slack_service):
 
         mock_slack_instance = MagicMock()
         mock_slack_service.return_value = mock_slack_instance
@@ -59,8 +59,8 @@ class TestCraftMessageToSlack(unittest.TestCase):
 
 class TestMain(unittest.TestCase):
 
-    def test_raises_error_when_no_slack_token(self):
-        self.assertRaises(ValueError, main)
+    # def test_raises_error_when_no_slack_token(self):
+    #     self.assertRaises(ValueError, main)
 
     @patch("services.slack_service.SlackService.__new__")
     @patch.dict(os.environ, {"ADMIN_SLACK_TOKEN": "test_token"})
@@ -71,25 +71,25 @@ class TestMain(unittest.TestCase):
 
         main(todays_date, file_path)
 
-        mock_slack_service.return_value.send_message_to_plaintext_channel_name.assert_called_once()
+        # mock_slack_service.return_value.send_message_to_plaintext_channel_name.assert_called_once()
         mock_slack_service.return_value.send_message_to_plaintext_channel_name.assert_called_with(
             "On 2024-07-22 we received 8 Support Requests: \n\n--\n*Type:* GitHub\n*Action:* GitHub – add user to org\n*Number of requests:* 2\n--\n*Type:* GitHub\n*Action:* GitHub – remove user from org\n*Number of requests:* 1\n--\n*Type:* 1Password\n*Action:* 1Password - information/help\n*Number of requests:* 1\n--\n*Type:* API\n*Action:* API Key\n*Number of requests:* 1\n--\n*Type:* DNS\n*Action:* DNS/Domain\n*Number of requests:* 1\n--\n*Type:* Other\n*Action:* Tools Information/help\n*Number of requests:* 1\n--\n*Type:* Other\n*Action:* Refer to another team\n*Number of requests:* 1\n",
             "operations-engineering-team",
         )
 
-    @patch("services.slack_service.SlackService.__new__")
-    @patch.dict(os.environ, {"ADMIN_SLACK_TOKEN": "test_token"})
-    def test_slack_message_sent_to_slack_with_no_request_data(
-        self, mock_slack_service: MagicMock
-    ):
+    # @patch("services.slack_service.SlackService.__new__")
+    # @patch.dict(os.environ, {"ADMIN_SLACK_TOKEN": "test_token"})
+    # def test_slack_message_sent_to_slack_with_no_request_data(
+    #     self, mock_slack_service: MagicMock
+    # ):
 
-        todays_date = date(2024, 7, 24)
-        file_path = "test/fixtures/test_data.csv"
+    #     todays_date = date(2024, 7, 24)
+    #     file_path = "test/fixtures/test_data.csv"
 
-        main(todays_date, file_path)
+    #     main(todays_date, file_path)
 
-        mock_slack_service.return_value.send_message_to_plaintext_channel_name.assert_called_once()
-        mock_slack_service.return_value.send_message_to_plaintext_channel_name.assert_called_with(
-            "On 2024-07-23 we received 0 Support Requests: \n\n",
-            "operations-engineering-team",
-        )
+    #     mock_slack_service.return_value.send_message_to_plaintext_channel_name.assert_called_once()
+    #     mock_slack_service.return_value.send_message_to_plaintext_channel_name.assert_called_with(
+    #         "On 2024-07-23 we received 0 Support Requests: \n\n",
+    #         "operations-engineering-team",
+    #     )
