1ba00e2f12566565c2d4b1166ca74f34e15e12bd
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-08-15 10:45:09 +0100

    repository for unit test generator poc

---
+++ terraform/github/repositories/ministryofjustice/operations-engineering-unit-test-generator-poc.tf
@@ -0,0 +1,12 @@
+module "operations-engineering-example" {
+  source  = "ministryofjustice/repository/github"
+  version = "1.1.1"
+
+  name        = "operations-engineering-unit-test-generator-poc"
+  description = "CLI tool to automatically generate Python unit tests, using the unittest framework. This project is in POC."
+  topics      = ["operations-engineering"]
+
+  team_access = {
+    admin = [var.operations_engineering_team_id]
+  }
+}
