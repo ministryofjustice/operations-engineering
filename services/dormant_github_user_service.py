67e4642efbf08e525888d37d8cc1ba9018f3bf23
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-08-20 10:39:44 +0100

    :art: Format Dormant User files using Black

--- services/dormant_github_user_service.py
+++ services/dormant_github_user_service.py
@@ -7,7 +7,12 @@ from services.github_service import GithubService
 
 
 class DormantGitHubUser:
-    def __init__(self, github_service: GithubService, name: str, days_considered_dormant: int = 90):
+    def __init__(
+        self,
+        github_service: GithubService,
+        name: str,
+        days_considered_dormant: int = 90,
+    ):
         self.github_service = github_service
         self.name = name
         self.threshold = datetime.now() - timedelta(days=days_considered_dormant)
@@ -18,15 +23,15 @@ class DormantGitHubUser:
     @property
     def email(self):
         if self._email is None:
-            self._email = self.github_service.get_user_org_email_address(
-                self.name)
+            self._email = self.github_service.get_user_org_email_address(self.name)
         return self._email
 
     @property
     def last_github_activity(self):
         if self._last_github_activity is None:
-            self._last_github_activity = self.github_service.get_last_audit_log_activity_date_for_user(
-                self.name)
+            self._last_github_activity = (
+                self.github_service.get_last_audit_log_activity_date_for_user(self.name)
+            )
         return self._last_github_activity
 
     @property
@@ -46,33 +51,39 @@ class DormantGitHubUser:
         if self.last_auth0_activity is None:
             return self.last_github_activity < self.threshold
 
-        return self.last_github_activity < self.threshold and self.last_auth0_activity < self.threshold
+        return (
+            self.last_github_activity < self.threshold
+            and self.last_auth0_activity < self.threshold
+        )
 
     def execute_cloudwatch_query(self, query, start_time, end_time):
-        client = boto3.client('logs', region_name='eu-west-2')
+        client = boto3.client("logs", region_name="eu-west-2")
         query_response = client.start_query(
-            logGroupName='/aws/events/LogsFromOperationsEngineeringAuth0',
+            logGroupName="/aws/events/LogsFromOperationsEngineeringAuth0",
             startTime=start_time,
             endTime=end_time,
             queryString=query,
         )
-        return query_response['queryId']
+        return query_response["queryId"]
 
     def get_cloudwatch_query_results(self, query_id):
         """Polls CloudWatch Logs for query results and returns the results when complete."""
-        client = boto3.client('logs', region_name='eu-west-2')
+        client = boto3.client("logs", region_name="eu-west-2")
         while True:
             response = client.get_query_results(queryId=query_id)
-            if response['status'] == 'Complete':
-                return response['results']
+            if response["status"] == "Complete":
+                return response["results"]
             time.sleep(1)  # Sleep to rate limit the polling
 
     def format_timestamp(self, timestamp):
-        return datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f").strftime("%Y-%m-%d %H:%M:%S")
+        return datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f").strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
 
     def fetch_last_auth0_activity(self):
         """Fetches the last activity timestamp for the user from Auth0 logs.
-        The Auth0 logs are stored in CloudWatch Logs and are queried using the user's email address."""
+        The Auth0 logs are stored in CloudWatch Logs and are queried using the user's email address.
+        """
         query = f"""
         fields @timestamp, @message
         | parse @message '"user_name":"*"' as user_name
@@ -86,6 +97,6 @@ class DormantGitHubUser:
         query_id = self.execute_cloudwatch_query(query, start_time, end_time)
         results = self.get_cloudwatch_query_results(query_id)
         if results:
-            timestamp = results[0][0]['value']
+            timestamp = results[0][0]["value"]
             return self.format_timestamp(timestamp)
         return None
