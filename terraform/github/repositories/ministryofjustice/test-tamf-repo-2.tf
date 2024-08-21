1c0652004343b5a33b3fcdff2bd0690daf170173
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-08-21 22:14:25 +0100

    alternative method of adding env vars and try enforcing obfuscation

--- terraform/github/repositories/ministryofjustice/test-tamf-repo-2.tf
+++ terraform/github/repositories/ministryofjustice/test-tamf-repo-2.tf
@@ -3,7 +3,7 @@ module "test_tamf_repo_2" {
   version = "1.1.2"
 
   name        = "test-tamf-repo-2"
-  description = "Test repo to test new module input team_access "
+  description = "Test repo to test new module input team_access"
   topics      = ["operations-engineering"]
   team_access = {
     maintain = [var.operations_engineering_team_id]
