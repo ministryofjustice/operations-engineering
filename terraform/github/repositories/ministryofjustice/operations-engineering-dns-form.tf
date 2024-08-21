e34cfe4431ecfa3bd35e88b19739cee41a1148f2
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-08-21 13:55:13 +0100

    add url to dns form

--- terraform/github/repositories/ministryofjustice/operations-engineering-dns-form.tf
+++ terraform/github/repositories/ministryofjustice/operations-engineering-dns-form.tf
@@ -2,9 +2,10 @@ module "operations-engineering-dns-form" {
   source  = "ministryofjustice/repository/github"
   version = "1.1.2"
 
-  name        = "operations-engineering-dns-form"
-  description = "A web form that captures the requirements for a DNS change"
-  topics      = ["operations-engineering"]
+  name         = "operations-engineering-dns-form"
+  description  = "A web form that captures the requirements for a DNS change"
+  homepage_url = "https://dns-form-dev.cloud-platform.service.justice.gov.uk/"
+  topics       = ["operations-engineering"]
 
   team_access = {
     admin = [var.operations_engineering_team_id]
