7e9173b35807d4252fdf740c6c837ba05ed690dd
abachleda-baca <agnieszka.bachleda@digital.justice.gov.uk>
2024-08-21 14:26:44 +0100

    further updates

--- bin/fetch_github_actions_quota.py
+++ bin/fetch_github_actions_quota.py
@@ -7,14 +7,19 @@ from services.kpi_service import KpiService
 from config.constants import ENTERPRISE, MINISTRY_OF_JUSTICE
 
 
+def get_environment_variables() -> str:
+    github_token = os.getenv("GH_TOKEN")
+    if not github_token:
+        raise ValueError(
+            "The env variable GH_TOKEN is empty or missing")
+
+    return github_token
+
+
 def fetch_gha_quota():
-    github_token = os.environ.get("GH_TOKEN")
+    github_token = get_environment_variables()
     if github_token is None:
-        print("No GITHUB_TOKEN environment variable set")
-        sys.exit(1)
-    slack_token = os.environ.get("ADMIN_SLACK_TOKEN")
-    if slack_token is None:
-        print("No SLACK_TOKEN environment variable set")
+        print("No GH_TOKEN environment variable set")
         sys.exit(1)
 
     github_service = GithubService(github_token, MINISTRY_OF_JUSTICE, ENTERPRISE)
@@ -23,11 +28,12 @@ def fetch_gha_quota():
     organisations = github_service.get_all_organisations_in_enterprise()
     # get total quota usage
     total_minutes_used = github_service.calculate_total_minutes_used(organisations)
-    # save metric into KPI dashboard database
+
+    # save metric in KPI dashboard db
     try:
         KpiService(os.getenv("KPI_DASHBOARD_URL"), os.getenv("KPI_DASHBOARD_API_KEY")).track_enterprise_github_actions_quota_usage(total_minutes_used)
     except Exception as e:
-        logging.info("Issue when trying to track number of repositories with standards label...")
+        logging.info("Issue when trying to fetch github action quota for the enterprise")
         logging.error(e)
 
 
