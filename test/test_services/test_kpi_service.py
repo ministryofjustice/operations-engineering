8daf9e685354cb2a520007fda6b755b783be9e17
abachleda-baca <agnieszka.bachleda@digital.justice.gov.uk>
2024-08-22 15:32:03 +0100

    updated kpi service unit tests

--- test/test_services/test_kpi_service.py
+++ test/test_services/test_kpi_service.py
@@ -100,5 +100,28 @@ class TestKPIServiceTrackSentryReplaysUsedForDay(unittest.TestCase):
         )
 
 
+@patch("services.kpi_service.requests")
+class TestKPIServiceTrackEnterpriceGithubActionQuotaUsage(unittest.TestCase):
+    def test_api_called(self, mock_requests: MagicMock):
+        KpiService(TEST_BASE_URL, TEST_API_KEY).track_enterprise_github_actions_quota_usage(1)
+
+        mock_requests.post.assert_has_calls(
+            [
+                call(
+                    url=f"{TEST_BASE_URL}/api/indicator/add",
+                    headers={
+                        "X-API-KEY": TEST_API_KEY,
+                        "Content-Type": "application/json",
+                    },
+                    timeout=10,
+                    json={
+                        "indicator": "ENTERPRISE_GITHUB_ACTIONS_QUOTA_USAGE",
+                        "count": 1,
+                    },
+                )
+            ]
+        )
+
+
 if __name__ == "__main__":
     unittest.main()
