1dfdfa1bfc55f8d7178532d4f66892ab81a62928
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-09-02 14:41:56 +0100

    :sparkles: Overhaul dormant GitHub user identification process
    Reworked the mechanisms for identifying and filtering dormant GitHub users. Instead of relying on our data lake's information, this update leverages Auth0 logs through a mocked CloudWatchService.
    
    - Added a function `get_active_users_from_auth0_log_group` to fetch users active in Auth0.
    - Created a test case for this function.
    - Introduced another function `filter_out_active_auth0_users` to filter out any active Auth0 users from the list of dormant users according to GitHub.
    - Tested this filtering function.
    - Developed `message_to_slack_channel` to send a report of dormant users into Slack, and wrote its corresponding unit test.
    
    These changes ensure that we acknowledge activity from any location when assessing dormancy. This will increase accuracy and reduce false positives in identifying inactive GitHub users.

--- test/test_bin/test_identify_dormant_github_users.py
+++ test/test_bin/test_identify_dormant_github_users.py
@@ -1,60 +1,56 @@
 import unittest
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 
 from bin.identify_dormant_github_users import (
-    ALLOWED_BOT_USERS,
-    get_dormant_users_from_data_lake,
-    get_inactive_users_from_data_lake_ignoring_bots_and_collaborators,
-)
-from services.cloudtrail_service import CloudtrailService
+    ALLOWED_BOT_USERS, DormantUser, filter_out_active_auth0_users,
+    get_active_users_from_auth0_log_group, message_to_slack_channel)
 
 
 class TestDormantGitHubUsers(unittest.TestCase):
 
     def setUp(self):
-        self.allowed_bot_users = ["bot1", "bot2"]
+        self.allowed_bot_users = ALLOWED_BOT_USERS
 
-    @patch(
-        "bin.identify_dormant_github_users.get_inactive_users_from_data_lake_ignoring_bots_and_collaborators"
-    )
-    @patch("services.dormant_github_user_service.DormantGitHubUser")
-    @patch("services.github_service.GithubService")
-    def test_get_dormant_users_from_data_lake(
-        self,
-        mock_github_service,
-        mock_dormant_github_user,
-        mock_get_inactive_users_from_data_lake_ignoring_bots_and_collaborators,
-    ):
-        mock_get_inactive_users_from_data_lake_ignoring_bots_and_collaborators.return_value = [
-            "user1",
-            "user2",
-            "user3",
+    @patch('services.cloudwatch_service.CloudWatchService')
+    def test_get_active_users_from_auth0_log_group(self, MockCloudWatchService):
+        mock_service = MockCloudWatchService.return_value
+        mock_service.get_all_auth0_users_that_appear_in_tenants.return_value = [
+            'user1@example.com', 'user2@example.com']
+
+        result = get_active_users_from_auth0_log_group()
+
+        self.assertEqual(result, ['user1@example.com', 'user2@example.com'])
+
+    def test_filter_out_active_auth0_users(self):
+        dormant_users_according_to_github = [
+            DormantUser(name='user1', email='user1@example.com'),
+            DormantUser(name='user2', email='user2@example.com'),
+            DormantUser(name='user3', email='user3@example.com')
         ]
-        github_service = mock_github_service
-        mock_dormant_github_user.side_effect = lambda github_service, user: MagicMock(
-            __str__=lambda: f"DormantGitHubUser({user})"
+        active_email_addresses = ['user1@example.com', 'user3@example.com']
+
+        result = filter_out_active_auth0_users(
+            dormant_users_according_to_github)
+
+        expected_result = [DormantUser(
+            name='user2', email='user2@example.com')]
+        self.assertEqual(result, expected_result)
+
+    def test_message_to_slack_channel(self):
+        dormant_users = [
+            DormantUser(name='user1', email='user1@example.com'),
+            DormantUser(name='user2', email='user2@example.com')
+        ]
+
+        result = message_to_slack_channel(dormant_users)
+
+        expected_message = (
+            "Hello 🤖, \n\n"
+            "The identify dormant GitHub users script has identified 2 dormant users. \n-----\n"
+            "GitHub username: user1 | Email: user1@example.com\n"
+            "GitHub username: user2 | Email: user2@example.com\n"
         )
-
-        result = get_dormant_users_from_data_lake(github_service)
-
-        self.assertEqual(len(result), 3)
-        mock_get_inactive_users_from_data_lake_ignoring_bots_and_collaborators.assert_called_once_with(
-            github_service, ALLOWED_BOT_USERS
-        )
-
-    @patch.object(CloudtrailService, "get_active_users_for_dormant_users_process")
-    def test_get_inactive_users_from_data_lake_ignoring_bots_and_collaborators(
-        self, mock_get_active_users_for_dormant_users_process
-    ):
-        mock_get_active_users_for_dormant_users_process.return_value = ["member1"]
-        mock_github_service = MagicMock()
-        mock_github_service.get_all_enterprise_members = MagicMock(
-            return_value=["member1", "member2", "bot1", "bot2"]
-        )
-
-        assert get_inactive_users_from_data_lake_ignoring_bots_and_collaborators(
-            mock_github_service, self.allowed_bot_users
-        ) == ["member2"]
+        self.assertEqual(result, expected_message)
 
 
 if __name__ == "__main__":
