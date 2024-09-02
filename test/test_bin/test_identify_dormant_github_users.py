92facde454cc9fbb02cf9e7467c34a4b3d936a7d
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-09-02 16:25:50 +0100

    :recycle: Refactor unit tests for identifying dormant GitHub users
    This commit refactors the unit tests for identifying dormant GitHub users. The changes provide clarity by improving mock setups. Now, instead of mocking entire `CloudWatchService`, a specific method `get_all_auth0_users_that_appear_in_tenants` is mocked in `test_get_active_users_from_auth0_log_group`. Similarly, `get_active_users_from_auth0_log_group` method is patched in `test_filter_out_active_auth0_users` to simulate active users in Auth0 directly. This makes the tests more focused and minimalist as it avoids unnecessary stubbing.

--- test/test_bin/test_identify_dormant_github_users.py
+++ test/test_bin/test_identify_dormant_github_users.py
@@ -11,29 +11,33 @@ class TestDormantGitHubUsers(unittest.TestCase):
     def setUp(self):
         self.allowed_bot_users = ALLOWED_BOT_USERS
 
-    @patch('services.cloudwatch_service.CloudWatchService')
-    def test_get_active_users_from_auth0_log_group(self, MockCloudWatchService):
-        mock_service = MockCloudWatchService.return_value
-        mock_service.get_all_auth0_users_that_appear_in_tenants.return_value = [
+    @patch('services.cloudwatch_service.CloudWatchService.get_all_auth0_users_that_appear_in_tenants')
+    def test_get_active_users_from_auth0_log_group(self, mock_get_users):
+        mock_get_users.return_value = [
             'user1@example.com', 'user2@example.com']
 
         result = get_active_users_from_auth0_log_group()
 
         self.assertEqual(result, ['user1@example.com', 'user2@example.com'])
 
-    def test_filter_out_active_auth0_users(self):
+    @patch('bin.identify_dormant_github_users.get_active_users_from_auth0_log_group')
+    def test_filter_out_active_auth0_users(self, mock_get_active_users):
+        # Mock the return value to simulate active users in Auth0
+        mock_get_active_users.return_value = [
+            'user1@example.com', 'user3@example.com']
+
         dormant_users_according_to_github = [
             DormantUser(name='user1', email='user1@example.com'),
             DormantUser(name='user2', email='user2@example.com'),
             DormantUser(name='user3', email='user3@example.com')
         ]
-        active_email_addresses = ['user1@example.com', 'user3@example.com']
 
         result = filter_out_active_auth0_users(
             dormant_users_according_to_github)
 
         expected_result = [DormantUser(
             name='user2', email='user2@example.com')]
+
         self.assertEqual(result, expected_result)
 
     def test_message_to_slack_channel(self):
@@ -43,12 +47,14 @@ class TestDormantGitHubUsers(unittest.TestCase):
         ]
 
         result = message_to_slack_channel(dormant_users)
+
         expected_message = (
             "Hello 🤖, \n\n"
             "Here is a list of dormant GitHub users that have not been seen in Auth0 logs:\n\n"
             "GitHub username: user1 | Email: user1@example.com\n"
             "GitHub username: user2 | Email: user2@example.com\n"
         )
+
         self.assertEqual(result, expected_message)
 
 
