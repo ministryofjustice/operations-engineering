a57a8f6cb22b72244783c9bec5cd91afd441e179
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-09-11 15:22:29 +0100

    :sparkles: Add new repository to hold a cloud platform tf module
    The module will allow operations engineering the ability to quickly create a cloud platform repository.

---
+++ terraform/github/repositories/ministryofjustice/operations-engineering-cloud-platform-namespace.tf
@@ -0,0 +1,12 @@
+module "operations-engineering-cloud-platform-namespace" {
+  source  = "ministryofjustice/repository/github"
+  version = "1.1.2"
+
+  name        = "operations-engineering-cloud-platform-namespace"
+  description = "This reoisitory contains the terraform code for an operations engineering namespace in the cloud platform"
+  topics      = ["operations-engineering"]
+
+  team_access = {
+    admin = [var.operations_engineering_team_id]
+  }
+}
