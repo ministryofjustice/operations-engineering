867bf576518d053235e83168dba0c1899a107378
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-09-25 14:05:09 +0100

    fix name clashes

--- terraform/dsd/iam/dns_check_change_status.tf
+++ terraform/dsd/iam/dns_check_change_status.tf
@@ -25,12 +25,12 @@ resource "aws_iam_role" "dns_check_change_status_user_role" {
   assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role_policy_document.json
 }
 
-resource "aws_iam_role_policy_attachment" "role_policy_attachment" {
+resource "aws_iam_role_policy_attachment" "dns_check_change_status_role_policy_attachment" {
   role       = aws_iam_role.dns_check_change_status_user_role.name
   policy_arn = aws_iam_policy.dns_check_change_status_policy.arn
 }
 
-resource "github_actions_secret" "aws_role_arn" {
+resource "github_actions_secret" "dns_check_change_status_aws_role_arn" {
   repository      = "dns"
   secret_name     = "DNS_CHECK_CHANGE_STATUS_AWS_ROLE_ARN"
   plaintext_value = aws_iam_role.dns_check_change_status_user_role.arn
