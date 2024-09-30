df7881e583229c6e6c1e4702b3e19fe5d14fcb8d
Jason Birchall <31217584+jasonBirchall@users.noreply.github.com>
2024-09-30 10:58:24 +0100

    Create aga_repo.tf

---
+++ terraform/github/repositories/ministryofjustice/aga_repo.tf
@@ -0,0 +1,18 @@
+module "operations-engineering-aga-test" {
+  source  = "ministryofjustice/repository/github"
+  version = "1.1.2"
+
+  name        = "operations-engineering-aga-test"
+  description = "this is a test and will be deleted"
+  topics      = ["operations-engineering", "poc"]
+  visibility  = "public"
+
+  template    = {
+    owner      = "ministryofjustice"
+    repository = "operations-engineering-flask-template"
+  }
+
+  team_access = {
+    admin = [var.operations_engineering_team_id]
+  }
+}
