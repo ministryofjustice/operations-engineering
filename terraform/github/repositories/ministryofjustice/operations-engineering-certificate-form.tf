62e2fbab558c59a361e1a0ccb48811a656be2f1c
Sam Pepper <124164618+PepperMoJ@users.noreply.github.com>
2024-09-30 11:49:50 +0100

    :sparkles: Added New Certificate Flask Application Repository (#4879)
    * :sparkles: New repository added for the certificate flask application.
    
    * :negative_squared_cross_mark: Removed homepage URL variable.

---
+++ terraform/github/repositories/ministryofjustice/operations-engineering-certificate-form.tf
@@ -0,0 +1,17 @@
+module "operations-engineering-certificate-form" {
+  source  = "ministryofjustice/repository/github"
+  version = "1.2.0"
+
+  name         = "operations-engineering-certificate-form"
+  description  = "An experimental web form that assists with automating part of the certificate renewal process."
+  topics       = ["operations-engineering"]
+
+  template     = {
+    owner      = "ministryofjustice"
+    repository = "operations-engineering-flask-template"
+  }
+
+  team_access = {
+    admin = [var.operations_engineering_team_id]
+  }
+}
