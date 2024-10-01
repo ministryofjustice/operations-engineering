21857a0752877ec2edd4460ff2b564cfcd3b0291
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-10-01 15:19:49 +0100

    amend state policy

--- terraform/dsd/iam/cloudwatch_admin.tf
+++ terraform/dsd/iam/cloudwatch_admin.tf
@@ -34,29 +34,23 @@ resource "aws_iam_policy" "cloudwatch_terraform_state_policy" {
   name        = "cloudwatch_terraform_state_policy"
   description = "IAM policy for CloudWatch Terraform state"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject",
-          "s3:ListBucket"
-        ]
-        Resource = [
-          "arn:aws:s3:::operations-engineering-test-terraform-state-bucket",
-          "arn:aws:s3:::operations-engineering-test-terraform-state-bucket/terraform/dsd/cloudwatch_alarms/terraform.tfstate"
-        ]
-        Condition = {
-          StringLike = {
-            "s3:prefix" = "terraform/dsd/cloudwatch_alarms/"
-          }
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : "s3:ListBucket",
+          "Resource" : "arn:aws:s3:::operations-engineering-test-terraform-state-bucket"
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : ["s3:GetObject", "s3:PutObject"],
+          "Resource" : "arn:aws:s3:::operations-engineering-test-terraform-state-bucket/terraform/dsd/cloudwatch_alarms/terraform.tfstate"
         }
-      }
-    ]
-  })
+      ]
+    }
+  )
 }
 
 resource "aws_iam_role_policy_attachment" "cloudwatch_terraform_state_policy_attachment" {
