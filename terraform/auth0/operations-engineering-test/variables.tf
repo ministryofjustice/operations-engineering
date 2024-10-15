8f219f580af3da274a19df5a5f280d12a352ff8c
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-10-15 11:15:55 +0100

    :fire: Removed default variable functions

--- terraform/auth0/operations-engineering-test/variables.tf
+++ terraform/auth0/operations-engineering-test/variables.tf
@@ -22,27 +22,23 @@ variable "auth0_client_secret_test" {
 variable "azure_client_secret_test" {
   description = "Test client secret for azure connection"
   type        = string
-  default     = env("TF_VAR_auth0_azure_client_secret_test")
   sensitive   = true
 }
 
 variable "azure_client_id_test" {
   description = "Test client ID for azure connection"
   type        = string
-  default     = env("TF_VAR_auth0_azure_client_id_test")
   sensitive   = true
 }
 
 variable "terraform_provider_auth0_encryption_key" {
   description = "Encryption key value for terraform provider auth0"
   type        = string
-  default     = env("TF_VAR_terraform_provider_auth0_encryption_key")
   sensitive   = true
 }
 
 variable "default_app_encryption_key" {
   description = "Encryption key value for the default app"
   type        = string
-  default     = env("TF_VAR_default_app_encryption_key")
   sensitive   = true
 }
