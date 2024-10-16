6997bca55d186191da579847251a0d15eeab5a5e
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-10-16 11:08:31 +0100

    :recycle: Updated connection code to hold google workspace connection

--- terraform/auth0/operations-engineering-test/connections.tf
+++ terraform/auth0/operations-engineering-test/connections.tf
@@ -1,38 +1,24 @@
-resource "auth0_connection" "operations_engineering_test_connection" {
-  name           = "test-connection-azure-ad"
-  strategy       = "waad"
-  show_as_button = true
+resource "auth0_connection" "google_workspace" {
+  name                 = "Google"
+  is_domain_connection = false
+  strategy             = "google-apps"
+  show_as_button       = true
   options {
-    identity_api      = "azure-active-directory-v1.0"
-    client_id         = var.azure_client_id_test
-    client_secret     = var.azure_client_secret_test
-    strategy_version  = 2
-    user_id_attribute = "userName"
-    app_id            = "app-id-123"
-    tenant_domain     = "example.onmicrosoft.com"
-    domain            = "example.onmicrosoft.com"
-    domain_aliases = [
-      "example.com",
-      "api.example.com"
-    ]
-    icon_url               = "https://example.onmicrosoft.com/assets/logo.png"
-    use_wsfed              = false
-    waad_protocol          = "openid-connect"
-    waad_common_endpoint   = false
-    max_groups_to_retrieve = 250
-    api_enable_users       = true
-    scopes = [
-      "basic_profile",
-      "ext_groups",
-      "ext_profile"
-    ]
-    set_user_root_attributes               = "on_each_login"
-    should_trust_email_verified_connection = "never_set_emails_as_verified"
+    client_id        = var.google_workspace_connection_client_id
+    client_secret    = var.google_workspace_connection_secret
+    domain           = "digital.justice.gov.uk"
+    tenant_domain    = "digital.justice.gov.uk"
+    api_enable_users = true
+    scopes           = ["ext_profile", "basic_profile"]
     upstream_params = jsonencode({
       "screen_name" : {
         "alias" : "login_hint"
       }
     })
-    non_persistent_attrs = ["ethnicity", "gender"]
+    set_user_root_attributes = "on_each_login"
   }
+
+  enabled_clients = [
+    var.github_application_client_id  # Add the GitHub application's client ID here
+  ]
 }
