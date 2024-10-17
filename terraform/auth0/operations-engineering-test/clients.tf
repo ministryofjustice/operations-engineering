75b263cac59a34c33be677b01e9f8bca1ec62810
Sam Pepper <124164618+PepperMoJ@users.noreply.github.com>
2024-10-17 12:03:53 +0100

    ✨  Added GitHub Application in Terraform (#4920)
    * :recycle:  Moved google_oath2 connection to correct Terraform file
    
    * :sparkles: Added new GitHub client in code.

--- terraform/auth0/operations-engineering-test/clients.tf
+++ terraform/auth0/operations-engineering-test/clients.tf
@@ -43,92 +43,69 @@ resource "auth0_client" "terraform_provider_auth0" {
   }
 }
 
-resource "auth0_connection" "google_oauth2" {
-  display_name         = null
-  is_domain_connection = false
-  metadata             = {}
-  name                 = "google-oauth2"
-  realms               = ["google-oauth2"]
-  show_as_button       = null
-  strategy             = "google-oauth2"
-  options {
-    adfs_server                            = null
-    allowed_audiences                      = []
-    api_enable_users                       = false
-    app_id                                 = null
-    auth_params                            = {}
-    authorization_endpoint                 = null
-    brute_force_protection                 = false
-    client_id                              = null
-    client_secret                          = null # sensitive
-    community_base_url                     = null
-    configuration                          = null # sensitive
-    custom_scripts                         = {}
-    debug                                  = false
-    digest_algorithm                       = null
-    disable_cache                          = false
-    disable_self_service_change_password   = false
-    disable_sign_out                       = false
-    disable_signup                         = false
-    discovery_url                          = null
-    domain                                 = null
-    domain_aliases                         = []
-    enable_script_context                  = false
-    enabled_database_customization         = false
-    entity_id                              = null
-    fed_metadata_xml                       = null
-    fields_map                             = null
-    forward_request_info                   = false
-    from                                   = null
-    gateway_url                            = null
-    icon_url                               = null
-    identity_api                           = null
-    import_mode                            = false
-    ips                                    = []
-    issuer                                 = null
-    jwks_uri                               = null
-    key_id                                 = null
-    map_user_id_to_id                      = false
-    max_groups_to_retrieve                 = null
-    messaging_service_sid                  = null
-    metadata_url                           = null
-    metadata_xml                           = null
-    name                                   = null
-    non_persistent_attrs                   = []
-    password_policy                        = null
-    ping_federate_base_url                 = null
-    pkce_enabled                           = false
-    protocol_binding                       = null
-    provider                               = null
-    request_template                       = null
-    requires_username                      = false
-    scopes                                 = ["email", "profile"]
-    scripts                                = {}
-    set_user_root_attributes               = null
-    should_trust_email_verified_connection = null
-    sign_in_endpoint                       = null
-    sign_out_endpoint                      = null
-    sign_saml_request                      = false
-    signature_algorithm                    = null
-    signing_cert                           = null
-    strategy_version                       = 0
-    subject                                = null
-    syntax                                 = null
-    team_id                                = null
-    template                               = null
-    tenant_domain                          = null
-    token_endpoint                         = null
-    twilio_sid                             = null
-    twilio_token                           = null # sensitive
-    type                                   = null
-    upstream_params                        = null
-    use_cert_auth                          = false
-    use_kerberos                           = false
-    use_wsfed                              = false
-    user_id_attribute                      = null
-    userinfo_endpoint                      = null
-    waad_common_endpoint                   = false
-    waad_protocol                          = null
+resource "auth0_client" "github" {
+  allowed_clients                       = []
+  allowed_logout_urls                   = []
+  allowed_origins                       = []
+  app_type                              = "regular_web"
+  callbacks                             = ["https://github.com/orgs/ministryofjustice-test/saml/consume"]
+  client_aliases                        = []
+  client_metadata                       = {}
+  cross_origin_auth                     = true
+  cross_origin_loc                      = null
+  custom_login_page                     = null
+  custom_login_page_on                  = true
+  description                           = "Web application to SSO into the ministryofjustice-test GitHub Organisations"
+  form_template                         = null
+  grant_types                           = ["authorization_code", "implicit", "refresh_token", "client_credentials"]
+  initiate_login_uri                    = null
+  is_first_party                        = true
+  is_token_endpoint_ip_header_trusted   = false
+  logo_uri                              = null
+  name                                  = "GitHub - Test Organisation"
+  oidc_conformant                       = true
+  organization_require_behavior         = "no_prompt"
+  organization_usage                    = null
+  require_pushed_authorization_requests = false
+  sso                                   = false
+  sso_disabled                          = false
+  web_origins                           = []
+  jwt_configuration {
+    alg                 = "RS256"
+    lifetime_in_seconds = 36000
+    scopes              = {}
+    secret_encoded      = false
+  }
+  native_social_login {
+    apple {
+      enabled = false
+    }
+    facebook {
+      enabled = false
+    }
+  }
+  refresh_token {
+    expiration_type              = "non-expiring"
+    idle_token_lifetime          = 2592000
+    infinite_idle_token_lifetime = true
+    infinite_token_lifetime      = true
+    leeway                       = 0
+    rotation_type                = "non-rotating"
+    token_lifetime               = 31557600
+  }
+  addons {
+    samlp {
+      mappings = {
+        user_id = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"
+        email   = "emails"
+        name    = "full_name"
+      }
+      passthrough_claims_with_no_mapping = false
+      map_identities                     = false
+      signature_algorithm                = "rsa-sha256"
+      digest_algorithm                   = "sha256"
+      name_identifier_probes             = ["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"]
+    }
   }
 }
 
