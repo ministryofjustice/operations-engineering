183689aeb80967bfa5bdc11353646b722725e04d
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-10-21 17:20:48 +0100

    :recycle: Split the function calls up to reduce complexity

--- bin/check_version_pinning.py
+++ bin/check_version_pinning.py
@@ -3,38 +3,47 @@ import os
 import yaml
 
 
-def check_version_pinning(workflow_directory=".github/workflows"):
-    # List to store the results
-    results = []
-
-    # Loop through all files in the specified workflow directory
+def find_workflow_files(workflow_directory=".github/workflows"):
     for root, _, files in os.walk(workflow_directory):
         for file in files:
             if file.endswith(".yml") or file.endswith(".yaml"):
-                file_path = os.path.join(root, file)
+                yield os.path.join(root, file)
 
-                # Open and parse the YAML file
-                with open(file_path, "r") as f:
-                    try:
-                        workflows = yaml.safe_load(f)
-                    except yaml.YAMLError as e:
-                        print(f"Error parsing {file_path}: {e}")
-                        continue
 
-                    # Check for `uses` fields
-                    if workflows:
-                        for job in workflows.get("jobs", {}).values():
-                            for step in job.get("steps", []):
-                                uses = step.get("uses", "")
-                                if "@v" in uses and not (
-                                    "actions/" in uses or "ministryofjustice" in uses
-                                ):
-                                    results.append(f"{file_path}: {uses}")
+def parse_yaml_file(file_path):
+    with open(file_path, "r") as f:
+        try:
+            return yaml.safe_load(f)
+        except yaml.YAMLError as e:
+            print(f"Error parsing {file_path}: {e}")
+            return None
 
-    # Report the results
-    if results:
+
+def check_uses_field_in_workflow(workflows, file_path):
+    results = []
+    if workflows:
+        for job in workflows.get("jobs", {}).values():
+            for step in job.get("steps", []):
+                uses = step.get("uses", "")
+                if "@v" in uses and not (
+                    "actions/" in uses or "ministryofjustice" in uses
+                ):
+                    results.append(f"{file_path}: {uses}")
+    return results
+
+
+def check_version_pinning(workflow_directory=".github/workflows"):
+    all_results = []
+
+    for file_path in find_workflow_files(workflow_directory):
+        workflows = parse_yaml_file(file_path)
+        if workflows:
+            results = check_uses_field_in_workflow(workflows, file_path)
+            all_results.extend(results)
+
+    if all_results:
         print("Found workflows with pinned versions (@v):")
-        for result in results:
+        for result in all_results:
             print(result)
     else:
         print("No workflows found with pinned versions (@v).")
