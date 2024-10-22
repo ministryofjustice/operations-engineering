1f17234a9757efc2122e491db12b4b7e1dbfb429
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-10-22 15:49:51 +0100

    :fire: Removed old messaging technique

--- services/slack_service.py
+++ services/slack_service.py
@@ -21,43 +21,6 @@ class SlackService:
     def __init__(self, slack_token: str) -> None:
         self.slack_client = WebClient(slack_token)
 
-    def send_message_to_plaintext_channel_name(self, message, channel_name: str):
-        """
-        Sends a message to a plaintext channel by name.
-
-        Args:
-            message (str): The message to send.
-            channel_name (str): The name of the channel to send the message to.
-        """
-        channel_id = self._lookup_channel_id(channel_name)
-        if channel_id is None:
-            logging.error("Could not find channel %s", channel_name)
-        else:
-            response = self.slack_client.chat_postMessage(
-                channel=channel_id, text=message)
-            if not response['ok']:
-                logging.error("Error sending message to channel %s: %s",
-                              channel_name, response['error'])
-            else:
-                logging.info("Message sent to channel %s", channel_name)
-
-    def _lookup_channel_id(self, channel_name, cursor=''):
-        channel_id = None
-        response = self.slack_client.conversations_list(
-            limit=200, cursor=cursor)
-
-        if response['channels'] is not None:
-            for channel in response['channels']:
-                if channel['name'] == channel_name:
-                    channel_id = channel['id']
-                    break
-
-        if channel_id is None and response['response_metadata']['next_cursor'] != '':
-            channel_id = self._lookup_channel_id(
-                channel_name, cursor=response['response_metadata']['next_cursor'])
-
-        return channel_id
-
     def send_usage_alert_to_operations_engineering(
         self, period_in_days: int, usage_stats: UsageStats, usage_threshold: float, category: str
     ):
