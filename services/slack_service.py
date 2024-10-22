618e4b9f5ae29b132d2c591d8db798fa0e17bbcf
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-10-22 15:52:05 +0100

    :recycle: Added new messaging functions and corrected formatting of old ones.

--- services/slack_service.py
+++ services/slack_service.py
@@ -82,99 +82,152 @@ class SlackService:
             ]
         )
 
+    def send_slack_support_stats_report(self, support_statistics):
+        message = dedent(f"""
+                            *Slack Support Stats Report*
+                            Here an overview of our recent support statistics:
+                            {support_statistics}
+                         """.strip("/n"))
+        blocks = self._create_block_with_message(message)
+        self._send_alert_to_operations_engineering(blocks)
+
+    def send_dormant_user_list(self, user_list):
+        message = dedent(f"""
+                            *Dormant User Report*
+                            Here is a list of dormant GitHub users that have not been seen in Auth0 logs:
+                            {user_list}
+                         """.strip("/n"))
+        blocks = self._create_block_with_message(message)
+        self._send_alert_to_operations_engineering(blocks)
+
+    def send_pat_report_alert(self):
+        message = dedent("""
+                            Some expired PAT(s) have been detected.
+                            Please review the current list here:
+                            https://github.com/organizations/ministryofjustice/settings/personal-access-tokens/active
+                         """).strip("/n")
+        blocks = self._create_block_with_message(message)
+        self._send_alert_to_operations_engineering(blocks)
+
+    def send_new_github_joiner_metrics_alert(self, new_members_added_by_oe, new_members_added_externally, percentage, total_new_members, org, audit_log_url, time_delta_in_days):
+        message = dedent(f"""
+                            *New GitHub Joiner Metrics*
+                            Here are the {total_new_members} new joiners added in the last {time_delta_in_days} days within the '{org}' GitHub org.
+                            *Added by Operations Engineering:*
+                            {new_members_added_by_oe}
+                            *Added externally:*
+                            {new_members_added_externally}
+                            {percentage}% of the new joiners were added by operations engineering.
+                            Please review the audit log for more details: {audit_log_url}
+                         """).strip("/n")
+        blocks = self._create_block_with_message(message)
+        self._send_alert_to_operations_engineering(blocks)
+
+    def send_new_github_owners_alert(self, new_owner, date_added, added_by, org, audit_log_url):
+        message = dedent(f"""
+                            *New GitHub Owners Detected*
+                            A new owner has been detected in the `{org}` GitHub org.
+                            *New owner:* {new_owner}
+                            *Date added:* {date_added}
+                            *By who:* {added_by}
+
+                            Please review the audit log for more details: {audit_log_url}
+
+                         """).strip("/n")
+        blocks = self._create_block_with_message(message)
+        self._send_alert_to_operations_engineering(blocks)
+
+    def send_low_github_licenses_alert(self, remaining_licenses):
+        message = dedent(f"""
+                            *Low GitHub Licenses Remaining*
+                            There are only {remaining_licenses} GitHub licenses remaining in the enterprise account.
+                            Please add more licenses using the instructions outlined here:
+                            https://runbooks.operations-engineering.service.justice.gov.uk/documentation/internal/low-github-seats-procedure.html
+                         """).strip("\n")
+        blocks = self._create_block_with_message(message)
+        self._send_alert_to_operations_engineering(blocks)
+
+    def send_low_github_actions_quota_alert(self, percentage_used):
+        message = dedent(f"""
+                            *Low GitHub Actions Quota*
+                            {round(100 - percentage_used, 1)}% of the Github Actions minutes quota remains.
+                            What to do next: https://runbooks.operations-engineering.service.justice.gov.uk/documentation/internal/low-github-actions-minutes-procedure.html#low-github-actions-minutes-procedure
+                         """).strip("\n")
+        blocks = self._create_block_with_message(message)
+        self._send_alert_to_operations_engineering(blocks)
+
+    def send_low_gandi_funds_alert(self, remaining_funds, threshold):
+        message = dedent(f"""
+                            *Low Gandi Funds Remaining*
+                            :warning: We currently have £{remaining_funds} left out of £{threshold}
+                            Please read the following Runbook for next steps:
+                            https://runbooks.operations-engineering.service.justice.gov.uk/documentation/certificates/manual-ssl-certificate-processes.html#regenerating-certificates
+                         """).strip("\n")
+        blocks = self._create_block_with_message(message)
+        self._send_alert_to_operations_engineering(blocks)
+
     def send_unknown_user_alert_to_operations_engineering(self, users: list):
-        self.slack_client.chat_postMessage(
-            channel=self.OPERATIONS_ENGINEERING_ALERTS_CHANNEL_ID,
-            mrkdown=True,
-            blocks=[
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": dedent(f"""
-                            *Dormants Users Automation*
-                            Remove these users from the Dormants Users allow list:
+        message = dedent(f"""
+                            *Dormant Users Automation*
+                            Remove these users from the Dormant Users allow list:
                             {users}
                         """).strip("\n")
-                    }
-                }
-            ]
-        )
+        blocks = self._create_block_with_message(message)
+        self._send_alert_to_operations_engineering(blocks)
 
     def send_remove_users_from_github_alert_to_operations_engineering(
         self, number_of_users: int, organisation_name: str
     ):
-        self.slack_client.chat_postMessage(
-            channel=self.OPERATIONS_ENGINEERING_ALERTS_CHANNEL_ID,
-            mrkdown=True,
-            blocks=[
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": dedent(f"""
-                            *Dormants Users Automation*
+        message = dedent(f"""
+                            *Dormant Users Automation*
                             Removed {number_of_users} users from the {organisation_name} GitHub Organisation.
-                            See the GH Action for more info: https://github.com/ministryofjustice/operations-engineering
+                            See the GH Action for more info: {self.OPERATION_ENGINEERING_REPOSITORY_URL}
                         """).strip("\n")
-                    }
-                }
-            ]
-        )
+        blocks = self._create_block_with_message(message)
+        self._send_alert_to_operations_engineering(blocks)
 
     def send_unused_circleci_context_alert_to_operations_engineering(self, number_of_contexts: int):
-        self.slack_client.chat_postMessage(
-            channel=self.OPERATIONS_ENGINEERING_ALERTS_CHANNEL_ID,
-            mrkdown=True,
-            blocks=[
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": dedent(f"""
+        message = dedent(f"""
                             *Unused CircleCI Contexts*
                             A total of {number_of_contexts} unused CircleCI contexts have been detected.
-                            Please see the GH Action for more information: https://github.com/ministryofjustice/operations-engineering
+                            Please see the GH Action for more information: {self.OPERATION_ENGINEERING_REPOSITORY_URL}
                         """).strip("\n")
-                    }
-                }
-            ]
-        )
+        blocks = self._create_block_with_message(message)
+        self._send_alert_to_operations_engineering(blocks)
 
     def send_undelivered_email_alert_to_operations_engineering(
         self, email_addresses: list, organisation_name: str
     ):
-        self.slack_client.chat_postMessage(
-            channel=self.OPERATIONS_ENGINEERING_ALERTS_CHANNEL_ID,
-            mrkdown=True,
-            blocks=[
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": dedent(f"""
-                            *Dormants Users Automation*
+        message = dedent(f"""
+                            *Dormant Users Automation*
                             Undelivered emails for {organisation_name} GitHub Organisation:
                             {email_addresses}
                             Remove these users manually
                         """).strip("\n")
-                    }
-                }
-            ]
-        )
+        blocks = self._create_block_with_message(message)
+        self._send_alert_to_operations_engineering(blocks)
+
+    def send_unowned_repos_slack_message(self, repositories: list):
+        message = dedent(f"""
+                            *Unowned Repositories Automation*
+                            Repositories on the GitHub Organisation that have no team or collaborator:
+                            {repositories}
+                        """).strip("\n")
+        blocks = self._create_block_with_message(message)
+        self._send_alert_to_operations_engineering(blocks)
 
     def send_remove_users_slack_message(self, number_of_users: int, organisation_name: str):
-        message = f"*Dormants Users Automation*\nRemoved {number_of_users} users from the {organisation_name} GitHub Organisation.\nSee the GH Action for more info: https://github.com/ministryofjustice/operations-engineering"
+        message = f"*Dormant Users Automation*\nRemoved {number_of_users} users from the {organisation_name} GitHub Organisation.\nSee the GH Action for more info: {self.OPERATION_ENGINEERING_REPOSITORY_URL}"
         blocks = self._create_block_with_message(message)
         self._send_alert_to_operations_engineering(blocks)
 
     def send_unknown_users_slack_message(self, unknown_users: list):
-        message = f"*Dormants Users Automation*\nRemove these users from the Dormants Users allow list:\n{unknown_users}"
+        message = f"*Dormant Users Automation*\nRemove these users from the Dormant Users allow list:\n{unknown_users}"
         blocks = self._create_block_with_message(message)
         self._send_alert_to_operations_engineering(blocks)
 
     def send_undelivered_emails_slack_message(self, email_addresses: list, organisation_name: str):
-        message = f"*Dormants Users Automation*\nUndelivered emails for {organisation_name} GitHub Organisation:\n{email_addresses}\nRemove these users manually"
+        message = f"*Dormant Users Automation*\nUndelivered emails for {organisation_name} GitHub Organisation:\n{email_addresses}\nRemove these users manually"
         blocks = self._create_block_with_message(message)
         self._send_alert_to_operations_engineering(blocks)
 
