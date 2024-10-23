37f07ad4d8b9c4072878b6e8b5ecb4c9cbfb92f2
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-10-23 16:14:58 +0100

    basic python job

---
+++ bin/alarm_for_old_poc_repositories.py
@@ -0,0 +1,30 @@
+# get all repositories in poc topic - github service
+# get age of all target repositories using gh api - github service
+# calculate age of repositories - github service
+# compare to threshold and alert - job level
+
+import sys
+import os
+
+from services.github_service import GithubService
+from services.slack_service import SlackService
+from config.constants import ENTERPRISE, MINISTRY_OF_JUSTICE, SLACK_CHANNEL
+
+def alert_for_old_poc_repositories():
+    github_token = os.environ.get("GH_TOKEN")
+    slack_token = os.environ.get("ADMIN_SLACK_TOKEN")
+
+    if github_token is None:
+        print("No GITHUB_TOKEN environment variable set")
+        sys.exit(1)
+
+    if slack_token is None:
+        print("No SLACK_TOKEN environment variable set")
+        sys.exit(1)
+
+    github_service = GithubService(github_token, MINISTRY_OF_JUSTICE, ENTERPRISE)
+
+    print(github_service.get_paginated_list_of_repositories_per_topic())
+
+if __name__ == "__main__":
+    alert_for_old_poc_repositories("poc")
