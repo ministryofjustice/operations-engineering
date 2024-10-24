39c0a1990a93a5a5d35970a378217eb68a1e8c61
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-10-24 14:12:40 +0100

    test proper function again

--- bin/alarm_for_old_poc_repositories.py
+++ bin/alarm_for_old_poc_repositories.py
@@ -29,10 +29,10 @@ def alert_for_old_poc_repositories():
     github_service = GithubService(github_token, MINISTRY_OF_JUSTICE, ENTERPRISE)
     slack_service = SlackService(slack_token)
 
-    # old_poc_repositories = github_service.get_old_poc_repositories()
-    created_date = github_service.test_function()
+    old_poc_repositories = github_service.get_old_poc_repositories()
+    # created_date = github_service.test_function()
 
-    print(created_date)
+    # print(created_date)
 
     # if old_poc_repositories != {}:
     #     slack_service.send_message_to_plaintext_channel_name(construct_message(old_poc_repositories), SLACK_CHANNEL)
