f3ac03d448b10cab6fa9b68d00a0763f3aadab2d
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-10-31 09:58:05 +0000

    :fire: Removed legacy dormant user S3 functionality.

--- services/s3_service.py
+++ services/s3_service.py
@@ -1,78 +1,13 @@
 # pylint: disable=wrong-import-order
-import csv
-import json
 import os
-from datetime import datetime
 import boto3
 from botocore.exceptions import ClientError
-from dateutil.relativedelta import relativedelta
-from config.constants import NO_ACTIVITY
+
 
 class S3Service:
     def __init__(self, bucket_name: str, organisation_name: str) -> None:
         self.client = boto3.client("s3")
         self.bucket_name = bucket_name
-        self.emailed_users_file_name = f"{organisation_name.lower()}_first_email_list.json"
-        self.emailed_users_file_path = self.emailed_users_file_name
-        self.dormant_users_file_name = "dormant.csv"
-        self.dormant_users_file_path = "dormant.csv"
-        self.org_people_file_name = f"export-{organisation_name.lower()}.json"
-        self.org_people_file_path = self.org_people_file_name
-
-    def delete_emailed_users_file(self):
-        self._delete_file(self.emailed_users_file_name)
-
-    def save_emailed_users_file(self, users: list):
-        with open(self.emailed_users_file_path, "w", encoding="utf8") as the_file:
-            the_file.write(json.dumps(users))
-        self._upload_file(self.emailed_users_file_name, self.emailed_users_file_path)
-
-    def get_users_have_emailed(self):
-        self._download_file(self.emailed_users_file_name, self.emailed_users_file_path)
-        with open(self.emailed_users_file_path, "r", encoding="utf8") as the_file:
-            return json.load(the_file)
-
-    def get_users_from_dormant_user_file(self) -> list:
-        username = 2
-        is_outside_collaborator = 6
-        users = []
-        self._download_file(self.dormant_users_file_name, self.dormant_users_file_path)
-        with open(self.dormant_users_file_path, "r", encoding="utf8") as the_file:
-            dormant_user_file = csv.reader(the_file)
-            next(the_file)  # Do not remove next() it skips the top row of the .csv file
-            for user in dormant_user_file:
-                users.append(
-                    {
-                        "username": user[username].lower(),
-                        "is_outside_collaborator": user[is_outside_collaborator],
-                    }
-                )
-        return users
-
-    def get_active_users_from_org_people_file(self) -> list:
-        three_months_ago_date = datetime.now() - relativedelta(months=3)
-        active_users = []
-        for user in self._get_users_from_org_people_file():
-            if user["last_active"].lower() == NO_ACTIVITY:  # Some users have "No activity" skip over these users
-                continue
-            last_active_date = datetime.strptime(user['last_active'][0:10], '%Y-%m-%d')
-            if last_active_date > three_months_ago_date:
-                active_users.append(user["username"])
-        return active_users
-
-    def _get_users_from_org_people_file(self) -> list:
-        users = []
-        self._download_file(self.org_people_file_name, self.org_people_file_path)
-        with open(self.org_people_file_path, "r", encoding="utf8") as the_file:
-            org_user_file = json.load(the_file)
-            for user in org_user_file:
-                users.append(
-                    {
-                        "username": user["login"].lower(),
-                        "last_active": user["last_active"]
-                    }
-                )
-        return users
 
     def _download_file(self, object_name: str, file_path: str):
         self.client.download_file(self.bucket_name, object_name, file_path)
