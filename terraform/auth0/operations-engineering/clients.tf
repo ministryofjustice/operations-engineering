d48c3fa6e488ca6f38429682962460dc6a0cd7b8
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-10-31 09:58:21 +0000

    :fire: Removed legacy dormant user Auth0 client.

--- terraform/auth0/operations-engineering/clients.tf
+++ terraform/auth0/operations-engineering/clients.tf
@@ -228,51 +228,6 @@ resource "auth0_client" "standards_report" {
   }
 }
 
-resource "auth0_client" "legacy_dormant_users" {
-  allowed_clients                       = []
-  allowed_logout_urls                   = []
-  allowed_origins                       = []
-  app_type                              = "non_interactive"
-  callbacks                             = []
-  client_aliases                        = []
-  client_metadata                       = {}
-  cross_origin_auth                     = false
-  cross_origin_loc                      = null
-  custom_login_page                     = null
-  custom_login_page_on                  = true
-  description                           = "Enables (Legacy) Dormant Users process to read and delete Auth0 users."
-  form_template                         = null
-  grant_types                           = ["client_credentials"]
-  initiate_login_uri                    = null
-  is_first_party                        = true
-  is_token_endpoint_ip_header_trusted   = false
-  logo_uri                              = null
-  name                                  = "(Legacy) Dormant Users"
-  oidc_backchannel_logout_urls          = []
-  oidc_conformant                       = true
-  organization_require_behavior         = null
-  organization_usage                    = null
-  require_pushed_authorization_requests = false
-  sso                                   = false
-  sso_disabled                          = false
-  web_origins                           = []
-  jwt_configuration {
-    alg                 = "RS256"
-    lifetime_in_seconds = 36000
-    scopes              = {}
-    secret_encoded      = false
-  }
-  refresh_token {
-    expiration_type              = "non-expiring"
-    idle_token_lifetime          = 2592000
-    infinite_idle_token_lifetime = true
-    infinite_token_lifetime      = true
-    leeway                       = 0
-    rotation_type                = "non-rotating"
-    token_lifetime               = 31557600
-  }
-}
-
 resource "auth0_client" "kpi_dashboard" {
   allowed_clients                       = []
   allowed_logout_urls                   = []
