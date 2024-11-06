83a4762597fa8d80d4ffa36887c6158234a21dad
abachleda-baca <agnieszka.bachleda@digital.justice.gov.uk>
2024-11-06 15:06:46 +0000

    added test utils

---
+++ test/test_utils/test_config_certificates.py
@@ -0,0 +1,18 @@
+test_config = {
+    "keys": {
+        "gandi_api_key": "test_key",
+        "notify_api_key": "test_key",
+    },
+    "template_ids": {
+        "cert_expiry": "test_template_id"
+    },
+    "urls": {
+        "gandi_base_url": "test_base_url",
+        "gandi_cert_url_extension": "test_cert_url_extension",
+    },
+    "cert_expiry_thresholds": [30, 15, 1],
+    "reply_email": "test_reply_email",
+    "gandi": {
+        "topup_amount": "test_amount"
+    }
+}
