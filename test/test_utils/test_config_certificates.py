a7bb012a514649be7ed00cc81078ed5f523eebc3
abachleda-baca <agnieszka.bachleda@digital.justice.gov.uk>
2024-11-07 15:50:13 +0000

    edit test config

--- test/test_utils/test_config_certificates.py
+++ test/test_utils/test_config_certificates.py
@@ -1,5 +1,9 @@
 test_config = {
     "CERT_EXPIRY_THRESHOLDS": [30, 15, 1],
     "CERT_REPLY_EMAIL": "test_reply_email",
-    "CERT_EXPIRY_TEMPALATE_ID": "test_expiry_template_id"
+    "CERT_EXPIRY_TEMPALATE_ID": "test_expiry_template_id",
+    "CERT_URL_EXTENSION": "v5/certificate/issued-certs",
+    "CERT_REPORT_TEMPLATE_ID": "test_report_template_id",
+    "CERT_UNDELIVERED_REPORT_TEMPALATE_ID": "test_undelivered_report_template_id"
+    
 }
