ec7b4565e3bf3e418c07bbd454cd8d5ed972e14f
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-11-08 14:51:39 +0000

    :wrench: Fixed org structure.

--- bin/dormant_user_removal_accuracy_report.py
+++ bin/dormant_user_removal_accuracy_report.py
@@ -1,7 +1,5 @@
-import os
 import logging
 from datetime import datetime, timedelta
-import requests
 from services.github_service import GithubService
 from config.constants import ENTERPRISE, MINISTRY_OF_JUSTICE, MOJ_ANALYTICAL_SERVICES
 from utils.environment import EnvironmentVariables
@@ -17,27 +15,25 @@ def main():
 
     env = EnvironmentVariables(required_env_vars)
 
-    moj_github_org_token = GithubService(env.get(
+    moj_github_org_service = GithubService(env.get(
         "GH_MOJ_ADMIN_TOKEN"), MINISTRY_OF_JUSTICE)
-    # ap_github_org_token = GithubService(env.get(
+    # ap_github_org_service = GithubService(env.get(
     #     "GH_AP_ADMIN_TOKEN"), MOJ_ANALYTICAL_SERVICES)
 
     # Configure logging
     logging.basicConfig(level=logging.INFO)
-
-    github_service = GithubService(moj_github_org_token, MINISTRY_OF_JUSTICE, ENTERPRISE)
 
     github_username = 'AntonyBishop'
 
     today = datetime.now()
     one_week_ago = (today - timedelta(days=7)).strftime('%Y-%m-%d')
 
-    removed_users_entries = github_service.get_user_removal_events(one_week_ago, github_username)
+    removed_users_entries = moj_github_org_service.get_user_removal_events(one_week_ago, github_username)
 
     removed_usernames = [entry['userLogin'].lower() for entry in removed_users_entries]
     logging.info("Users removed by %s in the last week: %s", github_username, removed_usernames)
 
-    current_members = [username.lower() for username in github_service.get_org_members_login_names()]
+    current_members = [username.lower() for username in moj_github_org_service.get_org_members_login_names()]
 
     users_rejoined = [username for username in removed_usernames if username in current_members]
     logging.info("Users who have rejoined: %s", users_rejoined)
