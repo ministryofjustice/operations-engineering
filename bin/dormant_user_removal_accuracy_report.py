e8ed03932315ba1a829fd187cfd9745a87982ce3
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-11-08 14:43:06 +0000

    :sparkles: New script for gathering and printing rejoiner metrics.

---
+++ bin/dormant_user_removal_accuracy_report.py
@@ -0,0 +1,68 @@
+import os
+import logging
+from datetime import datetime, timedelta
+import requests
+from services.github_service import GithubService
+from config.constants import ENTERPRISE, MINISTRY_OF_JUSTICE
+from utils.environment import EnvironmentVariables
+
+
+
+def get_environment_variables() -> str:
+    github_token = os.getenv("GH_TOKEN")
+    if not github_token:
+        raise ValueError(
+            "The env variable GH_TOKEN is empty or missing")
+
+    return github_token
+
+
+def main():
+
+    required_env_vars = [
+        "GH_MOJ_ADMIN_TOKEN",
+        "GH_AP_ADMIN_TOKEN",
+        "ADMIN_SLACK_TOKEN",
+    ]
+    env = EnvironmentVariables(required_env_vars)
+
+    # Configure logging
+    logging.basicConfig(level=logging.INFO)
+
+    github_token = get_environment_variables()
+    github_service = GithubService(github_token, MINISTRY_OF_JUSTICE, ENTERPRISE)
+
+    github_username = 'AntonyBishop'
+
+    today = datetime.now()
+    one_week_ago = (today - timedelta(days=7)).strftime('%Y-%m-%d')
+
+    removed_users_entries = github_service.get_user_removal_events(one_week_ago, github_username)
+
+    removed_usernames = [entry['userLogin'].lower() for entry in removed_users_entries]
+    logging.info("Users removed by %s in the last week: %s", github_username, removed_usernames)
+
+    current_members = [username.lower() for username in github_service.get_org_members_login_names()]
+
+    users_rejoined = [username for username in removed_usernames if username in current_members]
+    logging.info("Users who have rejoined: %s", users_rejoined)
+
+    total_removed = len(removed_usernames)
+    total_rejoined = len(users_rejoined)
+
+    if total_removed == 0:
+        error_rate = 0
+    else:
+        error_rate = (total_rejoined / total_removed) * 100
+
+    result_message = (
+        f"Total users removed by {github_username} in the last week: {total_removed}\n"
+        f"Total users who have rejoined: {total_rejoined}\n"
+        f"Percentage of users removed in error: {error_rate:.2f}%"
+    )
+
+    logging.info(result_message)
+
+
+if __name__ == '__main__':
+    main()
