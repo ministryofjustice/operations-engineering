a9ecded3de3157214c63f89040118bc57aeb7a94
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-11-14 16:18:18 +0000

    set up for running in multi orgs

--- bin/identify_non_committers.py
+++ bin/identify_non_committers.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime, timedelta
-from config.constants import MINISTRY_OF_JUSTICE, MINISTRY_OF_JUSTICE_TEST
+from config.constants import MINISTRY_OF_JUSTICE, MINISTRY_OF_JUSTICE_TEST, MOJ_ANALYTICAL_SERVICES
 import time
 from operator import itemgetter
 
@@ -11,43 +11,49 @@ def main():
 
     gh_admin_token = os.getenv("GH_ADMIN_TOKEN")
     if not gh_admin_token:
-        raise Exception("ADMIN_GITHUB_TOKEN must be set")
+        raise Exception("GH_ADMIN_TOKEN must be set")
 
-    gh = GithubService(str(gh_admin_token), MINISTRY_OF_JUSTICE_TEST)
-    # gh = GithubService(str(gh_admin_token), MINISTRY_OF_JUSTICE)
+    # gh_orgs = [
+    #     GithubService(str(gh_admin_token), MINISTRY_OF_JUSTICE),
+    #     GithubService(str(gh_admin_token), MOJ_ANALYTICAL_SERVICES)
+    # ]
 
-    # Create set of current user logins
-    current_users_set = gh.get_current_user_logins()
-    # Ignore set
-    ignore_set = {"moj-operations-engineering-bot"}
+    gh_orgs = [
+        GithubService(str(gh_admin_token), MINISTRY_OF_JUSTICE_TEST),
+    ]
 
+    for gh in gh_orgs:
+        # Create set of current user logins
+        current_users_set = gh.get_current_user_logins()
+        # Ignore set
+        ignore_set = {"moj-operations-engineering-bot"}
 
-    # Get list of dictionaries of active repos and their current contributors
-    repos_and_current_contributors = gh.get_current_contributors_for_active_repos()
-    print("\nRepos and current contributors:")
-    print(repos_and_current_contributors)
+        # Get list of dictionaries of active repos and their current contributors
+        repos_and_current_contributors = gh.get_current_contributors_for_active_repos()
+        print("\nRepos and current contributors:")
+        print(repos_and_current_contributors)
 
-    users_to_check = list(current_users_set - ignore_set)
+        users_to_check = list(current_users_set - ignore_set)
 
-    dormant_users = []
-    since_datetime=datetime(2023, 8, 7)
-    count = 1
-    number_of_users = len(users_to_check)
+        non_committers = []
+        since_datetime=datetime(2023, 8, 7)
+        count = 1
+        number_of_users = len(users_to_check)
 
-    for login in users_to_check:
-        print(f"\nChecking user {count} of {number_of_users}")
-        commits = gh.user_has_commmits_since(
-            login=login,
-            repos_and_contributors=repos_and_current_contributors,
-            since_datetime=since_datetime
-        )
-        if not commits:
-            dormant_users.append(login)
-        print(f"{login} has made commits since {since_datetime}: {commits}")
-        count += 1
+        for login in users_to_check:
+            print(f"\nChecking user {count} of {number_of_users}")
+            commits = gh.user_has_commmits_since(
+                login=login,
+                repos_and_contributors=repos_and_current_contributors,
+                since_datetime=since_datetime
+            )
+            if not commits:
+                non_committers.append(login)
+            print(f"{login} has made commits since {since_datetime}: {commits}")
+            count += 1
 
-    print(f"\nDormant users: {dormant_users}")
-    print(f"Active users: {set(users_to_check) - set(dormant_users)}")
+        print(f"\nNon-committers since {since_datetime}: {non_committers}")
+        print(f"Active committers since {since_datetime}: {set(users_to_check) - set(non_committers)}")
 
 
 if __name__ == "__main__":
