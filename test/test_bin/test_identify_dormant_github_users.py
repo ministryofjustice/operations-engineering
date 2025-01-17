d2ec4f3286e625347d55950de52e48f0715cc950
connormaglynn <connor.glynn@digital.justice.gov.uk>
2025-01-17 13:26:07 +0000

    🔧 Fix Unit Tests

--- test/test_bin/test_identify_dormant_github_users.py
+++ test/test_bin/test_identify_dormant_github_users.py
@@ -16,7 +16,7 @@ class TestDormantGitHubUsers(unittest.TestCase):
         mock_github_service = MagicMock()
         mock_github_service.get_all_enterprise_members = MagicMock(return_value=["member1", "member2", "analytical-platform-bot", "cloud-platform-dummy-user"])
 
-        assert get_inactive_users_from_data_lake_ignoring_bots_and_collaborators(mock_github_service, self.allowed_bot_users) == ["member2"]
+        assert get_inactive_users_from_data_lake_ignoring_bots_and_collaborators(mock_github_service, self.allowed_bot_users, False) == ["member2"]
 
     @patch("gql.transport.aiohttp.AIOHTTPTransport.__new__", new=MagicMock)
     @patch("gql.Client.__new__", new=MagicMock)
@@ -27,7 +27,7 @@ class TestDormantGitHubUsers(unittest.TestCase):
     @patch('bin.identify_dormant_github_users_v2.get_inactive_users_from_data_lake_ignoring_bots_and_collaborators')
     @patch('os.environ')
     def test_identify_dormant_github_users(self, mock_env, _mock_get_inactive_users_from_data_lake_ignoring_bots_and_collaborators, mock_map_usernames_to_emails, mock_filter_out_active_auth0_users, mock_filter_out_inactive_committers, _mock_github_client_core_api):
-        mock_env.get.side_effect = lambda k: 'mock_token' if k in ['GH_ADMIN_TOKEN', 'ADMIN_SLACK_TOKEN'] else None
+        mock_env.get.side_effect = lambda k: 'mock_token' if k in ['GH_ADMIN_TOKEN', 'ADMIN_SLACK_TOKEN', 'USE_MP_INFRASTRUCTURE'] else None
         mock_map_usernames_to_emails.return_value = [{"name": "user1", "email": "user1@gmail.com"}, {"name": "user2", "email": "user1@gmail.com"}, {"name": "user3", "email": "user1@gmail.com"}]
         mock_filter_out_active_auth0_users.return_value = ["user1", "user2"]
 
