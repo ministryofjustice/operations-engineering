98fe51d66fedeca0607b36b8304c0570a1f03c35
abachleda-baca <agnieszka.bachleda@digital.justice.gov.uk>
2025-02-14 14:54:08 +0000

    test

--- bin/fetch_github_actions_quota.py
+++ bin/fetch_github_actions_quota.py
@@ -1,8 +1,13 @@
 import os
-
+import logging
 from services.github_service import GithubService
 from services.kpi_service import KpiService
 from config.constants import ENTERPRISE, MINISTRY_OF_JUSTICE
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 
 def get_environment_variables() -> str:
@@ -19,7 +24,7 @@ def fetch_gha_quota():
     github_service = GithubService(github_token, MINISTRY_OF_JUSTICE, ENTERPRISE)
 
     total_minutes_used = github_service.calculate_total_minutes_enterprise()
-
+    logger.info("APIurl: %s ", os.getenv("KPI_DASHBOARD_URL"))
     # Save metric in KPI dashboard db
     KpiService(os.getenv("KPI_DASHBOARD_URL"), os.getenv("KPI_DASHBOARD_API_KEY")).track_enterprise_github_actions_quota_usage(total_minutes_used)
 
