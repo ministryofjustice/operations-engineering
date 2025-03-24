7920aa1c9b3aa30dab6b90da6d01c9e76daa698d
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-03-24 16:34:19 +0000

    wip tests

--- test/test_bin/test_identify_dormant_outside_collaborators.py
+++ test/test_bin/test_identify_dormant_outside_collaborators.py
@@ -3,24 +3,52 @@ from unittest.mock import MagicMock, patch
 
 from bin.identify_dormant_outside_collaborators import identify_dormant_outside_collaborators
 
+from services.github_service import GithubService
+from services.slack_service import SlackService
 
 class TestIdentifyDormantOutsideCollaborators(unittest.TestCase):
 
-    @patch("gql.transport.aiohttp.AIOHTTPTransport.__new__", new=MagicMock)
-    @patch("gql.Client.__new__", new=MagicMock)
-    @patch("services.github_service.Github")
+    @patch.object(GithubService, "__new__")
+    @patch.object(SlackService, "__new__")
     @patch('os.environ')
-    def test_identify_dormant_outside_collaborators(self, mock_env, mock_get_active_repos_and_outside_collaborators, mock_user_has_committed_to_repo_since):
+    def test_identify_non_active_outside_collaborators(self, mock_env, mock_slack_service, mock_github_service):
         mock_env.get.side_effect = lambda k: 'mock_token' if k in ['GH_MOJ_TOKEN', 'GH_MOJAS_TOKEN', 'ADMIN_SLACK_TOKEN'] else 90 if k in ['DAYS_SINCE'] else None
-        mock_get_active_repos_and_outside_collaborators.return_value = [
-            {'repository': 'repo1', 'public': False, 'outside_collaborators': ['outside_collab_1', 'outside_collab_2']},
-            {'repository': 'repo2', 'public': True, 'outside_collaborators': ['outside_collab_3']}
+
+        mock_github_instance = MagicMock()
+        mock_github_service.return_value = mock_github_instance
+        mock_github_instance.get_active_repos_and_outside_collaborators.return_value = [
+            {'repository': 'repo1', 'public': False, 'outside_collaborators': ['outside_collab_1']}
         ]
-        mock_user_has_committed_to_repo_since.return_value = [True, False, True]
+        mock_github_instance.user_has_committed_to_repo_since.return_value = False
+
+        mock_slack_instance = MagicMock()
+        mock_slack_service.return_value = mock_slack_instance
 
         identify_dormant_outside_collaborators()
 
-        mock_get_active_repos_and_outside_collaborators.assert_called_once()
+        mock_slack_instance.send_dormant_outside_collaborator_list.assert_called()
+        mock_slack_instance.send_dormant_outside_collaborator_list.assert_called_once_with(user_list=['outside_collab_1'], days_since='90')
+
+    @patch.object(GithubService, "__new__")
+    @patch.object(SlackService, "__new__")
+    @patch('os.environ')
+    def test_ignore_active_outside_collaborators(self, mock_env, mock_slack_service, mock_github_service):
+        mock_env.get.side_effect = lambda k: 'mock_token' if k in ['GH_MOJ_TOKEN', 'GH_MOJAS_TOKEN', 'ADMIN_SLACK_TOKEN'] else 90 if k in ['DAYS_SINCE'] else None
+
+        mock_github_instance = MagicMock()
+        mock_github_service.return_value = mock_github_instance
+        mock_github_instance.get_active_repos_and_outside_collaborators.return_value = [
+            {'repository': 'repo1', 'public': False, 'outside_collaborators': ['outside_collab_1']}
+        ]
+        mock_github_instance.user_has_committed_to_repo_since.return_value = True
+
+        mock_slack_instance = MagicMock()
+        mock_slack_service.return_value = mock_slack_instance
+
+        identify_dormant_outside_collaborators()
+
+        mock_slack_instance.send_dormant_outside_collaborator_list.assert_called()
+        mock_slack_instance.send_dormant_outside_collaborator_list.assert_called_once_with(user_list=[], days_since='90')
 
 
 if __name__ == "__main__":
