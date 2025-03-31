9c8956c2a0b78ad22a0718f4e01d8db153f41c19
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-03-31 14:23:16 +0100

    import branch protection

--- terraform/github/repositories/imports.tf
+++ terraform/github/repositories/imports.tf
@@ -2,3 +2,8 @@ import {
   to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_repository.default
   id = "test-tamf-repo-test-org"
 }
+
+import {
+  to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_branch_protection.default
+  id = "test-tamf-repo-test-org"
+}
