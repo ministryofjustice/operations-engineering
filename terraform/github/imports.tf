f6363b41f90e36db44be0bb0fce7272f2fea3818
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-03-31 13:49:29 +0100

    import from module

--- terraform/github/imports.tf
+++ terraform/github/imports.tf
@@ -1,4 +1,4 @@
 import {
   to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_repository.default
-  id = "test-tamf-repo-test-org"
+  id = "ministryofjustice-test.test-tamf-repo-test-org"
 }
