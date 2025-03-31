f0ba732d43fb6702230a66e3b2abfbf7b49cd124
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-03-31 14:50:35 +0100

    missed resource name

--- terraform/github/repositories/imports.tf
+++ terraform/github/repositories/imports.tf
@@ -9,11 +9,11 @@ import {
 }
 
 import {
-  to = module.ministryofjustice-test.module.test_tamf_repo_test_org.maintain["9456244"]
+  to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_team_repository.maintain["9456244"]
   id = "9456244:test-tamf-repo-test-org"
 }
 
 import {
-  to = module.ministryofjustice-test.module.test_tamf_repo_test_org.push["9472191"]
+  to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_team_repository.push["9472191"]
   id = "9472191:test-tamf-repo-test-org"
 }
