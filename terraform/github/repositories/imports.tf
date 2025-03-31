887edf8f86440cc7d6a5087fb9c31a809aebe6b2
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-03-31 14:41:38 +0100

    add team associations

--- terraform/github/repositories/imports.tf
+++ terraform/github/repositories/imports.tf
@@ -7,3 +7,13 @@ import {
   to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_branch_protection.default
   id = "test-tamf-repo-test-org"
 }
+
+import {
+  to = module.ministryofjustice-test.module.test_tamf_repo_test_org.maintain["9456244"]
+  id = "9456244:test-tamf-repo-test-org"
+}
+
+import {
+  to = module.ministryofjustice-test.module.test_tamf_repo_test_org.maintain["9472191"]
+  id = "9472191:test-tamf-repo-test-org"
+}
