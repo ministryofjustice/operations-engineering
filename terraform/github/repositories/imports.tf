6ba59bef1ed41340965dba5a293882f473b68670
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-04-02 15:31:50 +0100

    comment out already imported stuff

--- terraform/github/repositories/imports.tf
+++ terraform/github/repositories/imports.tf
@@ -1,31 +1,31 @@
-# ministryofjustice-test
+# # ministryofjustice-test
 
-import {
-  to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_repository.default
-  id = "test-tamf-repo-test-org"
-}
-import {
-  to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_branch_protection.default
-  id = "R_kgDOLRPf2Q:main"
-}
-import {
-  to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_team_repository.maintain["9456244"]
-  id = "9456244:test-tamf-repo-test-org"
-}
-import {
-  to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_team_repository.push["9472191"]
-  id = "9472191:test-tamf-repo-test-org"
-}
+# import {
+#   to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_repository.default
+#   id = "test-tamf-repo-test-org"
+# }
+# import {
+#   to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_branch_protection.default
+#   id = "R_kgDOLRPf2Q:main"
+# }
+# import {
+#   to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_team_repository.maintain["9456244"]
+#   id = "9456244:test-tamf-repo-test-org"
+# }
+# import {
+#   to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_team_repository.push["9472191"]
+#   id = "9472191:test-tamf-repo-test-org"
+# }
 
-import {
-  to = module.ministryofjustice-test.module.test-repo-levg.github_repository.default
-  id = "test-repo-levg"
-}
+# import {
+#   to = module.ministryofjustice-test.module.test-repo-levg.github_repository.default
+#   id = "test-repo-levg"
+# }
 
-import {
-  to = module.ministryofjustice-test.module.test-repo-levg.github_branch_protection.default
-  id = "R_kgDOLPRAVQ:main"
-}
+# import {
+#   to = module.ministryofjustice-test.module.test-repo-levg.github_branch_protection.default
+#   id = "R_kgDOLPRAVQ:main"
+# }
 
 # # ministryofjustice
 import {
