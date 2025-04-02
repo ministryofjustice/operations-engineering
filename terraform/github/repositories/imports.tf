69c3d5105c5c53f39edbd79207ea005b91bc0883
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-04-02 12:01:00 +0100

    use id throughout

--- terraform/github/repositories/imports.tf
+++ terraform/github/repositories/imports.tf
@@ -1,32 +1,31 @@
 # ministryofjustice-test
+
+# R_kgDOLRPf2Q test-tamf-repo-test-org
 import {
   to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_repository.default
-  id = "test-tamf-repo-test-org"
+  id = "R_kgDOLRPf2Q"
 }
-
 import {
   to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_branch_protection.default
   id = "R_kgDOLRPf2Q:main"
 }
-
 import {
   to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_team_repository.maintain["9456244"]
-  id = "9456244:test-tamf-repo-test-org"
+  id = "9456244:R_kgDOLRPf2Q"
 }
-
 import {
   to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_team_repository.push["9472191"]
-  id = "9472191:test-tamf-repo-test-org"
+  id = "9472191:R_kgDOLRPf2Q"
 }
-
+# R_kgDOLPRAVQ test-repo-levg
 import {
   to = module.ministryofjustice-test.module.test-repo-levg.github_repository.default
-  id = "test-repo-levg"
+  id = "R_kgDOLPRAVQ"
 }
 
 import {
   to = module.ministryofjustice-test.module.test-repo-levg.github_branch_protection.default
-  id = "test-repo-levg:main"
+  id = "R_kgDOLPRAVQ:main"
 }
 
 # ministryofjustice
