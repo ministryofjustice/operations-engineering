26612014756650517a2dc45dfc99cbf52b9171ef
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-04-02 12:08:08 +0100

    test

--- terraform/github/repositories/imports.tf
+++ terraform/github/repositories/imports.tf
@@ -11,11 +11,11 @@ import {
 }
 import {
   to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_team_repository.maintain["9456244"]
-  id = "9456244:R_kgDOLRPf2Q"
+  id = "9456244:test-tamf-repo-test-org"
 }
 import {
   to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_team_repository.push["9472191"]
-  id = "9472191:R_kgDOLRPf2Q"
+  id = "9472191:test-tamf-repo-test-org"
 }
 # R_kgDOLPRAVQ test-repo-levg
 import {
