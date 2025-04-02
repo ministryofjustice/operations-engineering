f3ca9886e4495209d3d71955e83345fd3f4d064c
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-04-02 17:05:24 +0100

    import 13 moj repos

--- terraform/github/repositories/imports.tf
+++ terraform/github/repositories/imports.tf
@@ -1,33 +1,4 @@
-# # ministryofjustice-test
-
-# import {
-#   to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_repository.default
-#   id = "test-tamf-repo-test-org"
-# }
-# import {
-#   to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_branch_protection.default
-#   id = "R_kgDOLRPf2Q:main"
-# }
-# import {
-#   to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_team_repository.maintain["9456244"]
-#   id = "9456244:test-tamf-repo-test-org"
-# }
-# import {
-#   to = module.ministryofjustice-test.module.test_tamf_repo_test_org.github_team_repository.push["9472191"]
-#   id = "9472191:test-tamf-repo-test-org"
-# }
-
-# import {
-#   to = module.ministryofjustice-test.module.test-repo-levg.github_repository.default
-#   id = "test-repo-levg"
-# }
-
-# import {
-#   to = module.ministryofjustice-test.module.test-repo-levg.github_branch_protection.default
-#   id = "R_kgDOLPRAVQ:main"
-# }
-
-# # ministryofjustice
+# ministryofjustice
 import {
   to = module.ministryofjustice.module.github-actions.github_repository.default
   id = "github-actions"
@@ -43,182 +14,182 @@ import {
   id = "4192115:github-actions"
 }
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-certificate-renewal.github_repository.default
-#   id = "operations-engineering-certificate-renewal"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-certificate-renewal.github_repository.default
+  id = "operations-engineering-certificate-renewal"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-certificate-renewal.github_branch_protection.default
-#   id = "operations-engineering-certificate-renewal:main"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-certificate-renewal.github_branch_protection.default
+  id = "operations-engineering-certificate-renewal:main"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-certificate-renewal.github_team_repository.admin["4192115"]
-#   id = "4192115:operations-engineering-certificate-renewal"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-certificate-renewal.github_team_repository.admin["4192115"]
+  id = "4192115:operations-engineering-certificate-renewal"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-cloud-platform-namespace.github_repository.default
-#   id = "operations-engineering-cloud-platform-namespace"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-cloud-platform-namespace.github_repository.default
+  id = "operations-engineering-cloud-platform-namespace"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-cloud-platform-namespace.github_branch_protection.default
-#   id = "operations-engineering-cloud-platform-namespace:main"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-cloud-platform-namespace.github_branch_protection.default
+  id = "operations-engineering-cloud-platform-namespace:main"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-cloud-platform-namespace.github_team_repository.admin["4192115"]
-#   id = "4192115:operations-engineering-cloud-platform-namespace"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-cloud-platform-namespace.github_team_repository.admin["4192115"]
+  id = "4192115:operations-engineering-cloud-platform-namespace"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-documentation-browser-extension.github_repository.default
-#   id = "operations-engineering-documentation-browser-extension"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-documentation-browser-extension.github_repository.default
+  id = "operations-engineering-documentation-browser-extension"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-documentation-browser-extension.github_branch_protection.default
-#   id = "operations-engineering-documentation-browser-extension:main"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-documentation-browser-extension.github_branch_protection.default
+  id = "operations-engineering-documentation-browser-extension:main"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-documentation-browser-extension.github_team_repository.admin["4192115"]
-#   id = "4192115:operations-engineering-documentation-browser-extension"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-documentation-browser-extension.github_team_repository.admin["4192115"]
+  id = "4192115:operations-engineering-documentation-browser-extension"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-find-a-github-repository-owner.github_repository.default
-#   id = "operations-engineering-find-a-github-repository-owner"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-find-a-github-repository-owner.github_repository.default
+  id = "operations-engineering-find-a-github-repository-owner"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-find-a-github-repository-owner.github_branch_protection.default
-#   id = "operations-engineering-find-a-github-repository-owner:main"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-find-a-github-repository-owner.github_branch_protection.default
+  id = "operations-engineering-find-a-github-repository-owner:main"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-find-a-github-repository-owner.github_team_repository.admin["4192115"]
-#   id = "4192115:operations-engineering-find-a-github-repository-owner"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-find-a-github-repository-owner.github_team_repository.admin["4192115"]
+  id = "4192115:operations-engineering-find-a-github-repository-owner"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-github-actions-dashboard.github_repository.default
-#   id = "operations-engineering-github-actions-dashboard"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-github-actions-dashboard.github_repository.default
+  id = "operations-engineering-github-actions-dashboard"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-github-actions-dashboard.github_branch_protection.default
-#   id = "operations-engineering-github-actions-dashboard:main"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-github-actions-dashboard.github_branch_protection.default
+  id = "operations-engineering-github-actions-dashboard:main"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-github-actions-dashboard.github_team_repository.admin["4192115"]
-#   id = "4192115:operations-engineering-github-actions-dashboard"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-github-actions-dashboard.github_team_repository.admin["4192115"]
+  id = "4192115:operations-engineering-github-actions-dashboard"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-kpi-dashboard.github_repository.default
-#   id = "operations-engineering-kpi-dashboard"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-kpi-dashboard.github_repository.default
+  id = "operations-engineering-kpi-dashboard"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-kpi-dashboard.github_branch_protection.default
-#   id = "operations-engineering-kpi-dashboard:main"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-kpi-dashboard.github_branch_protection.default
+  id = "operations-engineering-kpi-dashboard:main"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-kpi-dashboard.github_team_repository.admin["4192115"]
-#   id = "4192115:operations-engineering-kpi-dashboard"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-kpi-dashboard.github_team_repository.admin["4192115"]
+  id = "4192115:operations-engineering-kpi-dashboard"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-runbooks.github_repository.default
-#   id = "operations-engineering-runbooks"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-runbooks.github_repository.default
+  id = "operations-engineering-runbooks"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-runbooks.github_branch_protection.default
-#   id = "operations-engineering-runbooks:main"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-runbooks.github_branch_protection.default
+  id = "operations-engineering-runbooks:main"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-runbooks.github_team_repository.admin["4192115"]
-#   id = "4192115:operations-engineering-runbooks"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-runbooks.github_team_repository.admin["4192115"]
+  id = "4192115:operations-engineering-runbooks"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-support-metadata.github_repository.default
-#   id = "operations-engineering-support-metadata"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-support-metadata.github_repository.default
+  id = "operations-engineering-support-metadata"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-support-metadata.github_branch_protection.default
-#   id = "operations-engineering-support-metadata:main"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-support-metadata.github_branch_protection.default
+  id = "operations-engineering-support-metadata:main"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-support-metadata.github_team_repository.admin["4192115"]
-#   id = "4192115:operations-engineering-support-metadata"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-support-metadata.github_team_repository.admin["4192115"]
+  id = "4192115:operations-engineering-support-metadata"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-user-guide.github_repository.default
-#   id = "operations-engineering-user-guide"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-user-guide.github_repository.default
+  id = "operations-engineering-user-guide"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-user-guide.github_branch_protection.default
-#   id = "operations-engineering-user-guide:main"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-user-guide.github_branch_protection.default
+  id = "operations-engineering-user-guide:main"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering-user-guide.github_team_repository.admin["4192115"]
-#   id = "4192115:operations-engineering-user-guide"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering-user-guide.github_team_repository.admin["4192115"]
+  id = "4192115:operations-engineering-user-guide"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering.github_repository.default
-#   id = "operations-engineering"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering.github_repository.default
+  id = "operations-engineering"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering.github_branch_protection.default
-#   id = "operations-engineering:main"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering.github_branch_protection.default
+  id = "operations-engineering:main"
+}
 
-# import {
-#   to = module.ministryofjustice.module.operations-engineering.github_team_repository.admin["4192115"]
-#   id = "4192115:operations-engineering"
-# }
+import {
+  to = module.ministryofjustice.module.operations-engineering.github_team_repository.admin["4192115"]
+  id = "4192115:operations-engineering"
+}
 
-# import {
-#   to = module.ministryofjustice.module.terraform-aws-mtasts.github_repository.default
-#   id = "terraform-aws-mtasts"
-# }
+import {
+  to = module.ministryofjustice.module.terraform-aws-mtasts.github_repository.default
+  id = "terraform-aws-mtasts"
+}
 
-# import {
-#   to = module.ministryofjustice.module.terraform-aws-mtasts.github_branch_protection.default
-#   id = "terraform-aws-mtasts:main"
-# }
+import {
+  to = module.ministryofjustice.module.terraform-aws-mtasts.github_branch_protection.default
+  id = "terraform-aws-mtasts:main"
+}
 
-# import {
-#   to = module.ministryofjustice.module.terraform-aws-mtasts.github_team_repository.admin["4192115"]
-#   id = "4192115:terraform-aws-mtasts"
-# }
+import {
+  to = module.ministryofjustice.module.terraform-aws-mtasts.github_team_repository.admin["4192115"]
+  id = "4192115:terraform-aws-mtasts"
+}
 
-# import {
-#   to = module.ministryofjustice.module.terraform-github-repository.github_repository.default
-#   id = "terraform-github-repository"
-# }
+import {
+  to = module.ministryofjustice.module.terraform-github-repository.github_repository.default
+  id = "terraform-github-repository"
+}
 
-# import {
-#   to = module.ministryofjustice.module.terraform-github-repository.github_branch_protection.default
-#   id = "terraform-github-repository:main"
-# }
+import {
+  to = module.ministryofjustice.module.terraform-github-repository.github_branch_protection.default
+  id = "terraform-github-repository:main"
+}
 
-# import {
-#   to = module.ministryofjustice.module.terraform-github-repository.github_team_repository.admin["4192115"]
-#   id = "4192115:terraform-github-repository"
-# }
+import {
+  to = module.ministryofjustice.module.terraform-github-repository.github_team_repository.admin["4192115"]
+  id = "4192115:terraform-github-repository"
+}
--- terraform/github/repositories/ministryofjustice/operations-engineering-certificate-renewal.tf
+++ terraform/github/repositories/ministryofjustice/operations-engineering-certificate-renewal.tf
@@ -1,14 +1,14 @@
-# module "operations-engineering-certificate-renewal" {
-#   source  = "ministryofjustice/repository/github"
-#   version = "1.2.1"
+module "operations-engineering-certificate-renewal" {
+  source  = "ministryofjustice/repository/github"
+  version = "1.2.1"
 
-#   poc = false
+  poc = false
 
-#   name        = "operations-engineering-certificate-renewal"
-#   description = "An application to automatically manage the renewal of certificates, and notify when certificates are close to expiring."
-#   topics      = ["operations-engineering"]
+  name        = "operations-engineering-certificate-renewal"
+  description = "An application to automatically manage the renewal of certificates, and notify when certificates are close to expiring."
+  topics      = ["operations-engineering"]
 
-#   team_access = {
-#     admin = [var.operations_engineering_team_id]
-#   }
-# }
+  team_access = {
+    admin = [var.operations_engineering_team_id]
+  }
+}
