ae94889361f3edfeedbc59f7c73bf1c740b55a9a
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-04-04 17:08:12 +0100

    specifiy con ID and client ID in con_client

--- terraform/auth0/operations-engineering-test/imports-ops-eng-test.tf
+++ terraform/auth0/operations-engineering-test/imports-ops-eng-test.tf
@@ -17,3 +17,16 @@ import {
   to = auth0_connection.google_workspace
   id = "con_VacE1q5yZxznHQAj"
 }
+
+# This resource can be imported by specifying the
+# connection ID and client ID separated by "::" (note the double colon)
+# <connectionID>::<clientID>
+
+import {
+  to = auth0_connection_client.google_workspace_github
+  id = "con_VacE1q5yZxznHQAj::Ez0SbR2tPwaDgL6tjWALIKSy4m3WtSFr"
+}
+import {
+  to = auth0_connection.google_oauth2
+  id = "con_yyrB18AQbSUcMLTm"
+}
