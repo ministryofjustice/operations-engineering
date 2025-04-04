e02de65c475d9aac402b272708240a1fb36f2fba
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-04-04 16:52:48 +0100

    add imports for test tennant client and connection

--- terraform/auth0/operations-engineering-test/imports-ops-eng-test.tf
+++ terraform/auth0/operations-engineering-test/imports-ops-eng-test.tf
@@ -2,3 +2,18 @@ import {
   to = auth0_client.terraform_provider_auth0
   id = "xI46sB1af7TKeycQ3xfTA2qIxeLiX2R8"
 }
+
+import {
+  to = auth0_client.github
+  id = "Ez0SbR2tPwaDgL6tjWALIKSy4m3WtSFr"
+}
+
+import {
+  to = auth0_client.default_app
+  id = "qkvVnu0b4pXd6FdEbjm073w4MchhpOoB"
+}
+
+import {
+  to = auth0_connection.google_workspace
+  id = "con_VacE1q5yZxznHQAj"
+}
