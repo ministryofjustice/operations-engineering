a09f282b3dc2a5fab138d66d4a857709a38c1f21
tam <32667518+tamsinforbes@users.noreply.github.com>
2025-04-07 16:00:44 +0100

    Point at new auth0 tfstate file & import existing (#5308)
    * point at new auth0 tfstate file
    * update bucket in test auth0 app too
    * add imports for test tenant
    * add imports for test tenant client and connection
    * specifiy con ID and client ID in con_client
    * add tenant imports
    * import block resource server
    * import prod tenant resources
    * prod is eu not uk
    * add pagerduty sso client id import

---
+++ terraform/auth0/operations-engineering-test/imports-ops-eng-test.tf
@@ -0,0 +1,54 @@
+import {
+  to = auth0_client.terraform_provider_auth0
+  id = "xI46sB1af7TKeycQ3xfTA2qIxeLiX2R8"
+}
+
+import {
+  to = auth0_client.github
+  id = "Ez0SbR2tPwaDgL6tjWALIKSy4m3WtSFr"
+}
+
+import {
+  to = auth0_client.default_app
+  id = "qkvVnu0b4pXd6FdEbjm073w4MchhpOoB"
+}
+
+import {
+  to = auth0_connection.google_workspace
+  id = "con_VacE1q5yZxznHQAj"
+}
+
+# This resource can be imported by specifying the
+# connection ID and client ID separated by "::" (note the double colon)
+# <connectionID>::<clientID>
+
+import {
+  to = auth0_connection_client.google_workspace_github
+  id = "con_VacE1q5yZxznHQAj::Ez0SbR2tPwaDgL6tjWALIKSy4m3WtSFr"
+}
+import {
+  to = auth0_connection.google_oauth2
+  id = "con_yyrB18AQbSUcMLTm"
+}
+
+# As this is not a resource identifiable by an ID within the Auth0 Management API,
+# tenant can be imported using a random string.
+#
+# We recommend [Version 4 UUID](https://www.uuidgenerator.net/version4)
+import {
+  to = auth0_tenant.tenant
+  id = "db796dd0-9670-4641-85bb-b6462e3ab6a9"
+}
+
+import {
+  to = auth0_resource_server.auth0_management_api
+  id = "https://operations-engineering-test.uk.auth0.com/api/v2/"
+}
+
+# Use random string!
+# We recommend [Version 4 UUID](https://www.uuidgenerator.net/version4)
+
+import {
+  to = auth0_attack_protection.attack_protection
+  id = "f9e2376d-c64d-44ec-9a34-04b65dabb0ca"
+}
---
+++ terraform/auth0/operations-engineering/imports-ops-eng.tf
@@ -0,0 +1,65 @@
+# auth0_client
+
+import {
+  to = auth0_client.dockersso
+  id = "67v1ZXZuszSi8bQY9m4MIeSlUN4WMMeq"
+}
+
+import {
+  to = auth0_client.find_a_github_repository_owner
+  id = "75pylVfaiPH8nghRtTW6I2WPnKufCZDh"
+}
+
+import {
+  to = auth0_client.github
+  id = "XtMmeAkh5KcHjCgyFY9Zsh72giu36oPh"
+}
+
+import {
+  to = auth0_client.kpi_dashboard
+  id = "k6ysmMa4y8ubV1cHw7DhoZnmt5odBoeJ"
+}
+
+import {
+  to = auth0_client.pagerduty
+  id = "hrmwI3eZiyRLR2DQQmgCIZEPQ2wsuhxN"
+}
+
+import {
+  to = auth0_client.standards_report
+  id = "xXSrBtGQXB92MDyKwkTH6gDFGLcIHBR3"
+}
+
+import {
+  to = auth0_client.terraform_provider_auth0
+  id = "xrKMUJJxZaofhHo0foGwdnpLtcOxLeko"
+}
+
+
+
+# As this is not a resource identifiable by an ID within the Auth0 Management API,
+# tenant can be imported using a random string.
+#
+# We recommend [Version 4 UUID](https://www.uuidgenerator.net/version4)
+import {
+  to = auth0_tenant.tenant
+  id = "9c1d147b-d629-4a28-9300-40d923fa5633"
+}
+
+import {
+  to = auth0_resource_server.auth0_management_api
+  id = "https://operations-engineering.eu.auth0.com/api/v2/"
+}
+
+# Use random string!
+# We recommend [Version 4 UUID](https://www.uuidgenerator.net/version4)
+
+import {
+  to = auth0_attack_protection.attack_protection
+  id = "4e65102b-6e82-453c-9273-d8fb15e3d9a4"
+}
+
+import {
+  to = auth0_guardian.guardian
+  id = "2c20278e-f738-4b84-a876-688efc38dbcf"
+}
