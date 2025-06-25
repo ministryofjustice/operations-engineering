748657fd357fb7fe28252d00c744c898950bfce6
tam <32667518+tamsinforbes@users.noreply.github.com>
2025-06-25 09:27:46 +0100

    Add homepage url to COAT repo (#5433)
    * testsign
    * test
    * tf fmt

--- terraform/github/repositories/ministryofjustice/cloud-optimisation-and-accountability.tf
+++ terraform/github/repositories/ministryofjustice/cloud-optimisation-and-accountability.tf
@@ -8,6 +8,8 @@ module "cloud-optimisation-and-accountability" {
   description = "A GitHub repository for the Cloud Optimisation and Accountability Team"
   topics      = ["cloud-optimisation-and-accountability"]
 
+  homepage_url = "https://cloud-optimisation-and-accountability.justice.gov.uk/"
+
   team_access = {
     admin = [var.cloud_optimisation_and_accountability_team_id]
   }
