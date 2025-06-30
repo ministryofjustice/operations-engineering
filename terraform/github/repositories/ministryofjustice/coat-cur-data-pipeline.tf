04877b2f44e8909d148477a6f3858bc5e4c68e5c
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-06-30 13:25:04 +0100

    new template

--- terraform/github/repositories/ministryofjustice/coat-cur-data-pipeline.tf
+++ terraform/github/repositories/ministryofjustice/coat-cur-data-pipeline.tf
@@ -7,14 +7,15 @@ module "coat-cur-data-pipeline" {
   name        = "coat-cur-data-pipeline"
   description = "A GitHub repository for the Cloud Optimisation and Accountability Team Cost and Usage Report Data Pipeline "
   topics      = ["cloud-optimisation-and-accountability"]
-  template = {
-    "owner" : "ministryofjustice",
-    "repository" : "analytical-platform-airflow-python-template"
-  }
 
   homepage_url = "https://cloud-optimisation-and-accountability.justice.gov.uk/"
 
   team_access = {
     admin = [var.cloud_optimisation_and_accountability_team_id]
+  }
+
+  template = {
+    "owner" : "ministryofjustice",
+    "repository" : "analytical-platform-airflow-python-template"
   }
 }
