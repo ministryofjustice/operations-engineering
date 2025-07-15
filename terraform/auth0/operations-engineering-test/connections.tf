5510b521186cbf0b09a837e0e5c2f7d310269576
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-07-15 13:01:03 +0100

    fix fmt

--- terraform/auth0/operations-engineering-test/connections.tf
+++ terraform/auth0/operations-engineering-test/connections.tf
@@ -4,18 +4,18 @@ resource "auth0_connection" "google_workspace" {
   is_domain_connection = false
   strategy             = "google-apps"
   show_as_button       = true
-  # options {
-  #   client_id     = var.google_workspace_connection_client_id
-  #   client_secret = var.google_workspace_connection_secret
-  #   domain        = "justice.gov.uk"
-  #   tenant_domain = "justice.gov.uk"
-  #   api_enable_users = true
-  #   scopes           = ["ext_profile", "basic_profile"]
-  #   upstream_params = jsonencode({
-  #     "screen_name" : {
-  #       "alias" : "login_hint"
-  #     }
-  #   })
-  #   set_user_root_attributes = "on_each_login"
-  # }
+  options {
+    client_id        = var.google_workspace_connection_client_id
+    client_secret    = var.google_workspace_connection_secret
+    domain           = "justice.gov.uk"
+    tenant_domain    = "justice.gov.uk"
+    api_enable_users = true
+    scopes           = ["ext_profile", "basic_profile"]
+    upstream_params = jsonencode({
+      "screen_name" : {
+        "alias" : "login_hint"
+      }
+    })
+    set_user_root_attributes = "on_each_login"
+  }
 }
