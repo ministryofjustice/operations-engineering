88929b3afaa453654ed88a5289c8a4fbba2dba3d
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-07-15 12:55:18 +0100

    fix fmt

--- terraform/auth0/operations-engineering-test/connections.tf
+++ terraform/auth0/operations-engineering-test/connections.tf
@@ -5,12 +5,12 @@ resource "auth0_connection" "google_workspace" {
   strategy             = "google-apps"
   show_as_button       = true
   options {
-    client_id        = var.google_workspace_connection_client_id
-    client_secret    = var.google_workspace_connection_secret
-    domain           = "justice.gov.uk"
-    tenant_domain    = "justice.gov.uk"
-    api_enable_users = false
-    scopes           = ["ext_profile", "basic_profile"]
+    client_id     = var.google_workspace_connection_client_id
+    client_secret = var.google_workspace_connection_secret
+    domain        = "justice.gov.uk"
+    tenant_domain = "justice.gov.uk"
+    # api_enable_users = true
+    # scopes           = ["ext_profile", "basic_profile"]
     upstream_params = jsonencode({
       "screen_name" : {
         "alias" : "login_hint"
